a93740ac3f51f2939760290c0b2b0bed47d03180
Jonathan Soo <jcsoo@agora.com>
2017-04-28 15:20:29 -0400

    Add support for sleep in timer

--- s32k144evb/src/timer.rs
+++ s32k144evb/src/timer.rs
@@ -1,35 +1,34 @@
 use hal::{pcc, lpit};
+use core::ptr;
 
 pub fn lpit0() -> lpit::Timer {
     pcc::set_lpit_enabled(true, pcc::Source::SPLLDIV2);
     lpit::timer(lpit::LPIT0, 0)
 }
 
-// use hal::{sim, lpit};
-
-// pub fn tim0() -> pit::Timer {
-//     sim::set_pit_enabled(true);
-//     pit::set_enabled(true);
-//     pit::timer(0)
-// }
-
-// pub fn tim0_unchecked() -> pit::Timer {
-//     pit::timer(0)
-// }
-
+static mut DELAY_DONE: bool = false;
 pub fn delay(ms: u32) {
+    unsafe { DELAY_DONE = false; }
+    unsafe extern "C" fn delay_handler() {
+        let t = lpit::timer(lpit::LPIT0, 0);
+        if t.tif() {
+            t.clr_tif();
+            DELAY_DONE = true;
+        }
+    }
+
     // Assume clock is 40Mhz
     let t0 = lpit0();
     t0.set_value(40_000 * ms);
+    t0.set_handler(Some(delay_handler));
     t0.clr_tif();
-    t0.set_enabled(true);
-    while t0.tif() == false {}
-    t0.clr_tif();
+    t0.set_tie(true);
+    t0.set_doze_enabled(true);
+    t0.set_enabled(true);    
+    
+    unsafe {
+        while !ptr::read_volatile(&DELAY_DONE as *const bool) { asm!("wfi") }
+    }
     t0.set_enabled(false);
-//     let t0 = tim0();
-//     t0.set_load_value(60_000 * ms);
-//     t0.clr_interrupt_flag();
-//     t0.set_enabled(true);
-//     while t0.interrupt_flag() == false {}
-//     t0.clr_interrupt_flag();
+    t0.set_handler(None);
 }
