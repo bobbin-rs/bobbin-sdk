ba689ab0d6e281d39d3b8e2db4f39faa5029490c
Jonathan Soo <jcsoo@agora.com>
2017-05-01 16:49:08 -0400

    Add more output

--- evb-s32k144/examples/uart.rs
+++ evb-s32k144/examples/uart.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
 extern crate evb_s32k144 as board;
 
 use core::fmt::Write;
@@ -8,10 +9,12 @@ use core::fmt::Write;
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
-    let mut u = board::serial::serial1();
+    // Use external UART
+    let mut u = board::serial::serial0();
     let led0 = board::led::led_red();
     let mut i = 0;
-    write!(u, "Running LPUART Test\r\n").unwrap();
+    println!("UART Test - Output on LPUART0");
+    write!(u, "UART Test on LPUART0\r\n").unwrap();
     loop {
         if let Some(c) = u.try_getc() {
             write!(u, "{}: {:?}\r\n", i, c as char).unwrap();
