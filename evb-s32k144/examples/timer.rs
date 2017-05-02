50ee395aea2f86395f8a415a16463cb8d064b708
Jonathan Soo <jcsoo@agora.com>
2017-05-02 15:56:31 -0400

    Macro to instantiate timer

--- evb-s32k144/examples/timer.rs
+++ evb-s32k144/examples/timer.rs
@@ -12,78 +12,40 @@ use board::hal::lpit::Timer;
 
 // Assume PIT bus clock is 40Mhz
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
-    board::init();
-    
-    let client0 = setup(board::timer::lpit0(), static_semaphore!());
-    let client1 = setup(board::timer::lpit1(), static_semaphore!());    
-
-    client0.start(700);
-
-    client1.start(500);
-
-    let mut i = 0;
-    let mut j = 0;
-    loop {
-        if client0.ready() {
-            println!("Client 0: {}", j);
-            if j == 10 {
-                println!("Stopping Client 0");
-                client0.stop();
+macro_rules! timer_client {
+    ($timer:expr) => {
+        {
+            
+            static mut DRIVER: Option<Driver> = None;
+            unsafe extern "C" fn handler() {                
+                DRIVER.as_ref().unwrap().handle_irq()
             }
-            j += 1;
-        }
-        if client1.ready() {
-            println!("Client 1: {}", i);
-            if i == 10 {
-                println!("Stopping Client 1");
-                client1.stop();
-            }
-            i += 1;
+            $timer.set_handler(Some(handler));
+            
+            let (r, w) = static_semaphore!();
+            unsafe {
+                DRIVER = Some(Driver::new($timer.clone(), w));
+            }            
+            Client::new($timer, r)
         }
     }
-}
-
-pub fn setup(t: Timer, sem: (SemaphoreReader<'static>, SemaphoreWriter<'static>)) -> Client<'static> {    
-    let (r, w) = sem;
-    let index = unsafe {
-        if DRIVER0.is_none() {
-            DRIVER0 = Some(Driver::new(t.clone(), w));
-            0
-        } else if DRIVER1.is_none() {
-            DRIVER1 = Some(Driver::new(t.clone(), w));
-            1
-        } else {
-            panic!("No driver slots available");
-        }
-    };
-    Client::new(t, r, index)
 }
 
 pub struct Client<'a> {
     timer: Timer,
     reader: SemaphoreReader<'a>,
-    index: usize
 }
 
 impl<'a> Client<'a> {
-    pub fn new(timer: Timer, reader: SemaphoreReader<'a>, index: usize) -> Self {
+    pub fn new(timer: Timer, reader: SemaphoreReader<'a>) -> Self {
         Client {
             timer: timer,
             reader: reader,
-            index: index,
         }
     }
 
     pub fn start(&self, period: u32) {
         let timer = &self.timer;
-        let handler = match self.index { 
-            0 => handler_0,
-            1 => handler_1,
-            _ => panic!("out of handlers"),
-        };
-        timer.set_handler(Some(handler));
         timer.set_value(40_000 * period);
         timer.clr_tif();
         timer.set_tie(true);
@@ -93,7 +55,7 @@ impl<'a> Client<'a> {
     pub fn stop(&self) {
         let timer = &self.timer;
         timer.set_enabled(false);
-        timer.set_handler(None);
+        timer.set_tie(false);
     }
 
     pub fn ready(&self) -> bool {
@@ -104,18 +66,6 @@ impl<'a> Client<'a> {
         while self.reader.read() == 0 {}
     }
 }
-
-static mut DRIVER0: Option<Driver> = None;
-static mut DRIVER1: Option<Driver> = None;
-
-pub unsafe extern "C" fn handler_0() {
-    DRIVER0.as_ref().unwrap().handle_irq()
-}        
-
-pub unsafe extern "C" fn handler_1() {
-    DRIVER1.as_ref().unwrap().handle_irq()
-}        
-
 
 pub struct Driver<'a> {
     timer: Timer,
@@ -138,3 +88,36 @@ impl<'a> Driver<'a> {
     }
 }
 
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    
+    let client0 = timer_client!(board::timer::lpit0());
+    let client1 = timer_client!(board::timer::lpit1());
+
+    client0.start(700);
+
+    client1.start(500);
+
+    let mut i = 0;
+    let mut j = 0;
+    loop {
+        if client0.ready() {
+            println!("Client 0: {}", j);
+            if j == 10 {
+                println!("Stopping Client 0");
+                client0.stop();
+            }
+            j += 1;
+        }
+        if client1.ready() {
+            println!("            Client 1: {}", i);
+            if i == 10 {
+                println!("Stopping Client 1");
+                client1.stop();
+            }
+            i += 1;
+        }
+    }
+}
