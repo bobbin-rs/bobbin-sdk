d432751e50b339e2ab58eafbcb753eff6036fcbe
Jonathan Soo <jcsoo@agora.com>
2017-05-17 17:52:42 -0400

    Test build script

--- nucleo-f031k6/src/btn.rs
+++ nucleo-f031k6/src/btn.rs
@@ -1,10 +1,9 @@
-use chip::gpio::GPIOA;
 use chip::pins;
 use hal::rcc;
 use hal::gpio;
 
 // BTN0 = PA12
 pub fn btn0() -> gpio::PinInput { 
-    rcc::set_gpio_enabled(GPIOA, true);
+    rcc::set_gpio_enabled(pins::PA12.0, true);
     gpio::pin(pins::PA12).into_input(gpio::Pull::PullUp)
 }
