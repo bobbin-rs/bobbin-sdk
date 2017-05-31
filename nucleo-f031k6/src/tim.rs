df7232d2abc0cedc4e5de7c0a409cb6b3ec8754a
Jonathan Soo <jcsoo@agora.com>
2017-05-31 13:43:24 -0400

    Use new tim_gen delay

--- nucleo-f031k6/src/tim.rs
+++ nucleo-f031k6/src/tim.rs
@@ -2,19 +2,12 @@ use chip::tim_gen::*;
 use hal::rcc;
 use hal::tim_gen::TimGenExt;
 
-pub fn delay(ms: u32) {
-    rcc::enable(&TIM21);
-    let t = TIM21;
+pub fn delay(ms: u32) {    
     // Clock at 32MHz
     // Divide by 16,000 => 2khz
-    t.set_prescaler(15999);
-    t.set_update_event();
-    t.clr_update_interrupt_flag();
     // Set auto_reload to ms x 2
-    t.set_auto_reload(ms << 1);
-    t.set_enabled(true);
-    while t.update_interrupt_flag() == false {}
-    t.clr_update_interrupt_flag();
-    t.set_enabled(false);
-    rcc::disable(&TIM21);
+    let t = TIM21;
+    rcc::enable(&t);
+    t.delay(ms << 1, 15999);
+    rcc::disable(&t);
 }
