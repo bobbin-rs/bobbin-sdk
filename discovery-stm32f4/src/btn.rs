c03cd988f777fa74694937b23cb00066ff71c72e
Jonathan Soo <jcsoo@agora.com>
2017-06-01 10:53:25 -0400

    Use RCC traits

--- discovery-stm32f4/src/btn.rs
+++ discovery-stm32f4/src/btn.rs
@@ -1,9 +1,8 @@
 use hal::gpio::*;
-use hal::rcc;
 
 pub const BTN0: Pa0 = PA0;
 
 pub fn init() {
-    rcc::enable(&BTN0.port());
+    BTN0.port().rcc_enable();
     BTN0.mode_input().pull_down();
 }
