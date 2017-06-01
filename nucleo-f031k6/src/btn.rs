c19e4ce9ba1b249ecb94d86808b04c0182083b26
Jonathan Soo <jcsoo@agora.com>
2017-06-01 10:50:59 -0400

    Use rcc traits

--- nucleo-f031k6/src/btn.rs
+++ nucleo-f031k6/src/btn.rs
@@ -1,9 +1,8 @@
 use hal::gpio::*;
-use hal::rcc;
 
 pub const BTN0: Pa12 = PA12;
 
 pub fn init() {
-    rcc::enable(&BTN0.port());
+    BTN0.port().rcc_enable();
     BTN0.mode_input().pull_up();
 }
--- nucleo-f031k6/src/led.rs
+++ nucleo-f031k6/src/led.rs
@@ -1,9 +1,8 @@
 use hal::gpio::*;
-use hal::rcc;
 
 pub const LED0: Pb3 = PB3;
 
 pub fn init() {
-    rcc::enable(&LED0.port());
+    LED0.port().rcc_enable();
     LED0.mode_output();
 }
