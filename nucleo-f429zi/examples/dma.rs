a953cab485b972a87aa7fe7005a06a3aa6275126
Jonathan Soo <jcsoo@agora.com>
2017-06-13 15:52:57 -0400

    DMA Test

--- nucleo-f429zi/examples/dma.rs
+++ nucleo-f429zi/examples/dma.rs
@@ -1,58 +1,43 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 #[macro_use]
 extern crate nucleo_f429zi as board;
 
-use board::chip::dma::DMA2;
-use board::hal::dma;
-use board::hal::rcc;
-
-// Set up Memory-Memory DMA using DMA2 Stream 0
-// Interrupt on completion
-
+use board::hal::dma::*;
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
-    println!("Running DMA Example");
+
+    println!("DMA Test");
 
     let src = [0xffu8; 1024];
     let dst = [0u8; 1024];
-
-    rcc::set_dma_enabled(DMA2, true);
-    let s = dma::stream(DMA2, 0);
-    s    
+    
+    let ch = DMA2_STREAM0;
+    ch.periph().rcc_set_enabled(true);
+    ch    
         .set_pa(&src as *const u8 as u32)
         .set_m0a(&dst as *const u8 as u32)
-        .set_psize(dma::Size::Bit8)
+        .set_psize(Size::Bit8)
         .set_pinc(true)
-        .set_msize(dma::Size::Bit8)
+        .set_msize(Size::Bit8)
         .set_minc(true)
-        .set_dir(dma::Dir::MtoM)
+        .set_dir(Dir::MtoM)
         .set_ndt(1024)
         .clr_teif()
-        .clr_tcif()
-        .set_enabled(true);
+        .clr_tcif();
 
-    println!("Waiting for DMA to complete");
-    let mut n = 0;
-    loop {
-        if n == 100_000 {
-            panic!("Timeout")
-        }
-        if s.teif() {
-            panic!("DMA Error")
-        }
-        if s.tcif() {
-            break
-        }
-        n += 1;
-    }
-    println!("Transfer Complete");
+    println!("Starting DMA Transfer");
+    ch.clr_tcif().set_enabled(true);
+
+    while !ch.tcif() {}
     for i in 0..1024 {
         assert_eq!(src[i], dst[i]);
     }
     println!("Transfer Verified");
     println!("DMA Example Done");
+
     loop {}
 }
