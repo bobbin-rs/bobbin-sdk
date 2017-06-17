eeecc4c2fd7cc9423c299aff9e756382f83bdbde
Jonathan Soo <jcsoo@agora.com>
2017-06-17 15:34:47 -0400

    Switch to TCC0

--- arduino-zero/examples/pwm.rs
+++ arduino-zero/examples/pwm.rs
@@ -7,33 +7,43 @@ extern crate arduino_zero as board;
 
 use board::hal::gclk;
 use board::hal::tcc::*;
-use board::hal::port::{PmEnabled, ModeWo1};
+use board::hal::port::{PmEnabled, ModeWo7};
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
     let led0 = board::led::LED0;
 
-    let ch = TCC2_CH1;
+    let ch = TCC0_CH3;
     let tcc = ch.periph();
     tcc.pm_set_enabled(true);
-    led0.mode_wo_1(tcc);
+    led0.mode_wo_7(tcc);
 
     println!("Running PWM");    
 
-    gclk::set_clk(gclk::GenericClock::TCC2_TC3, gclk::GenericClockGen::GClkGen2);
+    gclk::set_clk(gclk::GenericClock::TCC0_TCC1, gclk::GenericClockGen::GClkGen3);
 
 
-    tcc.with_per(|r| r.set_per(64));
-    tcc.with_cc(1, |r| r.set_cc(32));
-
+    tcc.with_per(|r| r.set_per(2000));
+    tcc.with_cc(ch.index(), |r| r.set_cc(0));
     tcc.with_wave(|r| r.set_wavegen(0x02));
-    tcc.with_ctrla(|r| r.set_cpten1(1).set_enable(1));
+    tcc.with_ctrla(|r| r.set_cpten(ch.index(), 1).set_enable(1));
 
     println!("Setup Complete");
 
-    loop {
-        //println!("period: {} cc: {}, count: {}", tcc.per().per(), tcc.cc(1).cc(), tcc.count().count());
-        board::delay(1000);
+    let max = 2000;
+    let step = 20;
+    let mut i: u32 = step; 
+    let mut dir: bool = true;
+    loop {        
+        tcc.with_cc(ch.index(), |r| r.set_cc(i));
+        
+        if i == max { dir = false } else if i == 0 { dir = true }
+        if dir {
+            i += step 
+        } else {
+            i -= step;
+        }
+        board::delay(10);
     }
 }
