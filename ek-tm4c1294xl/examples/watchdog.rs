1f181fc990320e8304717e66fb3caa335413da68
Jonathan Soo <jcsoo@agora.com>
2017-06-23 16:30:48 -0400

    TM4C Watchdog

---
+++ ek-tm4c1294xl/examples/watchdog.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[macro_use]
+extern crate ek_tm4c1294xl as board;
+
+use board::hal::watchdog::*;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    println!("Running WDOG Test");
+    board::delay(1000);
+    println!("Configuring Watchdog");
+    let w = WATCHDOG0;
+    w.sysctl_set_enabled(true);    
+    while !w.sysctl_ready() {}
+
+    w.set_load(Load(0x0500_0000));
+    w.set_ctl(Ctl(0).set_resen(1));
+    
+    for i in 0..5 {
+        // Refresh Watchdog
+        println!("{} {}", i, w.value().value());
+        w.set_load(Load(0x0500_0000));
+        board::delay(500);
+    }
+    println!("Waiting for watchdog timeout...");
+
+    loop {}
+}
