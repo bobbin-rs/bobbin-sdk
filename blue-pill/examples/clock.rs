4bf3033d595837f7c02eed9f1de77f0ca0a56bbe
Jonathan Soo <jcsoo@agora.com>
2017-07-25 22:03:34 -0400

    Add more clock utility functions

--- blue-pill/examples/clock.rs
+++ blue-pill/examples/clock.rs
@@ -11,6 +11,12 @@ pub extern "C" fn main() -> ! {
     println!("Clock Test");
     println!("Current Source: {:?}", clk.sysclk_src());
 
+    println!("LSI_ON: {} LSI_RDY: {}", clk.lsi_on(), clk.lsi_rdy());
+    println!("LSE_ON: {} LSE_RDY: {}", clk.lse_on(), clk.lse_rdy());
+    println!("HSI_ON: {} HSI_RDY: {}", clk.hsi_on(), clk.hsi_rdy());
+    println!("HSE_ON: {} HSE_RDY: {}", clk.hse_on(), clk.hse_rdy());
+    println!("PLL_ON: {} PLL_RDY: {}", clk.pll_on(), clk.pll_rdy());
+
     println!("LSI:      {:?}", clk.lsi());
     println!("LSE:      {:?}", clk.lse());
     println!("HSI:      {:?}", clk.hsi());
@@ -27,10 +33,55 @@ pub extern "C" fn main() -> ! {
     println!("RTCCLK:   {:?}", clk.rtcclk());
     println!("FLITFCLK: {:?}", clk.flitfclk());
     println!("IWDGCLK:  {:?}", clk.iwdgclk());
+
+    println!("");
+
+    println!("LSI: {:?}", clk.lsi());
+    println!(" turning LSI ON");
+    clk.set_lsi_on(true);
+    while !clk.lsi_rdy() {}
+    println!("LSI: {:?}", clk.lsi());
+    println!(" turning LSI OFF");
+    clk.set_lsi_on(false);
+    while clk.lsi_rdy() {}
+    println!("LSI: {:?}", clk.lsi());
+
+    println!("");    
+    println!("LSE: {:?}", clk.lse());
+    println!(" turning LSE ON");
+    clk.set_lse_on(true);
+    while !clk.lse_rdy() {}
+    println!("LSE: {:?}", clk.lse());
+    println!(" turning LSE OFF");
+    clk.set_lse_on(false);
+    while clk.lse_rdy() {}
+    println!("LSE: {:?}", clk.lse());
+
+    println!("");
+
+    println!("HSI: {:?}", clk.hsi());
+    println!(" turning HSI on");
+    clk.set_hsi_on(true);
+    while !clk.hsi_rdy() {}
+    println!("HSI: {:?}", clk.hsi());
+    println!(" turning HSI off");
+    clk.set_hsi_on(false);
+    while clk.hsi_rdy() {}
+    println!("HSI: {:?}", clk.hsi());
+    
+    println!("");
+    println!("RTC_EN: {}", clk.rtc_enabled());
+    println!(" enable RTC");
+    clk.set_rtc_enabled(true);
+    println!("RTC_EN: {}", clk.rtc_enabled());
+    println!(" disable RTC");
+    clk.set_rtc_enabled(false);
+    println!("RTC_EN: {}", clk.rtc_enabled());
     loop {}
 }
 
 use board::chip::rcc::RCC;
+use board::chip::pwr::PWR;
 
 pub type Hz = Option<u32>;
 
@@ -51,36 +102,118 @@ pub struct Clock {
 
 impl Clock {
     pub fn hsi(&self) -> Hz { 
-        match RCC.cr().hserdy() {
-            0b1 => HSI,
-            0b0 => None,
+        if self.hsi_rdy() { HSI } else { None } 
+    }
+
+    pub fn hsi_rdy(&self) -> bool {
+        match RCC.cr().hsirdy() {
+            0b0 => false,
+            0b1 => true,
             _ => unimplemented!(),
         }
     }
 
-    pub fn hse(&self) -> Hz {
-        match RCC.cr().hserdy() {
-            0b1 => self.hse_osc,
-            0b0 => None,
+    pub fn hsi_on(&self) -> bool {
+        match RCC.cr().hsion() {
+            0b0 => false,
+            0b1 => true,
             _ => unimplemented!(),
         }
     }
+
+    pub fn set_hsi_on(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        RCC.with_cr(|r| r.set_hsion(value) );
+        self
+    }
+
+    pub fn hse(&self) -> Hz { 
+        if self.hse_rdy() { self.hse_osc } else { None } 
+    }
+
+    pub fn hse_rdy(&self) -> bool {
+        match RCC.cr().hserdy() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn hse_on(&self) -> bool {
+        match RCC.cr().hseon() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn set_hse_on(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        RCC.with_cr(|r| r.set_hseon(value) );
+        self
+    }
+
 
     pub fn lsi(&self) -> Hz { 
+        if self.lsi_rdy() {
+            LSI
+        } else {
+            None
+        }
+    }
+
+    pub fn lsi_rdy(&self) -> bool {
         match RCC.csr().lsirdy() {
-            0b1 => LSI,
-            0b0 => None,
+            0b0 => false,
+            0b1 => true,
             _ => unimplemented!(),
         }
+    }
+
+    pub fn lsi_on(&self) -> bool {
+        match RCC.csr().lsion() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn set_lsi_on(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        RCC.with_csr(|r| r.set_lsion(value) );
+        self
     }
 
     pub fn lse(&self) -> Hz {
+        if self.lse_rdy() { LSE } else { None }
+    }
+
+    pub fn lse_rdy(&self) -> bool {
         match RCC.bdcr().lserdy() {
-            0b1 => LSE,
-            0b0 => None,
+            0b0 => false,
+            0b1 => true,
             _ => unimplemented!(),
         }
     }
+
+    pub fn lse_on(&self) -> bool {
+        match RCC.bdcr().lseon() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn set_lse_on(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        let p = RCC.apb1enr().pwren();
+        RCC.with_apb1enr(|r| r.set_pwren(1));
+        PWR.with_cr(|r| r.set_dbp(1));
+        RCC.with_bdcr(|r| r.set_lseon(value) );
+        PWR.with_cr(|r| r.set_dbp(0));
+        RCC.with_apb1enr(|r| r.set_pwren(p));
+        self
+    }    
 
     pub fn pll_mul(&self) -> u32 {
         match RCC.cfgr().pllmul() {
@@ -103,20 +236,45 @@ impl Clock {
             _ => unimplemented!(),
         }
     }
-    
-    pub fn pllclk(&self) -> Hz {
-        let cfgr = RCC.cfgr();
-        match cfgr.pllsrc() {
-            0b0 => self.hsi().map(|v| v >> 1),
-            0b1 => {
-                match cfgr.pllxtpre() {
-                    0b0 => self.hse(),
-                    0b1 => self.hse().map(|v| v >> 1),
-                    _ => unimplemented!(),
-                }    
-            },
+
+    pub fn pll_rdy(&self) -> bool {
+        match RCC.cr().pllrdy() {
+            0b0 => false,
+            0b1 => true,
             _ => unimplemented!(),
-        }.map(|v| v * self.pll_mul())        
+        }
+    }
+
+    pub fn pll_on(&self) -> bool {
+        match RCC.cr().pllon() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn set_pll_on(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        RCC.with_cr(|r| r.set_pllon(value) );
+        self
+    }
+
+    pub fn pllclk(&self) -> Hz {
+        match RCC.cr().pllrdy() {
+            0b0 => None,
+            0b1 => match RCC.cfgr().pllsrc() {
+                0b0 => self.hsi().map(|v| v >> 1),
+                0b1 => {
+                    match RCC.cfgr().pllxtpre() {
+                        0b0 => self.hse(),
+                        0b1 => self.hse().map(|v| v >> 1),
+                        _ => unimplemented!(),
+                    }    
+                },
+                _ => unimplemented!(),
+            }.map(|v| v * self.pll_mul()),
+            _ => unimplemented!(),
+        }
     }
 
     pub fn sysclk_src(&self) -> SysClock {
@@ -126,6 +284,11 @@ impl Clock {
             0b10 => SysClock::Pll,
             _ => unimplemented!()
         }
+    }
+
+    pub fn set_sysclk_src(&self, value: SysClock) -> &Self {
+        RCC.with_cfgr(|r| r.set_sw(value as u32));
+        self
     }
 
     pub fn sysclk(&self) -> Hz {
@@ -207,6 +370,25 @@ impl Clock {
 
     pub fn systick(&self) -> Hz {
         self.hclk().map(|v| v >> 3)
+    }
+
+    pub fn rtc_enabled(&self) -> bool {
+        match RCC.bdcr().rtcen() {
+            0b0 => false,
+            0b1 => true,
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn set_rtc_enabled(&self, value: bool) -> &Self {
+        let value = if value { 1 } else { 0 };
+        let p = RCC.apb1enr().pwren();
+        RCC.with_apb1enr(|r| r.set_pwren(1));
+        PWR.with_cr(|r| r.set_dbp(1));
+        RCC.with_bdcr(|r| r.set_rtcen(value) );
+        PWR.with_cr(|r| r.set_dbp(0));
+        RCC.with_apb1enr(|r| r.set_pwren(p));
+        self
     }
 
     pub fn rtcclk(&self) -> Hz {
