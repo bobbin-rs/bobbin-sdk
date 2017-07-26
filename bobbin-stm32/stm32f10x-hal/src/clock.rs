3002b33a7bbf11f3ef1ea92a0a17d255a0876a92
Jonathan Soo <jcsoo@agora.com>
2017-07-26 10:38:21 -0400

    Cleanup and make LSE configurable

--- bobbin-stm32/stm32f10x-hal/src/clock.rs
+++ bobbin-stm32/stm32f10x-hal/src/clock.rs
@@ -1,80 +1,13 @@
-use ::chip::rcc::{self, RCC};
+use ::chip::rcc::RCC;
 use ::chip::pwr::PWR;
-use ::chip::flash;
 use ::chip::usart::*;
 use ::chip::tim_gen::*;
 use ::chip::tim_adv::*;
 use ::chip::iwdg::*;
 use ::chip::wwdg::*;
-
-// 8Mhz external clock
-// System = 72Mhz
-// AHB = 72Mhz (Divide by 1)
-// APB1 = 36Mhz (Divide by 2)
-// APB2 = 72Mhz (Divide by 1)
-
-pub fn enable_pll_external_mode() {
-    let rcc = rcc::RCC;
-    let flash = flash::FLASH;
-    
-    // Enable internal high-speed oscillator.
-    rcc.with_cr(|r| r.set_hsion(1));
-
-    // Wait for HSI Ready
-    while rcc.cr().hsirdy() == 0 {}
-
-    // Select HSI as SYSCLK source. 
-    rcc.with_cfgr(|r| r.set_sw(0));
-
-    // Enable external high-speed oscillator 8MHz.
-    rcc.with_cr(|r| r.set_hseon(1));
-
-    // Wait for HSE Ready
-    while rcc.cr().hserdy() == 0 {}
-
-    // Set prescalers for AHB, ADC, ABP1, ABP2
-
-    // HPRE = HPRE_DIV_NONE
-    // PPRE1 = PPRE_DIV_2 (0b100)
-    // PPRE2 = PPRE_DIV_NONE (0b000)
-    // PLLSRC = PREDIV1 (0b1)
-    // PLLMUL = x9 (0b0111)
-    // MCO = PLL/2 (0b0111)
-    rcc.with_cfgr(|r| 
-        r.set_hpre(0)
-            .set_ppre1(0b100)
-            .set_ppre2(0b000)
-            .set_pllsrc(1)
-            .set_pllmul(0b0111)
-    );
-
-    // Enable PLL oscillator and wait for it to stabilize.
-    rcc.with_cr(|r| r.set_pllon(1));
-    
-    // Wait for PLL Ready
-    while rcc.cr().pllrdy() == 0 {}
-
-    // Configure flash settings.
-    // PRFTBE = enabled (0b1)
-    // LATENCY = 2 Wait States (0b010)
-    flash.with_acr(|r| r.set_latency(0b010));
-    
-    // Select PLL as SYSCLK source.
-
-    rcc.with_cfgr(|r| r.set_sw(0b10));
-    
-    // Wait for PLL to be selected
-    while rcc.cfgr().sws() != 0b10 {}
-    
-    // Disable internal high-speed oscillator.        
-    rcc.with_cr(|r| r.set_hsion(0));
-}
-
-
 pub type Hz = Option<u32>;
 
 pub const LSI: Hz = Some(40_000);
-pub const LSE: Hz = Some(32768);
 pub const HSI: Hz = Some(8_000_000);
 
 #[derive(Debug, PartialEq)]
@@ -132,6 +65,7 @@ pub enum PPre2 {
 
 pub struct ClockTree {
     pub hse_osc: Hz,
+    pub lse_osc: Hz,
 }
 
 impl ClockTree {
@@ -236,7 +170,7 @@ impl ClockTree {
     }
 
     pub fn lse(&self) -> Hz {
-        if self.lse_rdy() { LSE } else { None }
+        if self.lse_rdy() { self.lse_osc } else { None }
     }
 
     pub fn lse_rdy(&self) -> bool {
