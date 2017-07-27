f74760240c835dcbda595a90b3b4e6ff8cff1d32
Jonathan Soo <jcsoo@agora.com>
2017-07-27 16:48:09 -0400

    sync clock source

--- frdm-k64f/examples/clock.rs
+++ frdm-k64f/examples/clock.rs
@@ -54,10 +54,13 @@ pub extern "C" fn main() -> ! {
 
     println!("Change Bus Divider");
     // println!("CLKDIV1: {:?}", SIM.clkdiv1());
+    println!("MCG_S: {:?}", MCG.s());
     board::delay(100);
     board::console::disable();
     MCG.with_c1(|r| r.set_clks(0b10));
+    while MCG.s().clkst() != 0b10 {}
     board::console::enable();
+    println!("MCG_S: {:?}", MCG.s());
     // println!("CLKDIV1: {:?}", SIM.clkdiv1());
     println!("CORE:           {:?}", clk.core());
     println!("BUS:           {:?}", clk.bus());
