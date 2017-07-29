ca33207927aa12ef48b57f0ff73b589bf5eb994a
Jonathan Soo <jcsoo@agora.com>
2017-07-28 22:31:28 -0400

    Implement dynamic clock tree for TM4C

--- bobbin-ti/tm4c129x-hal/src/clock.rs
+++ bobbin-ti/tm4c129x-hal/src/clock.rs
@@ -34,16 +34,78 @@ pub const KHZ: Hz = Some(1_000);
 pub const MHZ: Hz = Some(1_000_000);
 pub const GHZ: Hz = Some(1_000_000_000);
 
+pub const PIOSC: Hz = Some(16_000_000);
+pub const LFIOSC: Hz = Some(33_000);
+
 pub enum ExternalSource {
     Osc(u32),
     Clock(u32),
 }
 
 pub trait ClockTree {
-    fn mosc(&self) -> Hz;
-    fn rtosc(&self) -> Hz;
     fn sysclk(&self) -> Hz;
 }
+
+pub struct DynamicClockTree {
+    pub osc: Hz,
+    pub xosc: Hz,
+}
+
+impl DynamicClockTree {
+    // Fundamental Clock Sources
+    pub fn mosc(&self) -> Hz {
+        self.osc
+    }
+    pub fn rtcosc(&self) -> Hz {
+        self.xosc
+    }
+    pub fn piosc(&self) -> Hz { 
+        PIOSC 
+    }
+    pub fn lfiosc(&self) -> Hz {
+        LFIOSC
+    }
+    // Internal Clocks
+    pub fn oscclk(&self) -> Hz {
+        match SYSCTL.rsclkcfg().oscsrc() {
+            0 => self.piosc(),
+            2 => self.lfiosc(),
+            3 => self.mosc(),
+            4 => self.rtcosc(),
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn vco(&self) -> Hz {
+        let pllstat = SYSCTL.pllstat();
+        if pllstat.lock() == 0 {
+            return None
+        }
+        let rsclkcfg = SYSCTL.rsclkcfg();
+        let pllfreq0 = SYSCTL.pllfreq0();
+        let pllfreq1 = SYSCTL.pllfreq1();
+        let (q, n) = (pllfreq1.q(), pllfreq1.n());
+        let f_in = match rsclkcfg.pllsrc() {
+            0 => self.piosc(),
+            3 => self.mosc(),
+            _ => unimplemented!(),
+        }.map(|v| v / ((q + 1)  * (n + 1)));
+        let mdiv = pllfreq0.mint() + pllfreq0.mfrac() / 1024;
+        f_in.map(|v| v * mdiv)
+    }
+}
+
+impl ClockTree for DynamicClockTree {
+    fn sysclk(&self) -> Hz {
+        let rsclkcfg = SYSCTL.rsclkcfg();
+        if rsclkcfg.usepll() == 0 {
+            self.oscclk().map(|v| v / (SYSCTL.rsclkcfg().osysdiv() + 1))    
+        } else {
+            self.vco().map(|v| v / (SYSCTL.rsclkcfg().psysdiv() + 1))
+        }
+    }
+}
+
 
 pub trait Clock<T: ClockTree + ?Sized> {
     fn clock(&self, t: &T) -> Hz;
