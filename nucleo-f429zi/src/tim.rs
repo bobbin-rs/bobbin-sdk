a1a276d0956c3948b6c9bde439dd210d65cb18fc
Jonathan Soo <jcsoo@agora.com>
2017-07-31 17:43:54 -0400

    Update clocktree for STM32F429

--- nucleo-f429zi/src/tim.rs
+++ nucleo-f429zi/src/tim.rs
@@ -1,18 +1,19 @@
 use hal::tim::*;
+use hal::clock::Clock;
+use clock::CLK;
 
 pub const TIM: Tim14 = TIM14;
 pub const TIM_PRESCALE: u16 = 41999;
 
 // PLL Mode with 8Mhz External Oscillator
 //   168Mhz System Clock
-//   84Mhz AHB Clock
-//   42Mhz APB1 Clock
-//   84Mhz APB2 Clock
-//   9Mhz SysTick clock
-// TIM14 is APB1 Clock = 42MHz
+//   168Mhz AHB Clock
+//   84Mhz APB1 Clock
+//   168Mhz APB2 Clock
+// TIM14 is APB1 Clock = 84MHz
 
-// Clock at 42MHz
-// Divide by 42,000 => 2khz
+// Clock at 84MHz
+// Divide by 2KHz = 48,000
 // Set auto_reload to ms x 2
 
 pub fn init() {
@@ -20,5 +21,5 @@ pub fn init() {
 }
 
 pub fn delay(ms: u32) {    
-    TIM.delay(ms << 1, TIM_PRESCALE);    
+    TIM.delay(ms << 1, ((TIM.clock(&CLK).unwrap() / 2000) - 1) as u16);    
 }
