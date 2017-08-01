e6c268a8b7200dece9d5d8e8b476b001b91b7540
Jonathan Soo <jcsoo@agora.com>
2017-07-31 21:43:06 -0400

    Add clock tree for F302R8

--- bobbin-stm32/stm32f302x-hal/src/clock.rs
+++ bobbin-stm32/stm32f302x-hal/src/clock.rs
@@ -1,6 +1,12 @@
-use ::chip::rcc;
+use ::chip::rcc::{self, RCC};
 use ::chip::flash;
-//use ::chip::pwr;
+use ::chip::usart::*;
+use ::chip::tim_gen::*;
+use ::chip::tim_adv::*;
+use ::chip::tim_bas::*;
+use ::chip::rcc::En;
+
+use core::fmt;
 
 // Clock Configuration on Reset
 //   System Clock = HSI = 8Mhz
@@ -79,5 +85,264 @@ pub fn set_hsi_enabled(value: bool) {
     rcc::RCC.with_cr(|r| r.set_hsion(value));
     if value == 1 {
         while rcc::RCC.cr().hsirdy() == 0 {}
+    }
+}
+
+
+pub const HSI: Hz = Some(8_000_000);
+pub const LSI: Hz = Some(40_000);
+
+pub type Hz = Option<u32>;
+
+pub trait ClockTree {
+    fn hsi(&self) -> Hz { HSI }
+    fn lsi(&self) -> Hz { LSI }
+    fn hse(&self) -> Hz;
+    fn lse(&self) -> Hz;
+    fn pllclk(&self) -> Hz;
+    fn sysclk(&self) -> Hz;
+    fn hclk(&self) -> Hz;
+    fn systick(&self) -> Hz;
+    fn pclk1(&self) -> Hz;
+    fn tim_pclk1(&self) -> Hz;
+    fn pclk2(&self) -> Hz;
+    fn tim_pclk2(&self) -> Hz;
+}
+
+pub trait Clock<T: ClockTree> {
+    fn clock(&self, t: &T) -> Hz;
+}
+
+pub struct DynamicClock {
+    pub hse_osc: Hz,
+    pub lse_osc: Hz,
+}
+
+impl ClockTree for DynamicClock {
+    fn hse(&self) -> Hz {
+        self.hse_osc
+    }
+
+    fn lse(&self) -> Hz {
+        self.lse_osc
+    }
+
+    fn pllclk(&self) -> Hz {
+        let cfgr = RCC.cfgr();
+        let cfgr2 = RCC.cfgr2();
+        let prediv = cfgr2.prediv() + 1;
+        let pllmul = match cfgr.pllmul() {
+            0b1111 => 16,
+            m @ _ => m + 2,
+        };
+        match cfgr.pllsrc() {
+            0b00 => self.hsi().map(|v| v >> 1),
+            0b01 => self.hsi().map(|v| v * pllmul / prediv),
+            0b10 => self.hse().map(|v| v * pllmul / prediv),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn sysclk(&self) -> Hz {
+        match RCC.cfgr().sws() {
+            0b00 => self.hsi(),
+            0b01 => self.hse(),
+            0b10 => self.pllclk(),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn hclk(&self) -> Hz {
+        let shift = match RCC.cfgr().hpre() {
+            0b0000 ... 0b111 => 0,
+            0b1000 => 1,
+            0b1001 => 2,
+            0b1010 => 3,
+            0b1011 => 4,
+            // NOTE: Divide by 32 is skipped
+            0b1100 => 6,
+            0b1101 => 7,
+            0b1110 => 8,
+            0b1111 => 9,
+            _ => unimplemented!(),
+        };
+        self.sysclk().map(|v| v >> shift)
+    }
+
+    fn systick(&self) -> Hz {
+        self.hclk().map(|v| v >> 3)
+    }
+
+    fn pclk1(&self) -> Hz {
+        let shift = match RCC.cfgr().ppre1() {
+            0b000 ... 0b011 => 0,
+            0b100 => 1,
+            0b101 => 2,
+            0b110 => 3,
+            0b111 => 4,
+            _ => unimplemented!(),
+        };
+        self.hclk().map(|v| v >> shift)
+    }
+
+    fn tim_pclk1(&self) -> Hz {
+        match RCC.cfgr().ppre1() {
+            0b000 ... 0b011 => self.pclk1(),
+            _ => self.pclk1().map(|v| v << 1),
+        }
+    }
+
+    fn pclk2(&self) -> Hz {
+        let shift = match RCC.cfgr().ppre2() {
+            0b000 ... 0b011 => 0,
+            0b100 => 1,
+            0b101 => 2,
+            0b110 => 3,
+            0b111 => 4,
+            _ => unimplemented!(),
+        };
+        self.hclk().map(|v| v >> shift)
+    }
+
+    fn tim_pclk2(&self) -> Hz {
+        match RCC.cfgr().ppre2() {
+            0b000 ... 0b011 => self.pclk2(),
+            _ => self.pclk2().map(|v| v << 1),
+        }
+    }    
+}
+
+impl fmt::Debug for DynamicClock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[DynamicCLock")?;
+        write!(f, " LSI={:?}", self.lsi())?;
+        write!(f, " HSI={:?}", self.hsi())?;
+        write!(f, " LSE={:?}", self.lse())?;
+        write!(f, " HSE={:?}", self.hse())?;
+        write!(f, " PLLCLK={:?}", self.pllclk())?;
+        write!(f, " SYSCLK={:?}", self.sysclk())?;
+        write!(f, " HCLK={:?}", self.hclk())?;
+        write!(f, " PCLK1={:?}", self.pclk1())?;
+        write!(f, " TIM_PCLK1={:?}", self.tim_pclk1())?;
+        write!(f, " PCLK2={:?}", self.pclk2())?;
+        write!(f, " TIM_PCLK2={:?}", self.tim_pclk2())?;
+        write!(f, "]")?;
+        Ok(())
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Usart1 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            match RCC.cfgr3().usart1sw() {
+                0b00 => t.pclk2(),
+                0b01 => t.sysclk(),
+                0b10 => t.lse(),
+                0b11 => t.hsi(),
+                _ => unimplemented!(),
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Usart2 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            match RCC.cfgr3().usart2sw() {
+                0b00 => t.pclk1(),
+                0b01 => t.sysclk(),
+                0b10 => t.lse(),
+                0b11 => t.hsi(),
+                _ => unimplemented!(),
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Usart3 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            match RCC.cfgr3().usart3sw() {
+                0b00 => t.pclk1(),
+                0b01 => t.sysclk(),
+                0b10 => t.lse(),
+                0b11 => t.hsi(),
+                _ => unimplemented!(),
+            }
+        } else {
+            None
+        }
+    }
+}
+
+// // NOTE: Clock Tree Diagram differs from RCC_CFGR3 description,
+// // using clock tree as reference
+
+impl<T: ClockTree> Clock<T> for Tim1 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            match RCC.cfgr3().tim1sw() {
+                0b00 => t.tim_pclk2(),
+                0b01 => t.pllclk().map(|v| v << 1),
+                _ => unimplemented!(),
+            }
+        } else {
+            None
+        }
+    }
+}
+
+
+impl<T: ClockTree> Clock<T> for Tim2 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            t.tim_pclk1()
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Tim6 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            t.tim_pclk1()
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Tim15 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            t.tim_pclk2()
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Tim16 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            t.tim_pclk2()
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ClockTree> Clock<T> for Tim17 {
+    fn clock(&self, t: &T) -> Hz {
+        if self.en() != 0 {
+            t.tim_pclk2()
+        } else {
+            None
+        }
     }
 }
