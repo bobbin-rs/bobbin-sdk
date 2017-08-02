0b6a2bc00b56f0c80c230997dce5da2615848209
Jonathan Soo <jcsoo@agora.com>
2017-08-02 17:01:11 -0400

    FTM Timer Trait

--- evb-s32k144/examples/ftm.rs
+++ evb-s32k144/examples/ftm.rs
@@ -4,70 +4,30 @@
 #[macro_use]
 extern crate evb_s32k144 as board;
 
-use board::hal::port::*;
-//use board::hal::gpio::GpioExt;
 use board::hal::ftm::*;
 use board::hal::pcc;
+use board::hal::clock::Clock;
+use board::clock::CLK;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
     
-    let led0 = board::led::LED0;
-
-    println!("FTM Test");
-    
+    println!("FTM Test");    
     let ch = FTM0_CH2;
     let t0 = ch.periph();
 
-    led0.mode_ftm(&ch);
+    t0
+        .pcc_set_clock_source(pcc::ClockSource::SOSCDIV2)
+        .pcc_set_enabled(true);
 
+    let prescale = 128;
+    let reload = t0.clock(&CLK).unwrap() / prescale;
 
+    println!("{} / {}", reload, prescale);
 
-    t0.pcc_set_enabled(true);
-    t0.pcc_set_clock_source(pcc::ClockSource::SPLLDIV2);
-    t0.set_prescale(Prescale::Div64);
-    t0.set_modulo(2048);
-    t0.set_count(0);
-
-    // Setup Edge PWM    
-    
-    ch.set_pwmen(true);
-    ch.with_csc(|r| r.set_msb(1).set_msa(0).set_elsb(0).set_elsa(1));
-    ch.set_value(1024);
-
-    t0.set_clock(ClockSource::SystemClk);
-
-    println!("Clock Enabled");
-
-        
-    let max = 2000;
-    let step = 20;
-    let mut i: u32 = step; 
-    let mut dir: bool = true;
-    loop {        
-        ch.set_value(i as u16);
-        
-        if i == max { dir = false } else if i == 0 { dir = true }
-        if dir {
-            i += step 
-        } else {
-            i -= step;
-        }
-        board::delay(10);
+    loop {
+        println!("tick");
+        t0.delay(reload as u16, prescale as u16);
     }
-
-    // let mut n = 0;    
-    // loop {
-    //     while !t0.timer_overflow() {}
-    //     t0.clr_timer_overflow();
-    //     if n == 1_000_000 {
-    //         led0.toggle_output();
-    //         n = 0;
-    //     }
-
-    //     n += 1;
-    //     //board::delay(1000);
-    // }
-
 }
