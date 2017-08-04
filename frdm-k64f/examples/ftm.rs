de7862d6c67456b6911429f1e35e8f67ca976cf4
Jonathan Soo <jcsoo@agora.com>
2017-08-04 15:32:00 -0400

    tweak FTM timer for K64

--- frdm-k64f/examples/ftm.rs
+++ frdm-k64f/examples/ftm.rs
@@ -21,18 +21,18 @@ pub extern "C" fn main() -> ! {
         .sim_set_enabled(true);
 
     println!("t0: {:?}", t0.clock(&CLK));
-    let prescale = 128;
+    let prescale = 1;
     let period = ((t0.clock(&CLK).unwrap() / prescale) / 1000) as u16;
 
     println!("{} / {}", period, prescale);
 
     t0.set_prescale(prescale as u16);
-    ch
-        .with_csc(|r| r.set_chie(0).set_msb(0).set_msa(1).set_elsb(0).set_elsa(0))
-        .set_compare(period >> 1);
+    // ch
+    //     .with_csc(|r| r.set_chie(0).set_msb(0).set_msa(1).set_elsb(0).set_elsa(0))
+    //     .set_compare(period >> 1);
 
     loop {
-        t0.delay(1000);
+        t0.delay(period);
         println!("tick");
     }
 
