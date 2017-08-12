fcfd29b5018eefa7a7527e4d8ab8e44bb69a8ff1
Jonathan Soo <jcsoo@agora.com>
2017-08-12 12:28:13 -0400

    Add arduino pins to EVB-S32K144

--- evb-s32k144/src/pin.rs
+++ evb-s32k144/src/pin.rs
@@ -1,2 +1,29 @@
 use chip::port::*;
 
+pub const POT0: Ptc14 = PTC14;
+
+pub const A0: Ptd4 = PTD4;
+pub const A1: Ptb12 = PTB12;
+pub const A2: Ptb0 = PTB0;
+pub const A3: Ptb1 = PTB1;
+pub const A4: Pta6 = PTA6;
+pub const A5: Ptc0 = PTC0;
+
+pub const D0: Pta11 = PTA11;
+pub const D1: Pta17 = PTA17;
+pub const D2: Ptd10 = PTD10;
+pub const D3: Ptd11 = PTD11;
+pub const D4: Ptd12 = PTD12;
+pub const D5: Ptd5 = PTD5;
+pub const D6: Ptd3 = PTD3;
+pub const D7: Pte3 = PTE3;
+
+pub const D8: Ptd15 = PTD15;
+pub const D9: Ptd16 = PTD16;
+pub const D10: Ptd0 = PTD0;
+pub const D11: Pte8 = PTE8;
+pub const D12: Ptc12 = PTC12;
+pub const D13: Ptc13 = PTC13;
+pub const D14: Pte7 = PTE7;
+pub const D15: Pta14 = PTA14;
+
