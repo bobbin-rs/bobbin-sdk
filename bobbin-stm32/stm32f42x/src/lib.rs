f6e1ecd726e563ad0abfad393b8e45b101294ab5
Jonathan Soo <jcsoo@agora.com>
2017-08-14 16:27:30 -0400

    Export bobbin_common

--- bobbin-stm32/stm32f42x/src/lib.rs
+++ bobbin-stm32/stm32f42x/src/lib.rs
@@ -2,9 +2,10 @@
 #![feature(compiler_builtins_lib)]
 
 extern crate compiler_builtins;
-extern crate bobbin_common;
+pub extern crate bobbin_common;
 extern crate bobbin_cortexm;
 extern crate stm32_common;
 
+pub use bobbin_common as common;
 pub mod chip;
 pub mod hal;
