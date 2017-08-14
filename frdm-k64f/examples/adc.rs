f9b940f6165145a01d4168ae5e8496ff871f373f
Jonathan Soo <jcsoo@agora.com>
2017-08-14 15:24:31 -0400

    Use ADC traits

--- frdm-k64f/examples/adc.rs
+++ frdm-k64f/examples/adc.rs
@@ -4,6 +4,7 @@
 #[macro_use]
 extern crate frdm_k64f as board;
 
+use board::common::bits::*;
 use board::pin::*;
 use board::hal::sim::*;
 use board::hal::adc::*;
@@ -18,15 +19,16 @@ pub extern "C" fn main() -> ! {
     let a1 = A1; // PTB3 / ADC0_SE13
     
     let adc0 = ADC0;
-    let ch0 = ADC0_CH0;
+    let ch12 = ADC0_CH12;
+    let ch13 = ADC0_CH13;
 
     println!("Setting up Pins");
 
     a0.port().sim_set_enabled(true);
-    a0.mode_adc_se12(&adc0);
+    a0.mode_adc(&ch12);
 
     a1.port().sim_set_enabled(true);
-    a1.mode_adc_se13(&adc0);
+    a1.mode_adc(&ch13);
 
     println!("Setting up ADCs");
 
@@ -37,13 +39,15 @@ pub extern "C" fn main() -> ! {
     println!("Initialization Complete");
 
     loop {
-        ch0.set_input_channel(12.into());        
-        while !ch0.conversion_complete() {}
-        let v0 = ch0.result();
+        let v0: U16 = ch12.analog_read();
+        let v1: U16 = ch13.analog_read();
+        // ch0.set_input_channel(12.into());        
+        // while !ch0.conversion_complete() {}
+        // let v0 = ch0.result();
 
-        ch0.set_input_channel(13.into());        
-        while !ch0.conversion_complete() {}
-        let v1 = ch0.result();
+        // ch0.set_input_channel(13.into());        
+        // while !ch0.conversion_complete() {}
+        // let v1 = ch0.result();
 
         println!("{} {}", v0, v1);
         board::delay(1000);
