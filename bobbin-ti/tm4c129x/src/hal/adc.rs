e0620a7a2e5edd9f9733f42567a222d898eaab71
Jonathan Soo <jcsoo@agora.com>
2017-08-14 16:16:07 -0400

    Add basic AnalogRead support

--- bobbin-ti/tm4c129x/src/hal/adc.rs
+++ bobbin-ti/tm4c129x/src/hal/adc.rs
@@ -1,3 +1,5 @@
+use bobbin_common::bits::*;
+pub use bobbin_common::analog::AnalogRead;
 pub use ::chip::adc::*;
 pub use super::sysctl::SysctlEnabled;
 pub use super::gpio::ModeAin;
@@ -9,5 +11,20 @@ pub trait AdcExt {
 impl<T> AdcExt for Periph<T> {
     fn init(&self) -> &Self {
         self
+    }
+}
+
+impl<P, T> AnalogRead<U12> for Channel<P, T> {
+    fn analog_read(&self) -> U12 {
+        self.periph
+            .with_actss(|r| r.set_asen(0, 0))
+            .with_ssctl(0, |r| r.set_ts(0, 0).set_ie(0, 1).set_end(0, 1))
+            .with_ssmux(0, |r| r.set_mux(0, self.index()))
+            .with_actss(|r| r.set_asen(0, 1))
+            .set_pssi(|r| r.set_ss(0, 1));
+        while self.periph.ris().inr(0) == 0 {}
+        self.periph
+            .set_isc(|r| r.set_in(0, 1))
+            .ssfifo(0).data()
     }
 }
--- bobbin-ti/tm4c129x/src/lib.rs
+++ bobbin-ti/tm4c129x/src/lib.rs
@@ -2,8 +2,9 @@
 #![feature(compiler_builtins_lib)]
 
 extern crate compiler_builtins;
-extern crate bobbin_common;
+pub extern crate bobbin_common;
 extern crate bobbin_cortexm;
 
+pub use bobbin_common as common;
 pub mod chip;
 pub mod hal;
