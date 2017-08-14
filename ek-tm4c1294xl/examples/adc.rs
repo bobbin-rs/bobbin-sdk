b179b43db0670a4baa10880bb1d2662a12659070
Jonathan Soo <jcsoo@agora.com>
2017-08-14 16:16:50 -0400

    Use trait for ADC example

--- ek-tm4c1294xl/examples/adc.rs
+++ ek-tm4c1294xl/examples/adc.rs
@@ -22,22 +22,8 @@ pub extern "C" fn main() -> ! {
     a0.port().sysctl_set_enabled(true);
     a0.mode_ain(&ch0);
 
-    // Disable Sequencer 0
-    adc0.with_actss(|r| r.set_asen(0, 0));
-    // Setup Sequencer 0 to read temperature sensor 
-    adc0.with_ssctl(0, |r| r.set_ts(0, 0).set_ie(0, 1).set_end(0, 1));
-    // Enable Sequencer 0
-    adc0.with_actss(|r| r.set_asen(0, 1));
-
-    loop {        
-        // Initiate Sequencer 0
-        adc0.set_pssi(|r| r.set_ss(0, 1));
-        // Wait for Sequencer 0 Complete
-        while adc0.ris().inr(0) == 0 {}
-        // Clear Flag
-        adc0.set_isc(|r| r.set_in(0, 1));
-        // Read FIFO
-        println!("{}", adc0.ssfifo(0).data().value());
+    loop {
+        println!("{}", ch0.analog_read());
 
         board::delay(1000);
     }
