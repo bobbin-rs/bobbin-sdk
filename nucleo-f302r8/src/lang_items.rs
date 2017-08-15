dab6985f48223ed819dae8a5d9c7badd9189fac9
Jonathan Soo <jcsoo@agora.com>
2017-08-15 15:22:33 -0400

    Add ADC for L031x

--- nucleo-f302r8/src/lang_items.rs
+++ nucleo-f302r8/src/lang_items.rs
@@ -1,9 +1,10 @@
 use core::fmt::Arguments;
 
 #[lang = "panic_fmt"]
-unsafe extern "C" fn panic_fmt(_msg: Arguments,
-                               _file: &'static str,
-                               _line: u32)
+unsafe extern "C" fn panic_fmt(msg: Arguments,
+                               file: &'static str,
+                               line: u32)
                                -> ! {
+    println!("[PANIC] {}, {} at {}", msg, file, line);
     loop {}
 }
