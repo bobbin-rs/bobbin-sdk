af234ada8ec370ef987268f28fc46062323dd250
Jonathan Soo <jcsoo@agora.com>
2017-08-20 18:30:31 -0700

    Use potentiometer for example

--- evb-s32k144/examples/led_pwm.rs
+++ evb-s32k144/examples/led_pwm.rs
@@ -4,6 +4,8 @@
 #[macro_use]
 extern crate evb_s32k144 as board;
 
+use board::common::bits::*;
+use board::pot::AnalogRead;
 // use board::led::LedPwmSet;
 
 #[no_mangle]
@@ -15,20 +17,29 @@ pub extern "C" fn main() -> ! {
     let led = board::led::LED_RGB;
     led.init();
 
-    let max = 1000u16;
-    let step = 10u16;
-    let mut i: u16 = step; 
-    let mut dir: bool = true;
     led.start();
-    loop {       
-        led.set((i, 1000-i, 0));
-        
-        if i == max { dir = false } else if i == 0 { dir = true; board::delay(1000); }
-        if dir {
-            i += step 
-        } else {
-            i -= step;
-        }
+    loop {
+        let i: U10 = ::board::pot::POT0.analog_read();
+        let i: u16 = i.into();
+        led.set((i, i, i));
+        // led.set((i, 1024-i, 0));
         board::delay(10);
     }
+
+    // let max = 1000u16;
+    // let step = 10u16;
+    // let mut i: u16 = step; 
+    // let mut dir: bool = true;
+    // led.start();
+    // loop {       
+    //     led.set((i, 1000-i, 0));
+        
+    //     if i >= max { dir = false } else if i == 0 { dir = true; board::delay(1000); }
+    //     if dir {
+    //         i += step 
+    //     } else {
+    //         i -= step;
+    //     }
+    //     board::delay(10);
+    // }
 }
