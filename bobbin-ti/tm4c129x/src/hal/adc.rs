fc0595c955d8461dff43305ea8dae1afdd1124dd
Jonathan Soo <jcsoo@agora.com>
2017-08-22 11:44:08 -0400

    Update ADC implementation

--- bobbin-ti/tm4c129x/src/hal/adc.rs
+++ bobbin-ti/tm4c129x/src/hal/adc.rs
@@ -15,16 +15,22 @@ impl<T> AdcExt for Periph<T> {
 }
 
 impl<P, T> AnalogRead<U12> for Channel<P, T> {
-    fn analog_read(&self) -> U12 {
+    fn start(&self) -> &Self {
         self.periph
             .with_actss(|r| r.set_asen(0, 0))
             .with_ssctl(0, |r| r.set_ts(0, 0).set_ie(0, 1).set_end(0, 1))
             .with_ssmux(0, |r| r.set_mux(0, self.index()))
             .with_actss(|r| r.set_asen(0, 1))
-            .set_pssi(|r| r.set_ss(0, 1));
-        while self.periph.ris().inr(0) == 0 {}
-        self.periph
             .set_isc(|r| r.set_in(0, 1))
-            .ssfifo(0).data()
+            .set_pssi(|r| r.set_ss(0, 1));
+        self
+    }
+
+    fn is_complete(&self) -> bool {
+        self.periph.ris().inr(0) != 0
+    }
+
+    fn read(&self) -> U12 {
+        self.periph.ssfifo(0).data()
     }
 }
