96a97db29378973e73cddc972d62ba2ed40011ca
Jonathan Soo <jcsoo@agora.com>
2017-08-29 12:30:20 -0400

    Update IRQ example

--- evb-s32k144/examples/irq.rs
+++ evb-s32k144/examples/irq.rs
@@ -6,12 +6,13 @@
 extern crate evb_s32k144 as board;
 
 use board::hal::nvic;
-use board::hal::port::{PORTC, GpioPin, PinExt, RegisterPortHandler, HandlePort, IrqPort};
-use board::hal::gpio::{DigitalInput, DigitalOutput};
-use board::hal::rtc::{RTC, PccEnabled};
+use board::common::{IrqNum, SetHandler};
+use board::hal::port::*;
+use board::hal::gpio::*;
+// use board::hal::rtc::{RTC, PccEnabled};
 
-use board::btn::{BTN0, BTN1};
-use board::led::{LED0, LED1};
+use board::btn::*;
+use board::led::*;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
@@ -19,67 +20,73 @@ pub extern "C" fn main() -> ! {
 
     let p0 = PORTC;
 
-    let b0 = BTN0;
-    let b1 = BTN1;
+    let b0 = BTN0.port_pin();
+    let b1 = BTN1.port_pin();
 
     let l0 = LED0;
-    let l1 = LED1;
-
+    let l1 = LED1;    
 
     println!("IRQ Test");
 
-    let n = nvic::NVIC;
+    println!("PORTC IRQ: {:?}", p0.irq_port());
 
     b0.set_irqc(board::hal::port::InterruptConfig::IrqEitherEdge);
     b1.set_irqc(board::hal::port::InterruptConfig::IrqEitherEdge);
 
     l0.set_output(true);
-    l1.set_output(true);    
-    let h = BtnHandler {};
-    let _guard = p0.register_port_handler(&h);
-    
-    println!("SYSTICK_CSR: {:?}", board::chip::systick::SYSTICK.csr());
+    l1.set_output(true);
 
-    p0.irq_port().set_enabled(true);
-    while !p0.irq_port().is_enabled() {}
-    // board::delay(10);    
-    // board::chip::scb::SCB.with_scr(|r| r.set_sevonpend(true));
+    extern "C"
+    fn b0_handler() {
+        if BTN0.port_pin().isf() {
+            BTN0.port_pin().clr_isf();
+            println!("B0 IRQ");
+        }
+        if BTN1.port_pin().isf() {
+            BTN1.port_pin().clr_isf();
+            println!("B1 IRQ");
+        }        
+    }
+
+    p0.irq_port().set_handler(Some(b0_handler));
     
-    let mut count = 0;    
-    // Disable Interrupt Mask
-    // unsafe { asm!("CPSID i") }
+    nvic::set_enabled(p0.irq_port().irq_num() as usize, true);
 
     println!("starting loop");
+    let mut count = 0;
     loop {        
         if b0.isf() {
             b0.clr_isf();            
             println!("B0 {}", count);
-            p0.irq_port().set_pending(false);
+            count = 0;
+            // p0.irq_port().set_pending(false);
         }
         if b1.isf() {
             b1.clr_isf();
             println!("B1 {}", count);
-            p0.irq_port().set_pending(false);
+            count = 0;
+            // p0.irq_port().set_pending(false);
         }        
-        l0.set_output(b0.gpio_pin().input());
-        l1.set_output(b1.gpio_pin().input());              
-        unsafe { asm!("wfi") }
+        l0.set_output(!b0.gpio_pin().input());
+        l1.set_output(!b1.gpio_pin().input());
+        count += 1; 
+        // unsafe { asm!("wfi") }
     }
 }
 
-pub struct BtnHandler {}
+// pub struct BtnHandler {}
 
-impl HandlePort for BtnHandler {
-    fn handle_port(&self) {
-        let b0 = BTN0;
-        let b1 = BTN1;
-        if b0.isf() {
-            b0.clr_isf();
-            println!("B0");
-        }
-        if b1.isf() {
-            b1.clr_isf();
-            println!("B1");
-        }        
-    }
-}
+// impl HandlePort for BtnHandler {
+//     fn handle_port(&self) {
+//         let b0 = BTN0;
+//         let b1 = BTN1;
+//         if b0.isf() {
+//             b0.clr_isf();
+//             println!("B0");
+//         }
+//         if b1.isf() {
+//             b1.clr_isf();
+//             println!("B1");
+//         }        
+//     }
+// }
