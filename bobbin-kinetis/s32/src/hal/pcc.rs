9320cef4e125c21c0441a31e7fd4c3552e9c349f
Jonathan Soo <jcsoo@agora.com>
2017-08-31 17:05:30 -0400

    Add documentation to the PCC module

--- bobbin-kinetis/s32/src/hal/pcc.rs
+++ bobbin-kinetis/s32/src/hal/pcc.rs
@@ -1,6 +1,6 @@
 use ::chip::pcc::*;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum ClockSource {
     Disabled = 0b000,
     SOSCDIV2 = 0b001,
@@ -9,10 +9,15 @@ pub enum ClockSource {
     SPLLDIV2 = 0b110,
 }
 
+/// Enable or disable an associated peripheral clock.
 pub trait PccEnabled {
+    /// Returns true if the peripheral clock is enabled.
     fn pcc_enabled(&self) -> bool;
+    /// Enables the peripheral clock if true, otherwise disables.
     fn pcc_set_enabled(&self, value: bool) -> &Self;
+    /// Enables the peripheral clock.
     fn pcc_enable(&self) -> &Self { self.pcc_set_enabled(true); self }
+    /// Disables the peripheral clock.
     fn pcc_disable(&self) -> &Self { self.pcc_set_enabled(false); self }
 }
 
@@ -27,12 +32,16 @@ impl<P> PccEnabled for P where P: Cgc {
     }
 }
 
+/// Set an associated peripheral clock source.
 pub trait PccClockSource {
+    /// Returns the clock source for the peripheral.
     fn pcc_clock_source(&self) -> ClockSource;
+    /// Disables the clock gate and sets the clock source for the peripheral.
+    /// Then, if the clock source != ClockSource::Disabled, enables the clock gate.
     fn pcc_set_clock_source(&self, value: ClockSource) -> &Self; 
 }
 
-impl<P> PccClockSource for P where P: Pcs {
+impl<P> PccClockSource for P where P: Pcs + Cgc {
     fn pcc_clock_source(&self) -> ClockSource {
         match self.pcs() {
             0b000 => ClockSource::Disabled,
@@ -44,7 +53,11 @@ impl<P> PccClockSource for P where P: Pcs {
         }
     }
     fn pcc_set_clock_source(&self, value: ClockSource) -> &Self {
+        self.set_cgc(0);
         self.set_pcs(value as u32);
+        if value != ClockSource::Disabled {
+            self.set_cgc(1)
+        }
         self
     }
 }
