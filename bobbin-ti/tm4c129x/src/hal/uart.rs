54227d2a825fa6f2dc6bd5bbf00733feaa8b30ef
Jonathan Soo <jcsoo@agora.com>
2017-08-31 14:18:25 -0400

    Implement config for TM4C UART

--- bobbin-ti/tm4c129x/src/hal/uart.rs
+++ bobbin-ti/tm4c129x/src/hal/uart.rs
@@ -1,82 +1,106 @@
+pub use bobbin_common::configure::*;
+pub use bobbin_common::enabled::*;
 pub use bobbin_common::serial::*;
+
 pub use chip::uart::*;
 pub use super::sysctl::SysctlEnabled;
 
-impl UartPeriph {
-    pub fn configure(&self, baud_hz: u32, sysclk_hz: u32) -> &Self {
-        let baud_div = ((8 * sysclk_hz) / baud_hz) + 1;
+use bobbin_common::bits::*;
+
+#[derive(Debug, Default)]
+pub struct Config {
+    pub ibrd: Ibrd,
+    pub fbrd: Fbrd,
+    pub lcrh: Lcrh,
+    pub ctl: Ctl,
+}
+
+impl Config {
+    pub fn set_baud(mut self, baud_hz: u32, clock_hz: u32) -> Self {
+        let baud_div = ((8 * clock_hz) / baud_hz) + 1;
         let baud_int = baud_div / 64;
         let baud_frac = baud_div % 64;
-
-        self.with_ctl(|r| r.set_uarten(0));
-        self.with_ibrd(|r| r.set_divint(baud_int));
-        self.with_fbrd(|r| r.set_divfrac(baud_frac));
-        self.with_lcrh(|r| r.set_wlen(0x3).set_fen(1));
-        self.with_ctl(|r| r.set_hse(1).set_rxe(1).set_txe(1));        
+        self.ibrd = self.ibrd.set_divint(baud_int);
+        self.fbrd = self.fbrd.set_divfrac(baud_frac);
+        self.ctl = self.ctl.set_hse(1);
         self
     }
-
-    pub fn enable(&self) -> &Self {
-        self.with_ctl(|r| r.set_uarten(1))        
+    pub fn set_wlen(mut self, wlen: U2) -> Self {
+        self.lcrh = self.lcrh.set_wlen(wlen);
+        self
     }
-
-    pub fn disable(&self) -> &Self {
-        self.with_ctl(|r| r.set_uarten(0))
+    pub fn set_fen(mut self, fen: U1) -> Self {
+        self.lcrh = self.lcrh.set_fen(fen);
+        self
     }
-
-    // pub fn try_getc(&self) -> Option<u8> {
-    //     if !self.rxfe() {
-    //         Some(self.data())
-    //     } else {
-    //         None
-    //     }        
-    // }
-
-    // pub fn putc(&self, c: u8) {
-    //     while self.txff() {}
-    //     self.set_data(c);
-    // }
-
-    // pub fn write(&self, buf: &[u8]) {
-    //     for b in buf.iter() {
-    //         self.putc(*b)
-    //     }
-    // }
-
-    pub fn data(&self) -> u8 {
-        self.dr().data().value()
-    }
-
-    pub fn set_data(&self, value: u8) -> &Self {  
-        self.set_dr(|r| r.set_data(value))
-    }
-    
-    pub fn rxfe(&self) -> bool {
-        self.fr().rxfe() != 0
-    }
-
-    pub fn txff(&self) -> bool {
-        self.fr().txff() != 0
-    }        
 }
+
+
+impl Configure<Config> for UartPeriph {
+    fn config(&self) -> Config {
+        Config {
+            ibrd: self.ibrd(),
+            fbrd: self.fbrd(),
+            lcrh: self.lcrh(),
+            ctl: self.ctl(),
+        }        
+    }
+
+    fn configure(&self, cfg: Config) -> &Self {
+        self
+            .disable()
+            .set_ibrd(|_| cfg.ibrd)
+            .set_fbrd(|_| cfg.fbrd)
+            .set_lcrh(|_| cfg.lcrh)
+            .set_ctl(|_| cfg.ctl)
+    }
+}
+
+impl Enabled for UartPeriph {
+    fn enabled(&self) -> bool {
+        self.ctl().uarten() != 0
+    }
+
+    fn set_enabled(&self, value: bool) -> &Self {
+        self.with_ctl(|r| r
+            .set_uarten(value)
+            .set_rxe(value)
+            .set_txe(value))
+    }
+}
+
+// impl UartPeriph {
+//     pub fn configure(&self, baud_hz: u32, sysclk_hz: u32) -> &Self {
+//         let baud_div = ((8 * sysclk_hz) / baud_hz) + 1;
+//         let baud_int = baud_div / 64;
+//         let baud_frac = baud_div % 64;
+
+//         self.with_ctl(|r| r.set_uarten(0));
+//         self.with_ibrd(|r| r.set_divint(baud_int));
+//         self.with_fbrd(|r| r.set_divfrac(baud_frac));
+//         self.with_lcrh(|r| r.set_wlen(0x3).set_fen(1));
+//         self.with_ctl(|r| r.set_hse(1).set_rxe(1).set_txe(1));        
+//         self
+//     }
+// }
 
 
 impl SerialTx<u8> for UartPeriph  {
     fn can_tx(&self) -> bool {
-        !self.txff()
+        self.fr().txff() == 0
     }
 
     fn tx(&self, c: u8) -> &Self {
-        self.set_data(c)
+        self.set_dr(|r| r.set_data(c))
     }
 }
 
 impl SerialRx<u8> for UartPeriph {
     fn can_rx(&self) -> bool {
-        !self.rxfe()
+        self.fr().rxfe() == 0
     }
 
     fn rx(&self) -> u8 {
-        self.data()
+        self.dr().data().value()
     }
 }
