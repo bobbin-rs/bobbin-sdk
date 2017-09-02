94d91d0c47a4011ac8c5a92052539e7388d52e42
Jonathan Soo <jcsoo@agora.com>
2017-09-01 21:33:07 -0400

    Update config for F24

--- bobbin-stm32/stm32-common/src/hal/usart_f24.rs
+++ bobbin-stm32/stm32-common/src/hal/usart_f24.rs
@@ -1,77 +1,84 @@
+pub use bobbin_common::configure::*;
+pub use bobbin_common::enabled::*;
 pub use bobbin_common::serial::*;
 use core::fmt::{self, Write};
 use chip::usart_f24::*;
 
+#[derive(Debug)]
+pub struct Config {
+    cr1: Cr1,
+    cr2: Cr2,
+    cr3: Cr3,
+    brr: Brr,
+}
 
-impl UsartPeriph {
-    pub fn enable(&self, brr: u32) -> &Self {
-        // 29.5.4 - USART baud rate generation
-        // In case of oversampling by 16, the equation is: Baud = Fck / USARTDIV
-        // In case of oversampling by 8, the equation is: Baud = 2 * Fck / USARTDIV
+impl Default for Config {
+    fn default() -> Self {
+        Config { 
+            cr1: Cr1(0),
+            cr2: Cr2(0),
+            cr3: Cr3(0),
+            brr: Brr(0),
+        }
+    }
+}
 
-        // NOTE: USART1 is on APB2, all others on APB1
-        // Fck will be 36Mhz for APB1, 72Mhz for APB2
+impl Config {
+    // 29.5.4 - USART baud rate generation
+    // In case of oversampling by 16, the equation is: Baud = Fck / USARTDIV
+    // In case of oversampling by 8, the equation is: Baud = 2 * Fck / USARTDIV
 
-        // Assuming FCK is 36Mhz and 8x oversampling, 115,200 = 2 * 36Mhz / USARTDIV
-        // USARTDIV = 2 * 36Mhz / 115,200 = 625
-        // let brr = (apb_hz / baud_hz) as u16;
+    // NOTE: USART1 is on APB2, all others on APB1
+    // Fck will be 36Mhz for APB1, 72Mhz for APB2
 
+    // Assuming FCK is 36Mhz and 8x oversampling, 115,200 = 2 * 36Mhz / USARTDIV
+    // USARTDIV = 2 * 36Mhz / 115,200 = 625
+    // let brr = (apb_hz / baud_hz) as u16;    
+    pub fn set_baud(self, baud: u32, clock: u32) -> Self {
+        self.set_brr(clock / baud)
+    }
+
+    pub fn set_brr(mut self, brr: u32) -> Self {
+        self.brr = Brr(0)
+            .set_div_fraction((brr & 0b1111) as u32)
+            .set_div_mantissa(brr as u32 >> 4);
+        self            
+    }
+}
+
+impl Configure<Config> for UsartPeriph {
+    fn config(&self) -> Config {
+        Config {
+            cr1: self.cr1(),
+            cr2: self.cr2(),
+            cr3: self.cr3(),
+            brr: self.brr(),
+        }
+    }
+
+
+    fn configure(&self, cfg: Config) -> &Self {        
+        self.set_cr1(|_| cfg.cr1);
+        self.set_cr2(|_| cfg.cr2);
+        self.set_cr3(|_| cfg.cr3);
+        self.set_brr(|_| cfg.brr);
         self
-            .set_cr1(|r| r)
-            .set_cr2(|r| r)
-            .set_cr3(|r| r)
-            .set_brr(|r| r
-                .set_div_fraction((brr & 0b1111) as u32)
-                .set_div_mantissa(brr as u32 >> 4)
-            )    
+    }
+}
+
+impl Enabled for UsartPeriph {
+    fn enabled(&self) -> bool {
+        self.cr1().test_ue()
+    }
+
+    fn set_enabled(&self, value: bool) -> &Self {
+        self
             .set_cr1(|r| r
-                .set_ue(1)
-                .set_re(1)
-                .set_te(1)
+                .set_ue(value)
+                .set_re(value)
+                .set_te(value)
             )        
     }
-
-    pub fn disable(&self) -> &Self {
-        self.with_cr1(|r| r.set_ue(0).set_re(0).set_te(0))
-    }
-
-    // fn putc(&self, c: u8) {
-    //     let u = self;
-    //     while u.sr().txe() == 0 {}
-    //     u.set_dr(|r| r.set_dr(c as u32));    
-    // }
-
-    // fn try_putc(&self, c: u8) -> Option<usize> {
-    //     let u = self;
-    //     if u.sr().txe() != 0 {
-    //         u.set_dr(|r| r.set_dr(c as u32));
-    //         Some(1)
-    //     } else {
-    //         None
-    //     }          
-    // }
-
-    // fn try_getc(&self) -> Option<u8> {
-    //     let u = self;
-    //     if u.sr().rxne() != 0 {
-    //         Some(u.dr().dr().into())
-    //     } else {
-    //         None
-    //     }  
-    // }
-
-    // fn getc(&self) -> u8 {
-    //     let u = self;
-    //     while u.sr().rxne() == 0 {}
-    //     u.dr().dr().into()
-    // }
-
-    // fn write(&self, buf: &[u8]) -> usize {
-    //     for b in buf.iter() {
-    //         self.putc(*b)
-    //     }
-    //     buf.len()
-    // }    
 }
 
 impl Write for UsartPeriph {
