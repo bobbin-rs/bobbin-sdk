6f221c31b30d33ab3fb726d7e03ba9b36252bde2
Jonathan Soo <jcsoo@agora.com>
2017-09-07 16:05:39 -0400

    Add LPTMR tests

--- evb-s32k144/src/main.rs
+++ evb-s32k144/src/main.rs
@@ -10,6 +10,9 @@ pub extern "C" fn main() -> ! {
     println!("[start] Running tests for evb-s32k144");
     test_crc();
     test_gpio();
+    test_ftm();
+    test_lptmr();
+    test_systick();
     println!("[done] All tests passed");
     loop {}
 }
@@ -62,23 +65,6 @@ fn test_crc() {
     let v: u16 = CRC.init(0u16).write(&b"123456789"[..]).read();
     // println!("{:04x} {:04x}", v, 0x3EEB);
     assert_eq!(v, 0xBB3D);
-    // for tot in 0..4 {
-    //     for totr in 0..4 {
-    //         for fxor in 0..2 {
-    //             println!("{} {} {}", tot, totr,fxor);
-    //             CRC
-    //                 .set_ctrl(|r| r.set_tcrc(0).set_tot(tot).set_totr(totr).set_fxor(fxor))
-    //                 .set_poly(0x8005u16);
-
-    //             let v: u16 = CRC.init(0u16).write(0x1234u32).datal().0;
-    //             println!("{:04x} {:04x}", v, 0x770D);
-    //             if v == 0x770D {
-    //                 println!("----");
-    //                 loop {}
-    //             }
-    //         }
-    //     }
-    // }
 
     // assert_eq!(v, 0x3EEB);
     println!("[pass] CRC OK");
@@ -120,4 +106,130 @@ fn test_gpio() {
 
     println!("[pass] GPIO OK");
 
+}
+
+
+fn test_ftm() {
+    use board::hal::pcc;
+    use board::hal::ftm::*;
+
+    fn check_progress(tim: &FtmPeriph, tim_ch: &FtmCh) {
+        let mut c_max = 0;
+        while !tim.timeout_flag() {
+            let c = tim.counter();
+            if c > c_max {
+                c_max = c;
+            }
+        }
+        assert!(tim_ch.compare_flag());
+        assert!(c_max > 0);
+    }
+
+    let tim = FTM0;
+    let tim_ch = FTM0_CH1;
+    tim
+        .pcc_set_clock_source(pcc::ClockSource::SOSCDIV2)
+        .pcc_enable();
+    // tim.set_enabled(true);
+
+    // Repeat Up Counter
+    
+    tim_ch.with_csc(|r| r.set_chie(0).set_msb(0).set_msa(1).set_elsb(0).set_elsa(0));
+    tim_ch.set_compare(512);
+
+    tim_ch.clr_compare_flag();
+    assert!(!tim_ch.compare_flag());
+    tim
+        .start_up(1024)
+        .clr_timeout_flag();
+    check_progress(&tim, &tim_ch);
+    tim.clr_timeout_flag();
+    tim_ch.clr_compare_flag();
+    check_progress(&tim, &tim_ch);    
+    
+    // tim.set_enabled(false);
+    tim.pcc_disable();
+
+    println!("[pass] FTM OK");
+}
+
+fn test_lptmr() {
+    use board::hal::pcc;
+    use board::hal::lptmr::*;
+
+    fn check_progress(tim: &LptmrPeriph) {
+        let mut c_max = 0;
+        while !tim.timeout_flag() {
+            let c = tim.counter();
+            if c > c_max {
+                c_max = c;
+            }
+        }
+        assert!(tim.compare_flag());
+        assert!(c_max > 0);
+    }
+
+    let tim = LPTMR0;
+    tim
+        .pcc_set_clock_source(pcc::ClockSource::SOSCDIV2)
+        .pcc_set_clock_divider(pcc::ClockDivider::Div1)
+        .pcc_set_clock_divider_frac(pcc::ClockDividerFrac::Frac0)
+        .pcc_enable();
+    tim.with_csr(|r| r.set_ten(0));
+    // select PCC clock as input
+    tim.with_psr(|r| r.set_pbyp(1).set_pcs(0b11));
+
+    // Repeat Up Counter    
+    tim
+        .set_compare(2048)
+        .clr_timeout_flag()
+        .start_up(4096);
+    check_progress(&tim);
+    tim.clr_compare_flag().clr_timeout_flag();
+    check_progress(&tim);
+    
+    // tim.set_enabled(false);
+    tim.pcc_disable();
+
+    println!("[pass] FTM OK");
+}
+
+fn test_systick() {
+    use board::hal::systick;
+
+    let reload_value = 1000;
+
+    // println!("# Disable Systick");
+    systick::set_enabled(false);
+    assert!(!systick::enabled());
+
+    // println!("# Set Reload Value");
+    systick::set_reload_value(reload_value);
+    assert_eq!(systick::reload_value(), reload_value);
+
+    // println!("# Set Current Value");
+    systick::set_current_value(0);
+    assert_eq!(systick::current_value(), 0);
+
+    // println!("# Clear Count Flag");
+    let _ = systick::count_flag();
+    assert!(!systick::count_flag());
+
+
+    let mut value_min = systick::current_value();
+
+    // println!("# Start Test");
+    systick::set_enabled(true);
+    assert!(systick::current_value() > 0);
+
+    while !systick::count_flag() {
+        let v = systick::current_value();
+        if v < value_min {
+            value_min = v;
+        }
+    }
+    assert!(value_min < reload_value);
+    systick::set_enabled(false);
+
+    println!("[pass] SYSTICK OK");
 }
