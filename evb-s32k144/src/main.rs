fae100116a1e466b4b4f9a46d10532418449c393
Jonathan Soo <jcsoo@agora.com>
2017-09-07 11:23:27 -0400

    Add simple GPIO test

--- evb-s32k144/src/main.rs
+++ evb-s32k144/src/main.rs
@@ -8,10 +8,44 @@ extern crate evb_s32k144 as board;
 pub extern "C" fn main() -> ! {
     board::init();
     println!("[start] Running tests for evb-s32k144");
-    for i in 0..5 {
-        println!("[pass] {}", i);
-        board::delay(100);
-    }
+    test_gpio();
     println!("[done] All tests passed");
     loop {}
+}
+
+/// Jumper PTA11(D0) to PTA17(D1)
+fn test_gpio() {
+    use board::hal::port::*;
+    use board::hal::gpio::*;
+
+    let port = PORTA;
+    let port_out = PTA11;
+    let port_in = PTA17;
+    let gpio_out = port_out.gpio_pin();
+    let gpio_in = port_in.gpio_pin();
+
+    port.pcc_enable();
+    port_out.set_mux_gpio();
+    port_in.set_mux_gpio();
+
+    gpio_out.set_dir_output();
+    gpio_in.set_dir_input();
+
+    gpio_out.set_output(false);
+    assert_eq!(gpio_in.input(), false);
+
+    gpio_out.set_output(true);
+    assert_eq!(gpio_in.input(), true);
+
+    gpio_out.toggle_output();
+    assert_eq!(gpio_in.input(), false);
+
+    gpio_out.toggle_output();
+    assert_eq!(gpio_in.input(), true);
+
+    port_out.set_mux_disabled();
+    port_in.set_mux_disabled();
+
+    println!("[pass] GPIO OK");
+
 }
