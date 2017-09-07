0fd6187dbe079eb538e02331d88ae55d45be9a7a
Jonathan Soo <jcsoo@agora.com>
2017-09-07 13:40:52 -0400

    Add CRC16 test

--- evb-s32k144/src/main.rs
+++ evb-s32k144/src/main.rs
@@ -31,40 +31,51 @@ fn test_crc() {
         .set_poly(CRC_32);
         
     let v: u32 = CRC.init(CRC_32_START).write(0x12345678u32).read();
-    // println!("# {:08x} {:08x}", v, 0x4A090E98);
     assert_eq!(v, 0x4A090E98);
 
     let msg = b"Hello World";
     let v: u32 = CRC.init(CRC_32_START).write(&msg[..]).read();
-    // for c in b"Hello World" {
-    //     CRC.write(*c);
-    // }
-    // let v: u32 = CRC.read();
-    // println!("# {:08x} {:08x}", v, 0x4A17B156);
     assert_eq!(v, 0x4A17B156);
+
+    let tot = 1;
+    let totr = 1;
+    let fxor = 0;
+
+    // println!("{} {} {}", tot, totr,fxor);
+    CRC
+        .set_ctrl(|r| r.set_tcrc(0).set_tot(tot).set_totr(totr).set_fxor(fxor))
+        .set_poly(0x8005u16);
+
+    let v: u16 = CRC.init(0u16).write(0x1234u32).read();
+    // println!("{:04x} {:04x}", v, 0x770D);
+    assert_eq!(v, 0x770d);
+
+
+    let v: u16 = CRC.init(0u16).write(&msg[..]).read();
+    // println!("{:04x} {:04x}", v, 0x3EEB);
+    assert_eq!(v, 0x3eeb);
 
     // for tot in 0..4 {
     //     for totr in 0..4 {
     //         for fxor in 0..2 {
-    //             println!("{} {} {}", tot, totr, fxor);
-    //             CRC.set_ctrl(|r| r.set_tcrc(1).set_tot(tot).set_totr(totr).set_fxor(fxor));
-    //             CRC.set_gpoly(|_| Gpoly(0x04c11db7u32));
-    //             let v: u32 = CRC.poly();
-    //             println!("{:08x} {:08x}", v, CRC_32);
-    //             CRC.init(0xFFFFFFFFu32);
-    //             CRC.write(0x12345678u32);
-    //             let v = CRC.data().0;
-    //             println!("{:08x} {:08x}", v, 0x4A090E98);
-    //             if v == 0x4A090E98 {
-    //                 println!("---");
+    //             println!("{} {} {}", tot, totr,fxor);
+    //             CRC
+    //                 .set_ctrl(|r| r.set_tcrc(0).set_tot(tot).set_totr(totr).set_fxor(fxor))
+    //                 .set_poly(0x8005u16);
+
+    //             let v: u16 = CRC.init(0u16).write(0x1234u32).datal().0;
+    //             println!("{:04x} {:04x}", v, 0x770D);
+    //             if v == 0x770D {
+    //                 println!("----");
     //                 loop {}
     //             }
-
     //         }
     //     }
     // }
 
+    // assert_eq!(v, 0x3EEB);
     println!("[pass] CRC OK");
+
 }
 
 /// Jumper PTA11(D0) to PTA17(D1)
