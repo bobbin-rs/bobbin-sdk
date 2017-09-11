ffeb80df30355c394dec9fbb73f2423959533a50
Jonathan Soo <jcsoo@agora.com>
2017-09-11 11:27:35 -0400

    Add i2c_v1 for F429

--- bobbin-stm32/stm32f303x/src/chip/i2c.rs
+++ bobbin-stm32/stm32f303x/src/chip/i2c.rs
@@ -13,13 +13,19 @@ impl super::sig::SignalI2cSda<super::sig::I2c1Sda> for I2c1 {}
 impl super::sig::Signal<super::sig::I2c1Smbal> for I2c1 {}
 impl super::sig::SignalI2cSmbal<super::sig::I2c1Smbal> for I2c1 {}
 
-impl super::sig::Signal<super::sig::I2c1Scl> for I2c2 {}
-impl super::sig::SignalI2cScl<super::sig::I2c1Scl> for I2c2 {}
-impl super::sig::Signal<super::sig::I2c1Sda> for I2c2 {}
-impl super::sig::SignalI2cSda<super::sig::I2c1Sda> for I2c2 {}
-impl super::sig::Signal<super::sig::I2c1Smbal> for I2c2 {}
-impl super::sig::SignalI2cSmbal<super::sig::I2c1Smbal> for I2c2 {}
+impl super::sig::Signal<super::sig::I2c2Scl> for I2c2 {}
+impl super::sig::SignalI2cScl<super::sig::I2c2Scl> for I2c2 {}
+impl super::sig::Signal<super::sig::I2c2Sda> for I2c2 {}
+impl super::sig::SignalI2cSda<super::sig::I2c2Sda> for I2c2 {}
+impl super::sig::Signal<super::sig::I2c2Smbal> for I2c2 {}
+impl super::sig::SignalI2cSmbal<super::sig::I2c2Smbal> for I2c2 {}
 
+impl super::sig::Signal<super::sig::I2c3Scl> for I2c3 {}
+impl super::sig::SignalI2cScl<super::sig::I2c3Scl> for I2c3 {}
+impl super::sig::Signal<super::sig::I2c3Sda> for I2c3 {}
+impl super::sig::SignalI2cSda<super::sig::I2c3Sda> for I2c3 {}
+impl super::sig::Signal<super::sig::I2c3Smbal> for I2c3 {}
+impl super::sig::SignalI2cSmbal<super::sig::I2c3Smbal> for I2c3 {}
 
 
 
