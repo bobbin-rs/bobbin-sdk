85aa0302e7cbb9797388ebe57b14077508ce6bbf
Jonathan Soo <jcsoo@agora.com>
2017-09-17 10:10:26 -0400

    Working L432 blinky

---
+++ nucleo-l432kc/examples/exti.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[macro_use]
+extern crate nucleo_l432kc as board;
+
+use board::btn::BTN0;
+use board::hal::gpio::*;
+use board::hal::syscfg::*;
+use board::hal::exti::*;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    
+    println!("Test EXTI");
+
+    //let b = BTN0; // PA12
+    let line = EXTI_LINE12;
+    SYSCFG.rcc_set_enabled(true);
+    SYSCFG.set_exti(line.index(), Source::GpioA);
+    
+    line.set_interrupt_mask(true);
+    line.set_rising_trigger(true);
+    line.set_falling_trigger(true);
+    line.clr_pending();
+
+    let h = Btn0Handler {};
+    let _h = line.register_exti_handler(&h);
+
+    let irq = line.irq_exti();
+    irq.set_enabled(true);
+
+    loop { unsafe { asm!("wfi") } }
+}
+
+pub struct Btn0Handler {}
+impl HandleExti for Btn0Handler {
+    fn handle_exti(&self) {
+        let line = EXTI_LINE12;
+        line.clr_pending();
+        if BTN0.input() {
+            println!("up")
+        } else {
+            println!("down")
+        }
+    }
+}
---
+++ nucleo-l432kc/examples/tim_compare.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#[macro_use]
+extern crate nucleo_l432kc as board;
+
+use board::hal::tim::*;
+use board::hal::clock::Clock;
+use board::clock::CLK;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    println!("Compare Test");
+
+    let ch = TIM2_CH2;
+
+    let t = ch.periph();
+    t
+        .rcc_set_enabled(true)
+        .set_prescale((t.clock(&CLK).unwrap() / 1000) as u16);
+    ch.set_compare(500);
+    t.start(1000);
+    
+    loop {
+        ch.clr_compare_flag().wait_compare_flag();        
+        println!("{}: Compare", t.counter());
+        t.clr_timeout_flag().wait_timeout_flag();
+        println!("{}: Timeout", t.counter());
+    }
+}
