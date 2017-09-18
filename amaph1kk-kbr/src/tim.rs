da19cd663a8f2ad687c306884f17a2992bc02753
Jonathan Soo <jcsoo@agora.com>
2017-09-18 16:38:05 -0400

    Disable compare after delay

--- amaph1kk-kbr/src/tim.rs
+++ amaph1kk-kbr/src/tim.rs
@@ -11,4 +11,5 @@ pub fn delay(ms: u32) {
         .set_stmintclr(|r| r.set_compare(0, 1))
         .set_scmpr(0, |r| r.set_value(ms));
     while STIMER.stmintstat().compare(0) == 0 {}
+    STIMER.with_stcfg(|r| r.set_compare_en(0, 0));
 }
