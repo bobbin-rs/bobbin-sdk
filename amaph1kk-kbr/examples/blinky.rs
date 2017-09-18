703db6ebb376a3aa93eebebd81bc91ab50adb9b8
Jonathan Soo <jcsoo@agora.com>
2017-09-18 15:59:48 -0400

    Use GPIO Pin API

--- amaph1kk-kbr/examples/blinky.rs
+++ amaph1kk-kbr/examples/blinky.rs
@@ -11,22 +11,33 @@ use board::hal::gpio::*;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    GPIO.with_padkey(|r| r.set_padkey(0x73));
+    // GPIO.with_padkey(|r| r.set_padkey(0x73));
 
-    GPIO.with_padreg(4, |r| r.set_padfncsel(1, 0x3));
-    GPIO.with_cfg(2, |r| r.set_gpiooutcfg(1, 0x1));
+    let led = P17;
+
+    GPIO
+        .unlock();
+
+    led
+        .set_pad_gpio()
+        .set_gpio_output_pushpull();
+
+    // GPIO.with_padreg(4, |r| r.set_padfncsel(1, 0x3));
+    // GPIO.with_cfg(2, |r| r.set_gpiooutcfg(1, 0x1));
 
     pub const DELAY: usize = 4_000_000;
 
     loop {
         // Set GPIO17
-        GPIO.set_wts(0, |r| r.set_wts(17, 1));
+        led.set_output(true);
+        // GPIO.set_wts(0, |r| r.set_wts(17, 1));
         // Delay approx 1/2 second
         unsafe {
             for _ in 0..DELAY { asm!("nop") }
         }
         // Clr GPIO17
-        GPIO.set_wtc(0, |r| r.set_wtc(17, 1));
+        led.set_output(false);
+        // GPIO.set_wtc(0, |r| r.set_wtc(17, 1));
         // ptr::write_volatile(WTCA, 1 << 17);
         // Delay approx 1/2 second
         unsafe {
