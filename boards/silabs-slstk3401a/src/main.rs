d076fc4dc4cd2357bf4dfb8d836a0e78f43f35ca
Jonathan Soo <jcsoo@agora.com>
2017-09-25 16:22:57 -0400

    Set up basic placeholder test

--- boards/silabs-slstk3401a/src/main.rs
+++ boards/silabs-slstk3401a/src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
 extern crate silabs_slstk3401a as board;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
+    board::init();
+    println!("[start] Running tests for silabs-slstk3401a");
+    println!("[done] All tests passed");
     loop {}
 }
