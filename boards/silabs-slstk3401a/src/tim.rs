41bc35382df61ae650186a6ec7172f3738dbbd95
Jonathan Soo <jcsoo@agora.com>
2017-09-25 16:39:46 -0400

    enable and disable cryotimer each delay cycle

--- boards/silabs-slstk3401a/src/tim.rs
+++ boards/silabs-slstk3401a/src/tim.rs
@@ -5,18 +5,22 @@ pub const RATE: u32 = 1000;
 pub const DIV: u32 = CLOCK / RATE;
 
 pub fn init() {
+}
+
+pub fn delay(ms: u32) {
     CRYOTIMER.cmu_enable();
 
     // Use LFXO, Divide by 32 for 1024Hz
     CRYOTIMER.with_ctrl(|r| r.set_oscsel(2).set_presc(5));
-    CRYOTIMER.with_ctrl(|r| r.set_en(1));
     CRYOTIMER.set_periodsel(|r| r.set_periodsel(0));
-}
 
-pub fn delay(ms: u32) {
     CRYOTIMER.set_ifc(|r| r.set_period(1));
+    CRYOTIMER.with_ctrl(|r| r.set_en(1));
     for _ in 0..ms {       
         while CRYOTIMER._if().period() == 0 {}
         CRYOTIMER.set_ifc(|r| r.set_period(1));
     }
+    CRYOTIMER.with_ctrl(|r| r.set_en(0));
+
+    CRYOTIMER.cmu_disable();
 }
