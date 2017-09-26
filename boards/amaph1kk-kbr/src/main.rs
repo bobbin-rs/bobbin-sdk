d456fc52024bed1cca10c9411afd7518392e0514
Jonathan Soo <jcsoo@agora.com>
2017-09-25 20:16:24 -0400

    Add additional tests

--- boards/amaph1kk-kbr/src/main.rs
+++ boards/amaph1kk-kbr/src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
 
-// #[macro_use]
+#[macro_use]
 extern crate amaph1kk_kbr as board;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
+    board::init();
+    board::delay(10);
+    println!("[start] Running tests for amaph1kk-kbr");
+    println!("[done] All tests passed");
     loop {}
 }
