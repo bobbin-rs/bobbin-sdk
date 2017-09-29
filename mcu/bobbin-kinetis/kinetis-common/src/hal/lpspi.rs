0905bc21824b7aec9241054ce73e2f27151482c4
Jonathan Soo <jcsoo@agora.com>
2017-09-29 16:02:49 -0400

    Add read / write for Target

--- mcu/bobbin-kinetis/kinetis-common/src/hal/lpspi.rs
+++ mcu/bobbin-kinetis/kinetis-common/src/hal/lpspi.rs
@@ -157,6 +157,14 @@ impl Target {
         self.periph.rdr().data().into()
     }    
 
+    pub fn write(&self, tx: &[u8]) {
+        self.transfer(tx, &mut []);
+    }
+
+    pub fn read(&self, rx: &mut [u8]) {
+        self.transfer(&[], rx);
+    }
+
     pub fn transfer(&self, tx: &[u8], rx: &mut [u8]) {
         while self.periph.sr().tdf() == 0 {}
         self.periph.set_tcr(|_| self.tcr.set_framesz(7));
