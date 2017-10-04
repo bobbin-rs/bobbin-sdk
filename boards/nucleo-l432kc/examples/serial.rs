f921f5ceacbbc107d7d951c4dac542b52d735ac1
Jonathan Soo <jcsoo@agora.com>
2017-10-04 12:13:49 -0400

    Working basic USART driver

--- boards/nucleo-l432kc/examples/serial.rs
+++ boards/nucleo-l432kc/examples/serial.rs
@@ -7,6 +7,7 @@ extern crate nucleo_l432kc as board;
 use board::hal::usart::*;
 use board::common::Poll;
 use board::hal::RegisterPoll;
+use board::console::USART;
 // use board::common::serial::*;
 use board::common::ring::*;
 
@@ -15,49 +16,77 @@ pub extern "C" fn main() -> ! {
     board::init();
     println!("Running Serial Test");
     let irq = board::console::USART.irq_usart();
-    let tx: board::hal::usart::UsartPeriph = board::console::USART.into();
-    let mut buf = [0u8; 16];
-
-    let rb = Ring::new(&mut buf);
-   
-    let s = UsartDriver::new(tx, rb);
+    let mut tx_buf = [0u8; 16];
+    let mut rx_buf = [0u8; 16];
+    let s = UsartDriver::new(USART, &mut tx_buf, &mut rx_buf);
     let _g = irq.register_poll(&s);
+    s.enable_rx();
 
 
+    let mut buf = [0u8; 16];
     loop {
-        s.write(b"Hello\r\n");
-        board::delay(1000);
+        let n = s.read(&mut buf);
+        if n > 0 {
+            for b in (&buf[..n]).iter() {
+                if *b == b'\r' {
+                    s.write(b"\r\n");
+                } else {
+                    s.write(&[*b]);
+                }
+            }
+        }
     }
 }
 
 pub struct UsartDriver<'a> {
-    tx: UsartPeriph,
-    rb: Ring<'a, u8>,
+    usart: UsartPeriph,
+    tx: Ring<'a, u8>,
+    rx: Ring<'a, u8>,
 }
 
 impl<'a> UsartDriver<'a> {
-    pub fn new(tx: UsartPeriph, rb: Ring<'a, u8>) -> Self {
+    pub fn new<U: Into<UsartPeriph>>(usart: U, tx_buf: &mut [u8], rx_buf: &mut [u8]) -> Self {
+        let tx = Ring::new(tx_buf);
+        let rx = Ring::new(rx_buf);
+   
         UsartDriver {
+            usart: usart.into(),
             tx: tx,
-            rb: rb,
+            rx: rx,
         }
     }
 
+    pub fn enable_rx(&self) {
+        self.usart.with_cr1(|r| r.set_rxneie(1));
+    }
+
     pub fn write(&self, buf: &[u8]) -> usize {
-        let n = self.rb.write(buf);
-        self.tx.with_cr1(|r| r.set_txeie(1));
+        let n = self.tx.write(buf);
+        if n > 0 {
+            self.usart.with_cr1(|r| r.set_txeie(1));
+        }
         n
+    }
+
+    pub fn read(&self, buf: &mut [u8]) -> usize {
+        self.rx.read(buf)
     }
 }
 
 impl<'a> Poll for UsartDriver<'a> {
     fn poll(&self) {
-        if self.tx.can_tx() {
-            if let Some(b) = self.rb.dequeue() {
-                self.tx.tx(b);
-                if self.rb.len() == 0 {
-                    self.tx.with_cr1(|r| r.set_txeie(0));
+        if self.usart.can_tx() {
+            if let Some(b) = self.tx.dequeue() {
+                self.usart.tx(b);
+                if self.tx.is_empty() {
+                    self.usart.with_cr1(|r| r.set_txeie(0));
                 }
+            }
+        }
+        if self.usart.can_rx() {
+            let b = self.usart.rx();
+            if !self.rx.is_full() {
+                self.rx.enqueue(b);
             }
         }
     }
