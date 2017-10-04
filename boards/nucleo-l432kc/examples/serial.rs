974825c43e0de8739fe05fb44982bdae3df733e7
Jonathan Soo <jcsoo@agora.com>
2017-10-04 14:18:45 -0400

    Move UsartDriver to stm32-common

--- boards/nucleo-l432kc/examples/serial.rs
+++ boards/nucleo-l432kc/examples/serial.rs
@@ -5,14 +5,9 @@
 extern crate nucleo_l432kc as board;
 
 use board::hal::usart::*;
-use board::common::{Irq, Poll};
 use board::console::USART;
-// use board::common::serial::*;
-use board::common::ring::*;
-use core::fmt::{self, Write};
+use core::fmt::Write;
 
-use board::hal::nvic;
-use board::hal::scb::*;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
@@ -39,89 +34,6 @@ pub extern "C" fn main() -> ! {
                     s.write(&[*b]);
                 }
             }
-        }
-    }
-}
-
-pub struct UsartDriver<'a> {    
-    usart: UsartPeriph,
-    tx: Ring<'a, u8>,
-    rx: Ring<'a, u8>,    
-    irq_num: Option<u8>,
-}
-
-impl<'a> UsartDriver<'a> {
-    pub fn new<U: Into<UsartPeriph>>(usart: U, tx_buf: &mut [u8], rx_buf: &mut [u8]) -> Self {
-        let tx = Ring::new(tx_buf);
-        let rx = Ring::new(rx_buf);
-   
-        UsartDriver {
-            usart: usart.into(),
-            tx: tx,
-            rx: rx,
-            irq_num: None,
-        }
-    }
-
-    pub fn enable_irq<I: Irq>(&mut self, irq: &I) {
-        self.irq_num = Some(irq.irq_num());
-        SCB.set_irq_handler(irq.irq_num() as usize, Some(irq.wrap(self)));
-        nvic::set_enabled(irq.irq_num() as usize, true);
-    }
-
-    pub fn enable_rx(&self) {
-        self.usart.with_cr1(|r| r.set_rxneie(1));
-    }
-
-    pub fn write(&self, buf: &[u8]) -> usize {
-        let n = self.tx.write(buf);
-        if n > 0 {
-            self.usart.with_cr1(|r| r.set_txeie(1));
-        }
-        n
-    }
-
-    pub fn read(&self, buf: &mut [u8]) -> usize {
-        self.rx.read(buf)
-    }
-}
-
-impl<'a> Drop for UsartDriver<'a> {
-    fn drop(&mut self) {
-        if let Some(irq_num) = self.irq_num {
-            nvic::set_enabled(irq_num as usize, false);
-            SCB.set_irq_handler(irq_num as usize, None);
-        }
-    }
-}
-
-impl<'a> Poll for UsartDriver<'a> {
-    fn poll(&self) {
-        if self.usart.can_tx() {
-            if let Some(b) = self.tx.dequeue() {
-                self.usart.tx(b);
-                if self.tx.is_empty() {
-                    self.usart.with_cr1(|r| r.set_txeie(0));
-                }
-            }
-        }
-        if self.usart.can_rx() {
-            let b = self.usart.rx();
-            if !self.rx.is_full() {
-                self.rx.enqueue(b);
-            }
-        }
-    }
-
-}
-
-impl<'a> Write for UsartDriver<'a> {
-    fn write_str(&mut self, buf: &str) -> fmt::Result {
-        let buf = buf.as_bytes();
-        let mut n = 0;
-        loop {
-            if n == buf.len() { return Ok(())}
-            n += self.write(&buf[n..])
         }
     }
 }
--- mcu/bobbin-stm32/stm32-common/src/hal/usart.rs
+++ mcu/bobbin-stm32/stm32-common/src/hal/usart.rs
@@ -1,8 +1,14 @@
 pub use bobbin_common::configure::*;
 pub use bobbin_common::enabled::*;
 pub use bobbin_common::serial::*;
+pub use bobbin_common::{Irq, Poll};
+pub use chip::usart::*;
+
+use bobbin_common::ring::*;
+use bobbin_cortexm::hal::nvic;
+use bobbin_cortexm::hal::scb::*;
+
 use core::fmt::{self, Write};
-use chip::usart::*;
 
 #[derive(Debug)]
 pub struct Config {
@@ -109,5 +115,89 @@ impl SerialRx<u8> for UsartPeriph {
 
     fn rx(&self) -> u8 {
         self.rdr().rdr().value() as u8
+    }
+}
+
+
+pub struct UsartDriver<'a> {    
+    usart: UsartPeriph,
+    tx: Ring<'a, u8>,
+    rx: Ring<'a, u8>,    
+    irq_num: Option<u8>,
+}
+
+impl<'a> UsartDriver<'a> {
+    pub fn new<U: Into<UsartPeriph>>(usart: U, tx_buf: &mut [u8], rx_buf: &mut [u8]) -> Self {
+        let tx = Ring::new(tx_buf);
+        let rx = Ring::new(rx_buf);
+   
+        UsartDriver {
+            usart: usart.into(),
+            tx: tx,
+            rx: rx,
+            irq_num: None,
+        }
+    }
+
+    pub fn enable_irq<I: Irq>(&mut self, irq: &I) {
+        self.irq_num = Some(irq.irq_num());
+        SCB.set_irq_handler(irq.irq_num() as usize, Some(irq.wrap(self)));
+        nvic::set_enabled(irq.irq_num() as usize, true);
+    }
+
+    pub fn enable_rx(&self) {
+        self.usart.with_cr1(|r| r.set_rxneie(1));
+    }
+
+    pub fn write(&self, buf: &[u8]) -> usize {
+        let n = self.tx.write(buf);
+        if n > 0 {
+            self.usart.with_cr1(|r| r.set_txeie(1));
+        }
+        n
+    }
+
+    pub fn read(&self, buf: &mut [u8]) -> usize {
+        self.rx.read(buf)
+    }
+}
+
+impl<'a> Drop for UsartDriver<'a> {
+    fn drop(&mut self) {
+        if let Some(irq_num) = self.irq_num {
+            nvic::set_enabled(irq_num as usize, false);
+            SCB.set_irq_handler(irq_num as usize, None);
+        }
+    }
+}
+
+impl<'a> Poll for UsartDriver<'a> {
+    fn poll(&self) {
+        if self.usart.can_tx() {
+            if let Some(b) = self.tx.dequeue() {
+                self.usart.tx(b);
+                if self.tx.is_empty() {
+                    self.usart.with_cr1(|r| r.set_txeie(0));
+                }
+            }
+        }
+        if self.usart.can_rx() {
+            let b = self.usart.rx();
+            if !self.rx.is_full() {
+                self.rx.enqueue(b);
+            }
+        }
+    }
+
+}
+
+impl<'a> Write for UsartDriver<'a> {
+    fn write_str(&mut self, buf: &str) -> fmt::Result {
+        let buf = buf.as_bytes();
+        let mut n = 0;
+        loop {
+            if n == buf.len() { return Ok(())}
+            n += self.write(&buf[n..])
+        }
     }
 }
