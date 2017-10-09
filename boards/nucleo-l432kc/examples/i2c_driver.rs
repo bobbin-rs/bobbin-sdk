a2d4594ce69ef05bf772bec28df38d798a82fdd5
Jonathan Soo <jcsoo@agora.com>
2017-10-09 14:46:56 -0400

    I2C Rewrite WIP

--- boards/nucleo-l432kc/examples/i2c_driver.rs
+++ boards/nucleo-l432kc/examples/i2c_driver.rs
@@ -47,7 +47,7 @@ pub extern "C" fn main() -> ! {
         .set_scll(0x12)
     );
 
-    let mut tx_buf = [0u8; 64];
+    let mut tx_buf = [I2cAction::Idle; 64];
     let mut rx_buf = [0u8; 64];
 
     let d = I2cDriver::new(i2c, &mut tx_buf, &mut rx_buf);    
@@ -85,3 +85,254 @@ pub extern "C" fn main() -> ! {
         board::delay(500);
     }
 }
+
+use board::common::{Irq, Poll};
+use board::common::ring::Ring;
+use board::cortexm::hal::nvic;
+use board::cortexm::hal::scb::*;
+
+use core::marker::PhantomData;
+use core::cell::Cell;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum I2cAction {
+    Idle,
+    Start(u8),
+    Restart(u8),
+    WriteBytes(u8),
+    WriteByte(u8),
+    ReadBytes(u8),
+    ReadByte,
+    Stop,
+}
+
+pub struct I2cDriver<'a> {
+    i2c: I2cPeriph,
+    action: Cell<Option<I2cAction>>,
+    tx: Ring<'a, I2cAction>,
+    rx: Ring<'a, u8>,
+    _phantom: PhantomData<&'a mut [u8]>,
+}
+
+unsafe impl<'a> Sync for I2cDriver<'a> {}
+unsafe impl<'a> Send for I2cDriver<'a> {}
+
+impl<'a> I2cDriver<'a> {
+    pub fn new<I: Into<I2cPeriph>>(i2c: I, tx_buf: &'a mut [I2cAction], rx_buf: &'a mut [u8]) -> Self {
+        I2cDriver { 
+            i2c: i2c.into(),
+            action: Cell::new(None),
+            tx: Ring::new(tx_buf),
+            rx: Ring::new(rx_buf),
+            _phantom: PhantomData,
+        }
+    }
+    pub fn enable_irq<I: Irq>(&self, irq: &I) {
+        SCB.set_irq_handler(irq.irq_num() as usize, Some(irq.wrap(self)));
+        nvic::set_enabled(irq.irq_num() as usize, true);
+    }
+
+    pub fn action(&self) -> Option<I2cAction> {
+        self.action.get()
+    }
+
+    pub fn read_reg(&self, addr: U7, reg: u8) -> u8 {
+        let mut buf = [0u8];
+        self.transfer(addr, &[reg], &mut buf);
+        buf[0]
+    }
+
+    pub fn write_reg(&self, addr: U7, reg: u8, value: u8) {
+        let mut buf = [];
+        self.transfer(addr, &[reg, value], &mut buf);
+    }
+
+
+    pub fn transfer(&self, addr: U7, tx_buf: &[u8], rx_buf: &mut [u8]) {
+        println!("transfer: addr={:?} tx_buf={:?} rx_buf={:?}", addr, tx_buf, rx_buf);
+        if tx_buf.len() > 0 {
+            self.tx.enqueue(I2cAction::Start(addr.value() << 1));
+            // self.tx.enqueue(I2cAction::WriteBytes(tx_buf.len() as u8));
+            for b in tx_buf.iter() {
+                self.tx.enqueue(I2cAction::WriteByte(*b));
+            }
+            if rx_buf.len() == 0 {
+                self.tx.enqueue(I2cAction::Stop);
+            }
+        }
+        if rx_buf.len() > 0 {
+            if tx_buf.len() == 0 {
+                self.tx.enqueue(I2cAction::Start(addr.value() << 1 | 1));
+            } else {
+                self.tx.enqueue(I2cAction::Restart(addr.value() << 1 | 1));
+            }
+            // self.tx.enqueue(I2cAction::ReadBytes(tx_buf.len() as u8));
+            for _ in rx_buf.iter() {
+                self.tx.enqueue(I2cAction::ReadByte);
+            }
+            self.tx.enqueue(I2cAction::Stop);
+        }
+        println!("tx_len: {}", self.tx.len());
+        self.next();
+        loop {
+            while self.rx.len() < rx_buf.len() {}
+            self.rx.read(rx_buf);
+        }
+    }
+    
+
+    pub fn next(&self) {
+        loop {
+            // If currently processing an action, return without any changes
+            if self.action().is_some() { return }        
+
+            // Get the next action off of the queue
+            if let Some(action) = self.tx.dequeue() {
+                self.i2c.with_cr1(|r| r.set_txie(1).set_rxie(1).set_tcie(1).set_stopie(1));
+                match action {
+                    I2cAction::Idle => panic!("Unexpected I2cAction::Idle"),
+                    I2cAction::Start(addr) => {
+                        println!("Start: 0x{:02x} {}", addr >> 1, addr & 0b1);
+                        self.i2c.with_cr2(|r| r.set_start(1).set_sadd(addr >> 1).set_rd_wrn(addr & 0b1));
+                    },
+                    I2cAction::Restart(addr) => {
+                        println!("Restart: 0x{:02x} {}", addr >> 1, addr & 0b1);
+                        self.i2c.with_cr2(|r| r.set_start(1).set_sadd(addr >> 1).set_rd_wrn(addr & 0b1));
+                    },
+                    I2cAction::WriteBytes(_) => {},
+                    I2cAction::WriteByte(_) => {
+                        self.action.set(Some(action));
+                    },
+                    I2cAction::ReadBytes(_) => {},
+                    I2cAction::ReadByte => {
+                        self.action.set(Some(action));
+                    },
+                    I2cAction::Stop => {
+                        self.i2c.with_cr2(|r| r.set_stop(1));
+                        self.action.set(Some(action));
+                    },
+                }
+                
+            } else {
+                self.i2c.with_cr1(|r| r.set_txie(0).set_rxie(0).set_tcie(0).set_stopie(0));
+                return
+            }
+        }
+    }
+
+
+
+    // pub fn start_transfer(&self, addr: U7, tx_buf: &[u8], rx_len: usize) {
+        // self.clear();        
+        // self.addr.set(Some(addr));
+        // self.tx_len.set(tx_buf.len());                
+        // &self.tx()[..tx_buf.len()].copy_from_slice(tx_buf);
+        // self.rx_len.set(rx_len);
+
+        // self.i2c.with_cr1(|r| r.set_pe(1));
+        // // println!("tx start");
+        // if tx_buf.len() > 0 {
+        //     self.i2c.with_cr2(|r| r
+        //             .set_sadd(addr.value() << 1)
+        //             .set_rd_wrn(0)
+        //             .set_nbytes(self.tx_len.get())
+        //             .set_autoend(0)
+        //             // .set_autoend(self.rx_len.get() == 0)
+        //     );
+        // } else {
+        //     self.i2c.with_cr2(|r| r
+        //             .set_sadd(addr.value() << 1)
+        //             .set_rd_wrn(1)
+        //             .set_nbytes(self.tx_len.get())
+        //             .set_autoend(0)
+        //             // .set_autoend(self.rx_len.get() == 0)
+        //     );
+            
+        // }
+        // self.i2c.with_cr2(|r| r.set_start(1));                
+        // self.i2c.with_cr1(|r| r.set_txie(1).set_rxie(1).set_tcie(1).set_stopie(1));
+    // }
+
+}
+
+impl<'a> Poll for I2cDriver<'a> {
+    fn poll(&self) {       
+        let isr = self.i2c.isr();
+        let action = self.action().unwrap();
+        println!("ISR: {:?} Action: {:?}", isr, action);
+        match action {
+            I2cAction::Idle => unimplemented!(),
+            I2cAction::Start(_) => {
+                unimplemented!()
+            },
+            I2cAction::Restart(_) => {
+                unimplemented!()
+            },
+            I2cAction::WriteBytes(_) => {
+                unimplemented!()
+            },
+            I2cAction::WriteByte(n) => {
+                if isr.test_txis() {
+                    self.i2c.set_txdr(|r| r.set_txdata(n));
+                    self.action.set(None)
+                } else {
+                    panic!("Unexpected Interrupt: {:?}", isr);
+                }
+            },
+            I2cAction::ReadBytes(_) => {
+                unimplemented!()
+            },
+            I2cAction::ReadByte => {
+                if isr.test_rxne() {
+                    self.rx.enqueue(self.i2c.rxdr().rxdata().value());
+                    self.action.set(None)
+                } else {                    
+                    panic!("Unexpected Interrupt: {:?}", isr);
+                }
+            },
+            I2cAction::Stop => {
+                if isr.test_stopf() {
+                    self.i2c.set_icr(|r| r.set_stopcf(1));
+                    self.action.set(None)
+                } else {
+                    panic!("Unexpected Interrupt: {:?}", isr);
+                }
+            },
+        }
+        self.next();
+        board::delay(100);
+    }
+}
+
+// impl<'a> Poll for I2cDriver<'a> {
+//     fn poll(&self) {       
+//         let isr = self.i2c.isr();
+//         if isr.test_txis() {
+//             // println!("tx data");
+//             self.i2c.set_txdr(|r| r.set_txdata(self.tx()[self.tx_pos.get()]));
+//             self.tx_pos.set(self.tx_pos.get() + 1);
+//         } else if isr.test_rxne() {
+//             // println!("rx data");
+//             self.rx()[self.rx_pos.get()] = self.i2c.rxdr().rxdata().value();
+//             self.rx.enqueue(self.i2c.rxdr().rxdata().value());
+//             self.rx_pos.set(self.rx_pos.get() + 1);
+//         } else if isr.test_tc() {
+//             // println!("tc");
+//             let addr = self.addr.get().unwrap();
+//             if self.rx_len.get() - self.rx_pos.get() > 0 {
+//                 self.i2c.with_cr2(|r| r
+//                         .set_sadd(addr.value() << 1)
+//                         .set_rd_wrn(1)
+//                         .set_nbytes(self.rx_len.get())   
+//                 );
+//                 self.i2c.with_cr2(|r| r.set_start(1));               
+//                 self.i2c.with_cr2(|r| r.set_autoend(0));          
+//             } else {
+//                 self.i2c.with_cr2(|r| r.set_stop(1));
+//             }
+//         } else if isr.test_stopf() {
+//             self.i2c.with_cr1(|r| r.set_txie(0).set_rxie(0).set_tcie(0).set_stopie(1).set_stopie(0).set_pe(0));
+//             self.set_transfer_complete(true);
+//         }
+//     }
+// }
