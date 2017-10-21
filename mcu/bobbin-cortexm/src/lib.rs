5754e0e39ac45129e7b3c3523ce1aac1f830d37a
Jonathan Soo <jcsoo@agora.com>
2017-10-21 15:10:29 -0400

    Only use ASM on targets with no OS

--- mcu/bobbin-cortexm/src/lib.rs
+++ mcu/bobbin-cortexm/src/lib.rs
@@ -8,16 +8,19 @@ pub mod hal;
 
 #[inline(always)]
 pub fn wfi() {
+    #[cfg(target_os="none")]
     unsafe { asm!("wfi")}
 }
 
 #[inline(always)]
 pub fn wfe() {
+    #[cfg(target_os="none")]
     unsafe { asm!("wfe")}
 }
 
 #[inline(always)]
 pub fn nop() {
+    #[cfg(target_os="none")]
     unsafe { asm!{"nop"}}
 }
 
