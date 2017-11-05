14a578f9c3c3c3258a0765d6549c802308fe9a78
Jonathan Soo <jcsoo@agora.com>
2017-11-05 15:01:41 -0500

    Update I2C tests for nucleo-f746zg

--- boards/nucleo-f746zg/src/main.rs
+++ boards/nucleo-f746zg/src/main.rs
@@ -13,7 +13,7 @@ pub extern "C" fn main() -> ! {
     test_dma();
     test_adc();
     test_i2c();
-    test_spi_lora();
+    // test_spi_lora();
     println!("[done] All tests passed");
     loop {}
 }
@@ -139,15 +139,14 @@ fn test_i2c() {
 
     // i2c.set_config(|c| c.set_timing(0x8.into(), 0x3.into(), 0x0.into(), 0xd.into(), 0x12.into()));
     i2c.set_enabled(false);
-    // i2c.set_timingr(|_| Timingr(0x00300619));
-        // Set Peripheral Clock Frequency = 42Mhz
-    i2c.with_cr2(|r| r.set_freq(42));
     
-    // Set Clock Speed = 100khz (divisor = 42Mhz / 100khz = 420)
-    i2c.set_ccr(|_| Ccr(0).set_f_s(0).set_duty(0).set_ccr(420));
-
-    // Set Rise Time Register = 43
-    i2c.set_trise(|_| Trise(0).set_trise(43));
+    i2c.set_timingr(|r| r
+        .set_presc(0x0)
+        .set_scldel(0x3)
+        .set_sdadel(0x0)
+        .set_sclh(0xF)
+        .set_scll(0x12)
+    );
 
     assert_eq!(i2c.read_reg(addr, 0x0c), 0xc4);
    
@@ -176,78 +175,78 @@ fn test_i2c() {
 }
 
 
-fn test_spi_lora() {
-    use board::hal::gpio::*;
-    use board::hal::spi::*;
+// fn test_spi_lora() {
+//     use board::hal::gpio::*;
+//     use board::hal::spi::*;
 
-    let spi = SPI1;
-    let port = GPIOA;
-    let spi_miso = PA6; // A5
-    let spi_mosi = PA7; // A6
-    let spi_sck = PA5;
-    let spi_nss = PD14; // D10
+//     let spi = SPI1;
+//     let port = GPIOA;
+//     let spi_miso = PA6; // A5
+//     let spi_mosi = PA7; // A6
+//     let spi_sck = PA5;
+//     let spi_nss = PD14; // D10
 
-    spi.rcc_enable();
-    port.rcc_enable();
-    GPIOD.rcc_enable();
+//     spi.rcc_enable();
+//     port.rcc_enable();
+//     GPIOD.rcc_enable();
 
-    // NOTE: Pins must be set with output speed HIGH or leading edge
-    // of transmission will occasionally be missed.
+//     // NOTE: Pins must be set with output speed HIGH or leading edge
+//     // of transmission will occasionally be missed.
 
-    spi_miso.mode_spi_miso(&spi).speed_high().pull_up();
-    spi_mosi.mode_spi_mosi(&spi).speed_high().push_pull();
-    spi_sck.mode_spi_sck(&spi).speed_high().push_pull();
-    spi_nss.mode_output();
+//     spi_miso.mode_spi_miso(&spi).speed_high().pull_up();
+//     spi_mosi.mode_spi_mosi(&spi).speed_high().push_pull();
+//     spi_sck.mode_spi_sck(&spi).speed_high().push_pull();
+//     spi_nss.mode_output();
 
-    spi.set_config(|cfg| cfg
-        .set_frame_size(FrameSize::Bits8)
-        .set_master(true)
-        .set_baud_divider(0b100.into())
-    );
+//     spi.set_config(|cfg| cfg
+//         .set_frame_size(FrameSize::Bits8)
+//         .set_master(true)
+//         .set_baud_divider(0b100.into())
+//     );
 
-    spi.set_output_enabled(true).set_enabled(true);
-
-
-    let test_data = [(0x42, 0x12), (0x01, 0x09), (0x02, 0x1a), (0x03, 0x0b), (0x04, 0x00), (0x05, 0x52), (0x06, 0x6c)];
-
-    for &(tx, rx) in test_data.iter() {
-        // println!("0x{:02x}: 0x{:02x}", tx, rx);
-        assert_eq!(reg_read(&spi, &spi_nss, tx), rx);
-    }
+//     spi.set_output_enabled(true).set_enabled(true);
 
 
-    println!("[pass] SPI OK");
-    spi.rcc_disable();
-    spi_sck.mode_analog();
-    spi_mosi.mode_analog();
-    spi_miso.mode_analog();
-    spi_nss.mode_analog();
+//     let test_data = [(0x42, 0x12), (0x01, 0x09), (0x02, 0x1a), (0x03, 0x0b), (0x04, 0x00), (0x05, 0x52), (0x06, 0x6c)];
 
-    fn transfer(spi: &SpiPeriph, nss: &GpioPin, src: &[u8], dst: &mut[u8]) {
-        let mut i = 0;
-        let mut j = 0;
-        nss.set_output(false);
-        loop {
-            if i < src.len() && spi.can_tx() {
-                spi.tx(src[i]);
-                i += 1;
-            }
-            if j < dst.len() && spi.can_rx() {
-                dst[j] = spi.rx();
-                j += 1;
-            }
-            if j == dst.len() {
-                break;
-            }        
-        }
-        nss.set_output(true);
-    }
+//     for &(tx, rx) in test_data.iter() {
+//         // println!("0x{:02x}: 0x{:02x}", tx, rx);
+//         assert_eq!(reg_read(&spi, &spi_nss, tx), rx);
+//     }
 
-    fn reg_read(spi: &SpiPeriph, nss: &GpioPin, reg: u8) -> u8 {
-        let cmd = [reg, 0xff];
-        let mut buf = [0u8, 0u8];
-        transfer(spi, nss, &cmd, &mut buf);
-        buf[1]
-    }
+
+//     println!("[pass] SPI OK");
+//     spi.rcc_disable();
+//     spi_sck.mode_analog();
+//     spi_mosi.mode_analog();
+//     spi_miso.mode_analog();
+//     spi_nss.mode_analog();
+
+//     fn transfer(spi: &SpiPeriph, nss: &GpioPin, src: &[u8], dst: &mut[u8]) {
+//         let mut i = 0;
+//         let mut j = 0;
+//         nss.set_output(false);
+//         loop {
+//             if i < src.len() && spi.can_tx() {
+//                 spi.tx(src[i]);
+//                 i += 1;
+//             }
+//             if j < dst.len() && spi.can_rx() {
+//                 dst[j] = spi.rx();
+//                 j += 1;
+//             }
+//             if j == dst.len() {
+//                 break;
+//             }        
+//         }
+//         nss.set_output(true);
+//     }
+
+//     fn reg_read(spi: &SpiPeriph, nss: &GpioPin, reg: u8) -> u8 {
+//         let cmd = [reg, 0xff];
+//         let mut buf = [0u8, 0u8];
+//         transfer(spi, nss, &cmd, &mut buf);
+//         buf[1]
+//     }
     
-}
+// }
