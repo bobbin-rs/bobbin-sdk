8ec8331c6e7f455933537a2eb78d509d6363c2e1
Jonathan Soo <jcsoo@agora.com>
2017-11-05 15:03:31 -0500

    Enable I2C test for nucleo-l432kc

--- boards/nucleo-l432kc/src/main.rs
+++ boards/nucleo-l432kc/src/main.rs
@@ -18,8 +18,9 @@ pub extern "C" fn main() -> ! {
     test_exti();
     // test_lpuart();
     // test_usart();
-    test_spi_lora();
-    // test_i2c();
+    test_i2c();
+    // test_spi_lora();
+    
     println!("[done] All tests passed");
     loop {}
 }
@@ -423,64 +424,64 @@ fn test_lpuart() {
 //     println!("[pass] SPI OK");
 // }
 
-// fn test_i2c() {
-//     use board::hal::gpio::*;
-//     use board::hal::i2c::*;
-//     use board::common::bits::*;
+fn test_i2c() {
+    use board::hal::gpio::*;
+    use board::hal::i2c::*;
+    use board::common::bits::*;
 
-//     let addr: U7 = U7::from(0x60);
+    let addr: U7 = U7::from(0x60);
 
-//     let i2c = I2C1;
-//     let i2c_port = GPIOB;
-//     let i2c_scl = PB6; // D4
-//     let i2c_sda = PB7; // D5
+    let i2c = I2C1;
+    let i2c_port = GPIOB;
+    let i2c_scl = PB6; // D4
+    let i2c_sda = PB7; // D5
 
-//     i2c.rcc_enable();
-//     i2c_port.rcc_enable();
+    i2c.rcc_enable();
+    i2c_port.rcc_enable();
 
-//     // Attached to MPL3115A2 
-//     // NOTE: SCL and SCA must have pull-up resistors.
+    // Attached to MPL3115A2 
+    // NOTE: SCL and SCA must have pull-up resistors.
 
-//     i2c_scl.mode_i2c_scl(&i2c).open_drain();
-//     i2c_sda.mode_i2c_sda(&i2c).open_drain();
+    i2c_scl.mode_i2c_scl(&i2c).open_drain();
+    i2c_sda.mode_i2c_sda(&i2c).open_drain();
 
-//     // println!("# Configuring I2C");
+    // println!("# Configuring I2C");
 
-//     // i2c.set_config(|c| c.set_timing(0x8.into(), 0x3.into(), 0x0.into(), 0xd.into(), 0x12.into()));
-//     i2c.set_enabled(false);
-//     // i2c.set_timingr(|_| Timingr(0x00300619));
-//     i2c.set_timingr(|r| r
-//         .set_presc(0x0)
-//         .set_scldel(0x3)
-//         .set_sdadel(0x0)
-//         .set_sclh(0xF)
-//         .set_scll(0x12)
-//     );
-//     assert_eq!(i2c.read_reg(addr, 0x0c), 0xc4);
+    // i2c.set_config(|c| c.set_timing(0x8.into(), 0x3.into(), 0x0.into(), 0xd.into(), 0x12.into()));
+    i2c.set_enabled(false);
+    // i2c.set_timingr(|_| Timingr(0x00300619));
+    i2c.set_timingr(|r| r
+        .set_presc(0x0)
+        .set_scldel(0x3)
+        .set_sdadel(0x0)
+        .set_sclh(0xF)
+        .set_scll(0x12)
+    );
+    assert_eq!(i2c.read_reg(addr, 0x0c), 0xc4);
     
-//     // println!("Mode:  0x{:08x}", i2c.read_reg(addr, 0x26));
-    
-
-//     i2c.write_reg(addr, 0x26, 0xb8); // OSR = 128
-//     i2c.write_reg(addr, 0x13, 0x06); // Enable Data Flags
-//     i2c.write_reg(addr, 0x26, 0xb9); // Set Active
-//     // println!("Mode:  0x{:08x}", i2c.read_reg(addr, 0x26));
-
-//     loop {
-//         while i2c.read_reg(addr, 0x00) != 0x04 {}    
-//         let mut buf = [0u8; 5];
-//         i2c.transfer(addr, &[0x01], &mut buf);
-//         println!("# {:?}", buf);
-//         // assert!(buf[0] == 0 && buf[1] != 0 && buf[2] != 0 && buf[3] != 0 && buf[4] != 0);
-//         break
-//     }
-
+    // println!("Mode:  0x{:08x}", i2c.read_reg(addr, 0x26));
     
 
-//     i2c_port.rcc_disable();
-//     i2c.rcc_disable();
-//     println!("[pass] I2C OK");
-// }
+    i2c.write_reg(addr, 0x26, 0xb8); // OSR = 128
+    i2c.write_reg(addr, 0x13, 0x06); // Enable Data Flags
+    i2c.write_reg(addr, 0x26, 0xb9); // Set Active
+    // println!("Mode:  0x{:08x}", i2c.read_reg(addr, 0x26));
+
+    loop {
+        while i2c.read_reg(addr, 0x00) != 0x04 {}    
+        let mut buf = [0u8; 5];
+        i2c.transfer(addr, &[0x01], &mut buf);
+        println!("# {:?}", buf);
+        // assert!(buf[0] == 0 && buf[1] != 0 && buf[2] != 0 && buf[3] != 0 && buf[4] != 0);
+        break
+    }
+
+    
+
+    i2c_port.rcc_disable();
+    i2c.rcc_disable();
+    println!("[pass] I2C OK");
+}
 
 /// RFM9x LoRa Radio on pins D10-D13
 fn test_spi_lora() {
