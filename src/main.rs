74319bc506d08f9b042afec766865022e4ce11b6
Jonathan Soo <jcsoo@agora.com>
2017-11-16 11:34:11 -0500

    Add new output argument

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,36 @@ use chip::writer::{Context, write_device};
 use xml::reader::EventReader;
 
 use std::fs::File;
-use std::io::{BufReader};
+use std::io::{BufReader, BufWriter};
 
 use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("svd2chip")
         .arg(Arg::with_name("input"))
+        .arg(Arg::with_name("output"))
         .get_matches();
 
-    if !matches.is_present("input") {
+    let input = if let Some(input) = matches.value_of("input") {
+        BufReader::new(File::open(input).unwrap())
+    } else {
         println!("{}", matches.usage());
         std::process::exit(1);
-    }
-    let input = matches.value_of("input").unwrap();
-    let mut out = std::io::stdout();
+    };
 
-    let file = File::open(input).unwrap();
-    let file = BufReader::new(file);
-    let mut reader = EventReader::new(file);
+    let mut output = if let Some(output) = matches.value_of("output") {
+        BufWriter::new(File::create(output).unwrap())
+    } else {
+        println!("{}", matches.usage());
+        std::process::exit(1);
+    };
+
+    let mut reader = EventReader::new(input);
     let doc = read_document(&mut reader).unwrap();
     let dev = doc.device;
 
     let mut ctx = Context {
-        out: &mut out,
+        out: &mut output,
     };
 
     write_device(&mut ctx, 0, &dev).unwrap();
