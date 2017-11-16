74e87fe622bde04fc8d5634fa9b7e69daa9eeafc
Jonathan Soo <jcsoo@agora.com>
2017-11-16 12:20:44 -0500

    Generate periphs as separate files

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ extern crate clap;
 extern crate bobbin_svd;
 
 use bobbin_svd::*;
-use chip::writer::{Context, write_device};
+use chip::writer::*;
 
 use xml::reader::EventReader;
 
-use std::fs::File;
+use std::path::Path;
+use std::fs::{File, create_dir};
 use std::io::{BufReader, BufWriter};
 
 use clap::{Arg, App};
@@ -19,27 +20,54 @@ fn main() {
         .arg(Arg::with_name("output"))
         .get_matches();
 
-    let input = if let Some(input) = matches.value_of("input") {
-        BufReader::new(File::open(input).unwrap())
+    let input_path = if let Some(input) = matches.value_of("input") {
+        Path::new(input)
     } else {
         println!("{}", matches.usage());
         std::process::exit(1);
     };
 
-    let mut output = if let Some(output) = matches.value_of("output") {
-        BufWriter::new(File::create(output).unwrap())
+    let output_dir = if let Some(output) = matches.value_of("output") {
+        Path::new(output)
     } else {
         println!("{}", matches.usage());
         std::process::exit(1);
-    };
+    };    
+    if !output_dir.is_dir() {
+        println!("Output argument must be directory");
+        std::process::exit(1);
+    }
+    
+    let output_path = output_dir.with_file_name("lib.rx");        
 
-    let mut reader = EventReader::new(input);
+    let mut reader = EventReader::new(BufReader::new(File::open(input_path).unwrap()));
+    let mut writer = BufWriter::new(File::create(&output_path).unwrap());
     let doc = read_document(&mut reader).unwrap();
     let dev = doc.device;
 
     let mut ctx = Context {
-        out: &mut output,
+        out: &mut writer,
     };
 
-    write_device(&mut ctx, 0, &dev).unwrap();
+    write_device_open(&mut ctx, 0, &dev).unwrap();
+    for p in dev.peripherals.iter() {
+        let periph_name = &p.name.to_lowercase();
+        write_peripheral_include(&mut ctx, 1, &format!("periph/{}.rx", periph_name)).unwrap();
+    }
+    write_device_close(&mut ctx, 0, &dev).unwrap();
+
+    let periph_dir = output_dir.join("periph");
+    if !periph_dir.exists() {
+        create_dir(&periph_dir).unwrap();
+    }
+
+    for p in dev.peripherals.iter() {
+        let periph_name = &p.name.to_lowercase();
+        let periph_path = periph_dir.join(periph_name).with_extension("rx");
+        let mut writer = BufWriter::new(File::create(periph_path).unwrap());
+        let mut ctx = Context {
+            out: &mut writer,
+        };        
+        write_peripheral(&mut ctx, 0, p).unwrap();
+    }
 }
