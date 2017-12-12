9cb50502782283ae7de9d1a0e4b5cce4bcac0d8d
Jonathan Soo <jcsoo@agora.com>
2017-12-12 10:42:02 -0500

    Minimal allocator (not implemented safely)

--- boards/nucleo-f746zg/src/lib.rs
+++ boards/nucleo-f746zg/src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![feature(lang_items)]
-
+#![feature(alloc, core_intrinsics, offset_to, allocator_api, const_fn, global_allocator)]
 #![feature(asm)]
 
 extern crate r0;
-
 extern crate log;
+extern crate alloc;
 
 #[macro_use] pub mod itm;
 #[macro_use] pub mod console;
@@ -23,6 +23,11 @@ pub mod clock;
 pub mod led;
 pub mod btn;
 pub mod tim;
+
+pub mod heap;
+
+#[global_allocator]
+static ALLOCATOR: heap::Heap = heap::Heap::empty();
 
 // pub fn delay(ms: u32) {
 //     for _ in 0..1_000_000 {
