d1960a2b56fd242310e2a60a0b9bb97f2951082f
Jonathan Soo <jcsoo@agora.com>
2018-02-26 17:14:30 -0500

    Switch to new Channel and Irq

--- mcu/bobbin-common/src/channel.rs
+++ mcu/bobbin-common/src/channel.rs
@@ -3,14 +3,63 @@ use signal::SignalType;
 
 #[macro_export]
 macro_rules! channel {
-    ($id:ident, $ty:ident, $pty:ident, $num:expr) => {
+    ($id:ident, $ty:ident, $periph_id:ident, $periph_type:ident, $base_id:ident, $base_type:ident, $base_periph:ident, $index:expr) => (    
         pub const $id: $ty = $ty {};
+        pub const $base_id: $base_type = $base_type { periph: $base_periph, index: $index };
         #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
         pub struct $ty {}
-        impl $crate::channel::Channel<$pty> for $ty {
-            fn index(&self) -> u8 { $num }
+        impl $crate::channel::Channel<$periph_type> for $ty {
+            #[inline(always)]
+            fn periph(&self) -> $periph_type { $periph_id }
+            #[inline(always)]            
+            fn index(&self) -> u8 { $index }
+        }
+
+        impl ::core::ops::Deref for $ty {
+            type Target = $base_type;
+            #[inline(always)]            
+            fn deref(&self) -> &$base_type {
+                &$base_id
+            }
+        }
+
+        impl Into<$base_type> for $ty {
+            #[inline(always)]
+            fn into(self) -> $base_type {
+                $base_id
+            }
+        }            
+    )
+}
+
+
+#[macro_export]
+macro_rules! xchannel {
+    ($id:ident, $ty:ident, $periph_id:ident, $periph_type:ident, $base_id:ident, $base_type:ident, $base_periph:ident, $index:expr) => (
+        pub const $id: $ty = $ty {};     
+        pub const $base_id: $base_type = $base_type { periph: $base_periph, index: $index };
+        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+        pub struct $ty {}
+        impl Channel<$periph_type> for $ty {
+            #[inline(always)]
+            fn periph(&self) -> $periph_type { $periph_id }
+            #[inline(always)]            
+            fn index(&self) -> usize { $index }
+        }
+        impl Deref for $ty {
+            type Target = $base_type;
+            #[inline(always)]            
+            fn deref(&self) -> &$base_type {
+                &$base_id
+            }
+        }
+        impl Into<$base_type> for $ty {
+            #[inline(always)]
+            fn into(self) -> $base_type {
+                $base_id
+            }
         }        
-    }
+    )    
 }
 
 #[macro_export]
