106a0c05dd95f77b1b719e2f10cf0121997f768a
Jonathan Soo <jcsoo@agora.com>
2018-02-28 10:07:17 -0500

    Prefix gate trait methods

--- mcu/bobbin-common/src/gate.rs
+++ mcu/bobbin-common/src/gate.rs
@@ -1,35 +1,35 @@
 use ::bits;
 
 pub trait GateEn {
-    fn en(&self) -> bits::U1;
-    fn set_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
-    fn enable(&self) -> &Self { self.set_en(true) }
-    fn disable(&self) -> &Self { self.set_en(false) }
+    fn gate_en(&self) -> bits::U1;
+    fn set_gate_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
+    fn gate_enable(&self) -> &Self { self.set_gate_en(true) }
+    fn gate_disable(&self) -> &Self { self.set_gate_en(false) }
 }    
 
 pub trait GateRst {
-    fn rst(&self) -> bits::U1;
-    fn set_rst<V: Into<bits::U1>>(&self, value: V) -> &Self;
-    fn toggle_reset(&self) -> &Self { self.set_rst(true).set_rst(false) }
+    fn gate_rst(&self) -> bits::U1;
+    fn set_gate_rst<V: Into<bits::U1>>(&self, value: V) -> &Self;
+    fn toggle_gate_reset(&self) -> &Self { self.set_gate_rst(true).set_gate_rst(false) }
 }    
 
 pub trait GateSleepEn {
-    fn sleep_en(&self) -> bits::U1;
-    fn set_sleep_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
-    fn sleep_enable(&self) -> &Self { self.set_sleep_en(true) }
-    fn sleep_disable(&self) -> &Self { self.set_sleep_en(false) }
+    fn gate_sleep_en(&self) -> bits::U1;
+    fn set_gate_sleep_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
+    fn gate_sleep_enable(&self) -> &Self { self.set_gate_sleep_en(true) }
+    fn gate_sleep_disable(&self) -> &Self { self.set_gate_sleep_en(false) }
 }
 
 pub trait GateDeepSleepEn {
-    fn deep_sleep_en(&self) -> bits::U1;
-    fn set_deep_sleep_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
-    fn deep_sleep_enable(&self) -> &Self { self.set_deep_sleep_en(true) }
-    fn deep_sleep_disable(&self) -> &Self { self.set_deep_sleep_en(false) }
+    fn gate_deep_sleep_en(&self) -> bits::U1;
+    fn set_gate_deep_sleep_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
+    fn gate_deep_sleep_enable(&self) -> &Self { self.set_gate_deep_sleep_en(true) }
+    fn gate_deep_sleep_disable(&self) -> &Self { self.set_gate_deep_sleep_en(false) }
 }
 
 pub trait GateStopEn {
-    fn stop_en(&self) -> bits::U1;
-    fn set_stop_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
-    fn stop_enable(&self) -> &Self { self.set_stop_en(true) }
-    fn stop_disable(&self) -> &Self { self.set_stop_en(false) }
+    fn gate_stop_en(&self) -> bits::U1;
+    fn set_gate_stop_en<V: Into<bits::U1>>(&self, value: V) -> &Self; 
+    fn gate_stop_enable(&self) -> &Self { self.set_gate_stop_en(true) }
+    fn gate_stop_disable(&self) -> &Self { self.set_gate_stop_en(false) }
 }
