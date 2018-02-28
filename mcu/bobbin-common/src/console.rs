ccce64ba762b122089c7392c054f44cf7b194990
Jonathan Soo <jcsoo@agora.com>
2018-02-28 15:34:17 -0500

    Move console macros

--- mcu/bobbin-common/src/console.rs
+++ mcu/bobbin-common/src/console.rs
@@ -1,28 +1,5 @@
 use core::fmt::{self, Write};
 
-/// Macro for sending `print!`-formatted messages over the Console
-#[macro_export]
-macro_rules! print {
-    ($s:expr) => {
-        $crate::console::write_str($s)
-    };
-    ($($arg:tt)*) => {
-        $crate::console::write_fmt(format_args!($($arg)*))
-    };
-}
-
-/// Macro for sending `print!`-formatted messages over the Console, with a
-/// newline
-#[macro_export]
-macro_rules! println {
-    ($fmt:expr) => {
-        print!(concat!($fmt, "\n"))
-    };
-    ($fmt:expr, $($arg:tt)*) => {
-        print!(concat!($fmt, "\n"), $($arg)*)
-    };
-}
-
 pub trait Putc {
     fn console_putc(&self, c: u8);
 }
--- mcu/bobbin-common/src/macros.rs
+++ mcu/bobbin-common/src/macros.rs
@@ -207,3 +207,26 @@ macro_rules! irq_type {
         impl IrqType for $ty {}        
     }
 }
+
+/// Macro for sending `print!`-formatted messages over the Console
+#[macro_export]
+macro_rules! print {
+    ($s:expr) => {
+        $crate::console::write_str($s)
+    };
+    ($($arg:tt)*) => {
+        $crate::console::write_fmt(format_args!($($arg)*))
+    };
+}
+
+/// Macro for sending `print!`-formatted messages over the Console, with a
+/// newline
+#[macro_export]
+macro_rules! println {
+    ($fmt:expr) => {
+        print!(concat!($fmt, "\n"))
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        print!(concat!($fmt, "\n"), $($arg)*)
+    };
+}
