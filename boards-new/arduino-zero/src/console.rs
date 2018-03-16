b30ba4bfa77a78382450cbd70e83afc764170dc9
Jonathan Soo <jcsoo@agora.com>
2018-03-16 17:40:38 -0400

    SAMD sercom WIP

--- boards-new/arduino-zero/src/console.rs
+++ boards-new/arduino-zero/src/console.rs
@@ -1,10 +1,41 @@
+pub use mcu::bobbin_common::console::*;
+use common::periph::IntoPeriph;
+use common::configure::Configure;
+use common::enabled::Enabled;
+use mcu::pin::*;
+use mcu::sercom::*;
+use mcu::gclk;
+
+pub const SERCOM: Sercom5 = SERCOM5;
+pub const SERCOM_TX: Pb22 = PB22;
+pub const SERCOM_RX: Pb23 = PB23;
+
 pub fn init() {
-}
+    SERCOM.gate_enable();
+    SERCOM_RX.port().gate_enable();
+    SERCOM_TX.port().gate_enable();
+    // Set GCLK_GEN0 as source for SERCOM
 
-pub fn write_str(_s: &str) {
-    unimplemented!()
-}
+    gclk::GCLK.set_clkctrl(|r| r
+        .set_id(0x14 + 5)
+        .set_gen(0x0)
+        .set_clken(1)
+    );
+    // Wait for synchronization
+    while gclk::GCLK.status().syncbusy() != 0 {}
 
-pub fn write_u32(_v: u32, _radix: u32) {
-    unimplemented!()
+    // Set Pin Configuration
+    SERCOM_TX.mode_pad_2(&SERCOM);
+    SERCOM_RX.mode_pad_3(&SERCOM);
+
+    SERCOM
+        .set_config(|c| c
+            .set_mode_usart_int()
+            .set_baud(63018)
+            .set_txpo(1)
+            .set_rxpo(3)
+        )
+        .set_enabled(true);
+
+    set_console(Console::new(SERCOM.into_periph()));
 }
--- mcu/bobbin-sam/samd21/src/hal/sercom.rs
+++ mcu/bobbin-sam/samd21/src/hal/sercom.rs
@@ -1,1 +1,154 @@
 pub use periph::sercom::*;
+
+pub use bobbin_common::console::Putc;
+pub use bobbin_common::configure::*;
+pub use bobbin_common::enabled::*;
+pub use bobbin_common::serial::*;
+// pub use super::pm::PmEnabled;
+
+use bobbin_common::bits::*;
+
+// NOTE: Before usage, power up and set clocks
+
+// pm::set_sercom_enabled(sercom, true);
+
+// // Set GCLK_GEN0 as source for SERCOM
+
+// gclk::GCLK.set_clkctrl(gclk::Clkctrl(0)
+//     .set_id(0x14 + sercom_index(sercom))
+//     .set_gen(0x0)
+//     .set_clken(1)
+// );
+// // Wait for synchronization
+// while gclk::GCLK.status().syncbusy() != 0 {}
+
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u8)]
+pub enum Mode {
+    UsartExternal = 0x0,
+    UsartInternal = 0x1,
+    SpiSlave = 0x2,
+    SpiMaster = 0x3,
+    I2cSlave = 0x4,
+    I2cMaster = 0x5,
+}
+
+pub struct Config {
+    ctrla: usart::Ctrla,
+    ctrlb: usart::Ctrlb,
+    baud: usart::Baud,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            ctrla: usart::Ctrla(0).set_dord(0x1),
+            ctrlb: usart::Ctrlb(0).set_rxen(0x1).set_txen(0x1).set_chsize(0x0),
+            baud: usart::Baud(0),
+        }
+    }
+}
+
+impl Config {
+    pub fn set_mode_usart_ext(self) -> Self {
+        self.set_mode(Mode::UsartExternal)
+    }
+
+    pub fn set_mode_usart_int(self) -> Self {
+        self.set_mode(Mode::UsartInternal)
+    }
+
+    pub fn set_mode(mut self, mode: Mode) -> Self {
+        self.ctrla = self.ctrla.set_mode(mode as u8);
+        self
+    }
+
+    pub fn set_baud(mut self, value: u16) -> Self {
+        self.baud = usart::Baud(value);
+        self
+    }
+
+    pub fn set_txpo<V: Into<U2>>(mut self, value: V) -> Self {
+        self.ctrla = self.ctrla.set_txpo(value.into());
+        self
+    }
+
+    pub fn set_rxpo<V: Into<U2>>(mut self, value: V) -> Self {
+        self.ctrla = self.ctrla.set_rxpo(value.into());
+        self
+    }
+}
+
+impl Configure<Config> for SercomPeriph {
+    fn config(&self) -> Config {
+        let s = self.usart();
+        Config {
+            ctrla: s.ctrla(),
+            ctrlb: s.ctrlb(),
+            baud: s.baud(),
+        }
+    }
+
+    fn configure(&self, cfg: Config) -> &Self {
+        let s = self.usart();
+
+        // Reset peripheral
+        s.set_ctrla(|r| r.set_swrst(0x1));
+
+        // Wait for reset
+        while s.ctrla().swrst() != 0 {}
+
+        // Update CTRLA
+        s.set_ctrla(|_| cfg.ctrla);
+
+        // Update CTRLB
+        s.set_ctrlb(|_| cfg.ctrlb);
+
+        // Wait for synchronization
+        while s.syncbusy().ctrlb() != 0 {}
+
+        // Update BAUD
+        s.set_baud(|_| cfg.baud);
+
+        self
+    }
+}
+
+impl Enabled for SercomPeriph {
+    fn enabled(&self) -> bool {
+        self.usart().ctrla().test_enable()
+    }
+
+    fn set_enabled(&self, value: bool) -> &Self {
+        self.usart().with_ctrla(|r| r.set_enable(value));
+        self
+    }        
+}
+
+impl SerialTx<u8> for SercomPeriph {    
+    fn can_tx(&self) -> bool {
+        self.usart().intflag().dre() != 0
+    }
+
+    fn tx(&self, c: u8) -> &Self {
+        self.usart().set_data(|r| r.set_data(c));
+        self
+    }
+}
+
+impl SerialRx<u8> for SercomPeriph {
+    fn can_rx(&self) -> bool {
+        self.usart().intflag().rxc() != 0
+    }
+
+    fn rx(&self) -> u8 {
+        self.usart().data().data().value() as u8
+    }
+}
+impl Putc for SercomPeriph {
+    fn console_putc(&self, c: u8) {
+        while !self.can_tx() {}
+        self.tx(c);
+    }
+}
