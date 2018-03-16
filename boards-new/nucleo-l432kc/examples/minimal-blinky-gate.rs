baceed8bf7e36af39e86a519822a4ee259cb823f
Jonathan Soo <jcsoo@agora.com>
2018-03-16 14:20:17 -0400

    Minimal blinky gate for L432

---
+++ boards-new/nucleo-l432kc/examples/minimal-blinky-gate.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+extern crate nucleo_l432kc as board;
+
+// LED0 = PB3;
+
+use board::mcu::pin::*;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+
+    PB3.port().gate_enable();
+
+    // Set PB3 Mode = Output
+    PB3.mode_output();
+
+    loop {
+        PB3.toggle_output();
+        // Delay approx 1/2 second
+        for _ in 0..1_000_000 { unsafe { asm!("nop") } } 
+    }
+}
