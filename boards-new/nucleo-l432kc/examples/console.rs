515cb4bae18714ce4349feefe62d439fa802d435
Jonathan Soo <jcsoo@agora.com>
2018-03-16 16:23:27 -0400

    Console on f746 and l432

---
+++ boards-new/nucleo-l432kc/examples/console.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[macro_use]
+extern crate nucleo_l432kc as board;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    println!("Running Console");
+    loop {
+        println!("Tick...");
+        board::delay(500);
+    }
+}
