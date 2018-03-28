eddc2156202f82d41174e5e15647998a6aadde73
Jonathan Soo <jcsoo@agora.com>
2018-03-28 14:42:22 -0400

    Remove unused code

--- src/codegen/periph.rs
+++ src/codegen/periph.rs
@@ -198,9 +198,11 @@ pub fn mkdir(path: &Path) -> Result<()> {
     }
     Ok(())
 }
+
 pub fn copy_file(src_path: &Path, dst_path: &Path) -> Result<()> {
     copy_file_with(src_path, dst_path, |s| s)
 }
+
 pub fn copy_file_with<F: FnOnce(String) -> String>(src_path: &Path, dst_path: &Path, f: F) -> Result<()> {
     let mut src = File::open(src_path)?;
     let mut dst = File::create(dst_path)?;
@@ -210,123 +212,3 @@ pub fn copy_file_with<F: FnOnce(String) -> String>(src_path: &Path, dst_path: &P
     dst.write(&data.as_bytes())?;    
     Ok(())
 }
-
-
-pub fn gen_register<W: Write>(out: &mut W, r: &Register, size: &'static str) -> Result<()> {
-    let reg_struct = to_camel(&r.name);    
-
-    if let Some(ref description) = r.description {
-        gen_doc(out, 4, description)?;
-    }
-    writeln!(out, "    #[derive(PartialEq, Eq, Clone, Copy)]")?;
-    writeln!(out, "")?;
-    writeln!(out, "    pub struct {}({});", reg_struct, size)?;
-    writeln!(out, "")?;
-
-    writeln!(out, "    impl From<{}> for {} {{", size, reg_struct)?;
-    writeln!(out, "        fn from(other: {}) -> Self {{ {}(other) }}", size, reg_struct)?;
-    writeln!(out, "    }}")?;    
-    writeln!(out, "")?;
-
-    writeln!(out, "    impl From<{}> for {} {{", reg_struct, size)?;
-    writeln!(out, "        fn from(other: {}) -> Self {{ other.0 }}", reg_struct)?;
-    writeln!(out, "    }}")?;
-    writeln!(out, "")?;
-
-
-    writeln!(out, "    impl {} {{", reg_struct)?;
-    writeln!(out, "        pub fn value(&self) -> {} {{ self.0 }}", size)?;
-    writeln!(out, "")?;
-
-    for f in r.fields.iter() {
-        assert!(f.dim.is_none(), "{}: Field arrays are not currently implemented", f.name);
-        let f_getter = field_getter(&f.name);
-        let f_setter = field_setter(&f.name);
-        let f_offset = f.bit_offset;
-        let f_width = f.bit_width;
-        let f_lo = f_offset;
-        let f_hi = (f_offset + f_width) - 1;    
-        let f_mask = if f_width == 64 {
-            u64::max_value()
-        } else {
-            ((1 << f_width) - 1)
-        };
-        let f_bits = if f_width > 1 {
-            format!("[{}:{}]", f_hi, f_lo)
-        } else {
-            format!("[{}]", f_lo)
-        };  
-        if let Some(ref description) = f.description {
-            gen_doc(out, 8, description)?;
-        }                  
-        writeln!(out, "        pub fn {}(&self) -> {} {{", f_getter, size)?;
-        writeln!(out, "            ((self.0 as {}) >> {}) & 0x{:x} // {}", size, f_offset, f_mask, f_bits)?;
-        writeln!(out, "        }}")?;    
-        writeln!(out, "    ")?;
-
-        writeln!(out, "        pub fn {}(mut self, value: {}) -> Self {{", f_setter, size)?;
-        writeln!(out, "            assert!((value & !0x{:x}) == 0);", f_mask)?;
-        writeln!(out, "            self.0 &= !(0x{:x} << {});", f_mask, f_offset)?;
-        writeln!(out, "            self.0 |= value << {};", f_offset)?;
-        writeln!(out, "            self")?;
-        writeln!(out, "        }}")?;    
-        writeln!(out, "    ")?;
-    }
-    writeln!(out, "    }}")?;
-    writeln!(out, "")?;
-
-    writeln!(out, "    impl ::core::fmt::Display for {} {{", reg_struct)?;
-    writeln!(out, "        fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {{")?;
-    writeln!(out, "            self.0.fmt(f)")?;
-    writeln!(out, "        }}")?;
-    writeln!(out, "    }}")?;        
-    writeln!(out, "")?;
-
-
-    writeln!(out, "    impl ::core::fmt::Debug for {} {{", reg_struct)?;
-    writeln!(out, "        fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {{")?;
-    match size {
-        "u8" => writeln!(out, "            write!(f, \"[0x{{:02x}}\", self.0)?;")?,
-        "u16" => writeln!(out, "            write!(f, \"[0x{{:04x}}\", self.0)?;")?,
-        "u32" => writeln!(out, "            write!(f, \"[0x{{:08x}}\", self.0)?;")?,
-        s @ _ => panic!("invalid size: {}", s),
-    }        
-    
-    for f in r.fields.iter() {
-        let f_name = field_name(&f.name);
-        let f_getter = field_getter(&f.name);
-
-        if let Some(dim) = f.dim {
-            for i in 0..dim {
-                match f.bit_width {
-                    1 => {
-                        writeln!(out, "            if self.{}({}) != 0 {{ write!(f, \" {}[{}]\")? }}", f_getter, i, f_getter, i)?;
-                    },
-                    32 => {},
-                    _ => {
-                        writeln!(out, "            if self.{}({}) != 0 {{ write!(f, \" {}[{}]=0x{{:x}}\", self.{}({})? }}", f_getter, i, f_name, i, f_getter, i)?;
-                    }
-                }                    
-            }
-        } else {
-            match f.bit_width {
-                1 => {
-                    writeln!(out, "            if self.{}() != 0 {{ write!(f, \" {}\")? }}", f_getter, f_getter)?;
-                },
-                32 => {},
-                _ => {
-                    writeln!(out, "            if self.{}() != 0 {{ write!(f, \" {}=0x{{:x}}\", self.{}())? }}", f_getter, f_name, f_getter)?;
-                }
-            }
-        }
-        
-    }
-    writeln!(out, "            write!(f, \"]\")?;")?;
-    writeln!(out, "            Ok(())")?;
-    writeln!(out, "        }}")?;
-    writeln!(out, "    }}")?;        
-    writeln!(out, "")?;    
-
-    Ok(())
-}
-
