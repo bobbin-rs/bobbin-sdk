baa9ceeeabd87969c3d1ecd675a470e5e50e1f25
Jonathan Soo <jcsoo@agora.com>
2018-03-30 11:29:48 -0400

    I2C and SPI demo for L432KC

---
+++ boards-new/nucleo-l432kc/examples/mb85rc.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate nucleo_l432kc as board;
+extern crate embedded_hal as hal;
+extern crate examples;
+
+use board::mcu::pin::*;
+use board::mcu::i2c::*;
+
+// A5 = PB6 = I2C1_SCL
+// A4 = PB7 = I2C1_SDA
+
+// Address = 0x35
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    let brd = board::board();
+    println!("Running I2C");
+    
+    let i2c = I2C1;
+    let i2c_scl = PB6; // A5
+    let i2c_sda = PB7; // A4
+
+    i2c.gate_enable();
+    i2c_scl.port().gate_enable();
+    i2c_sda.port().gate_enable();
+
+    i2c_scl.connect_to(i2c);
+    i2c_sda.connect_to(i2c);
+
+    i2c_scl.open_drain();
+    i2c_sda.open_drain();
+
+    println!("# Configuring I2C");
+
+    // i2c.set_config(|c| c.set_timing(0x8.into(), 0x3.into(), 0x0.into(), 0xd.into(), 0x12.into()));
+    i2c.set_enabled(false);
+    // i2c.set_timingr(|_| Timingr(0x00300619));
+    i2c.set_timingr(|r| r
+        .set_presc(0x0)
+        .set_scldel(0x3)
+        .set_sdadel(0x0)
+        .set_sclh(0xF)
+        .set_scll(0x12)
+    );
+
+    let i2c_drv = I2cDriver::new(i2c.into());
+    let mut app = examples::mb85rc::Example::new(brd.console(), i2c_drv);
+    let _ = app.run();
+    loop {}
+
+}
+
+pub struct Error;
+
+pub struct I2cDriver { i2c: I2cPeriph }
+
+impl I2cDriver {
+    pub fn new(i2c: I2cPeriph) -> Self {
+        Self { i2c }
+    }    
+}
+
+impl hal::blocking::i2c::WriteRead for I2cDriver {
+    type Error = Error;
+
+    fn write_read(
+        &mut self, 
+        address: u8, 
+        bytes: &[u8], 
+        buffer: &mut [u8]
+    ) -> Result<(), Self::Error> {
+        self.i2c.transfer(address, bytes, buffer);
+        Ok(())
+    }
+
+}
---
+++ boards-new/nucleo-l432kc/examples/mb85rs.rs
@@ -0,0 +1,114 @@
+#![no_std]
+#![no_main]
+
+extern crate nucleo_l432kc as board;
+extern crate embedded_hal as hal;
+extern crate examples;
+
+use board::mcu::pin::*;
+use board::mcu::spi::*;
+
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    let brd = board::board();
+
+    let spi = SPI1;
+
+    let spi_sck = PB3;  // D13
+    let spi_miso = PB4; // D12
+    let spi_mosi = PB5; // D11
+    let spi_nss = PA11; // D10
+
+    spi.gate_enable();
+    spi_sck.port().gate_enable();
+    spi_miso.port().gate_enable();
+    spi_mosi.port().gate_enable();
+    spi_nss.port().gate_enable();    
+
+    spi_sck.connect_to(spi);
+    spi_miso.connect_to(spi);
+    spi_mosi.connect_to(spi);
+
+    // NOTE: Pins must be set with output speed HIGH or leading edge
+    // of transmission will occasionally be missed.
+
+    spi_sck.speed_high().push_pull();
+    spi_miso.speed_high().pull_up();
+    spi_mosi.speed_high().push_pull();
+    spi_nss.mode_output().set_output(true).set_output(false).set_output(true);
+
+    spi.set_config(|cfg| cfg
+        .set_data_size(DataSize::Bits8)
+        .set_master(true)
+        .set_baud_divider(0b011.into())
+    );
+    spi.with_cr1(|r| r.set_cpha(0).set_cpol(0));
+    spi.with_cr2(|r| r.set_frxth(1));
+    spi.set_output_enabled(true).set_enabled(true);
+
+
+    let nss_drv = PinDriver::new(spi_nss.into());
+    let spi_drv = SpiDriver::new(spi.into());
+    let mut app = examples::mb85rs::Example::new(brd.console(), spi_drv, nss_drv);
+    let _ = app.run();
+   
+    loop {}
+
+}
+
+pub struct PinDriver { pin: GpioPin }
+impl PinDriver {
+    pub fn new(pin: GpioPin) -> Self {
+        Self { pin }
+    }
+}
+
+impl hal::digital::OutputPin for PinDriver {
+    fn is_high(&self) -> bool {
+        self.pin.output()
+    }
+
+    fn is_low(&self) -> bool {
+        !self.pin.output()
+    }
+
+    fn set_low(&mut self) { self.pin.set_output(false); }
+    fn set_high(&mut self) { self.pin.set_output(true); }
+}
+
+
+pub struct Error;
+
+pub struct SpiDriver { spi: SpiPeriph }
+
+impl SpiDriver {
+    pub fn new(spi: SpiPeriph) -> Self {
+        Self { spi }
+    }
+}
+
+// pub trait Transfer<W> {
+//     type Error;
+//     fn transfer<'w>(
+//         &mut self, 
+//         words: &'w mut [W]
+//     ) -> Result<&'w [W], Self::Error>;
+// }
+
+impl hal::blocking::spi::Write<u8> for SpiDriver {
+    type Error = Error;
+    fn write<'w>(&mut self, words: &'w [u8]) -> Result<(), Self::Error> {
+        self.spi.transfer(words, &mut []);
+        Ok(())
+    }
+}
+
+impl hal::blocking::spi::Transfer<u8> for SpiDriver {
+    type Error = Error;
+    fn transfer<'w>(&mut self, words: &'w mut [u8]) -> Result<&'w [u8], Self::Error> {
+        self.spi.transfer(&[], words);
+        Ok(words)
+    }
+}
