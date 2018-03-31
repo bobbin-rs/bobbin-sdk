103ca6edf42618e79dc4582d00b0b3c106b9d44e
Jonathan Soo <jcsoo@agora.com>
2018-03-30 20:18:47 -0400

    Only sample one channel

--- boards-new/nucleo-l432kc/examples/adc.rs
+++ boards-new/nucleo-l432kc/examples/adc.rs
@@ -19,21 +19,16 @@ pub extern "C" fn main() -> ! {
 
     
     let a0 = PA0;  // A0
-    let a1 = PA1;  // A1
 
     let adc = ADC1;
     let ch1 = ADC1_CH5;
-    let ch2 = ADC1_CH6;
 
     adc.gate_enable();
     a0.port().gate_enable();
-    a1.port().gate_enable();
 
     a0.connect_to(ch1);
-    a1.connect_to(ch2);
 
     a0.mode_analog();
-    a1.mode_analog();
 
     println!("Initializing");
     C_ADC.with_ccr(|r| r.set_ckmode(0b01));
@@ -46,8 +41,7 @@ pub extern "C" fn main() -> ! {
     println!("Initialized...");
     loop {        
         let v0: U12 = ch1.analog_read();
-        let v1: U10 = ch2.analog_read();
-        println!("{} {}", v0, v1);
-        board::delay(1_000);
+        println!("{}", v0);
+        board::delay(500);
     }
 }
