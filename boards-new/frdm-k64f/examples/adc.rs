4252c1cd68203f6314efa93c8f9b1e1c19133854
Jonathan Soo <jcsoo@agora.com>
2018-03-30 20:43:40 -0400

    ADC for K64F

--- boards-new/frdm-k64f/examples/adc.rs
+++ boards-new/frdm-k64f/examples/adc.rs
@@ -3,8 +3,8 @@
 
 #[macro_use]
 extern crate frdm_k64f as board;
+extern crate examples;
 
-use board::common::analog::*;
 use board::common::bits::*;
 use board::mcu::pin::*;
 use board::mcu::adc::*;
@@ -12,43 +12,24 @@ use board::mcu::adc::*;
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
+    let brd = board::board();
     println!("ADC Test");
 
     let a0 = PTB2; // A0 // PTB2 / ADC0_SE12
-    let a1 = PTB3; // A1 // PTB3 / ADC0_SE13
     
     let adc0 = ADC0;
     let ch12 = ADC0_CH12;
-    let ch13 = ADC0_CH13;
-
-    println!("Setting up Pins");
 
     a0.port().gate_enable();
     a0.connect_to(ch12);
-
-    a1.port().gate_enable();
-    a1.connect_to(ch13);
-
-    println!("Setting up ADCs");
 
     adc0        
         .gate_enable()
         .init();    
 
-    println!("Initialization Complete");
+    let ch12: AdcCh = ch12.into();
 
-    loop {
-        let v0: U16 = ch12.analog_read();
-        let v1: U16 = ch13.analog_read();
-        // ch0.set_input_channel(12.into());        
-        // while !ch0.conversion_complete() {}
-        // let v0 = ch0.result();
+    let mut app = examples::adc::AdcExample::new(brd.console(), ch12, brd, 500, U16::from(0));
+    app.run()
 
-        // ch0.set_input_channel(13.into());        
-        // while !ch0.conversion_complete() {}
-        // let v1 = ch0.result();
-
-        println!("{} {}", v0, v1);
-        board::delay(1000);
-    }
 }
