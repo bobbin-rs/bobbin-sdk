535fa7701c797c6370a49aa8aa7eae9e69f7679d
Jonathan Soo <jcsoo@agora.com>
2018-03-30 20:50:10 -0400

    ArduinoZero ADC WIP

---
+++ boards-new/arduino-zero/examples/adc.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[macro_use]
+extern crate arduino_zero as board;
+extern crate examples;
+
+use board::common::bits::*;
+use board::mcu::pin::*;
+use board::mcu::adc::*;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    let brd = board::board();
+    println!("Analog Test");
+
+    ADC.gate_enable();
+    board::mcu::adc::init();
+    
+    let a0 = PA02;
+
+    let ch0 = adc::ADC_CH0;
+
+    // a0.mode_ain(&ch0);
+    a0.connect_to(ch0);
+
+    let ch0: AdcCh = ch0.into();
+    let mut app = examples::adc::AdcExample::new(brd.console(), ch0, brd, 500, U16::from(0));
+    app.run()
+    // loop {
+    //     let v0: U12 = ch0.analog_read();
+    //     let v1: U8 = ch2.analog_read();
+    //     println!("{}, {}", v0, v1);
+    //     board::delay(500);
+    // }
+
+}
