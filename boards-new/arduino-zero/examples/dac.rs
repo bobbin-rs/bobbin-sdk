1becf42c845473f49f3f43d9ec9209a1a369f041
Jonathan Soo <jcsoo@agora.com>
2018-04-01 15:51:01 -0400

    Partly working DAC output

--- boards-new/arduino-zero/examples/dac.rs
+++ boards-new/arduino-zero/examples/dac.rs
@@ -9,39 +9,56 @@ extern crate examples;
 use board::mcu::pin::*;
 use board::mcu::dac::*;
 use board::mcu::gclk;
+use board::mcu::pm::*;
+
 // DAC = PA0 = PA02
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
+    board::delay(500);
     println!("DAC Test");
 
+    PA02.port().gate_enable();
+    PA02.set_dir_input();
+    PA02.set_pull_enabled(false);
+    PA02.set_pmux_enabled(false);
+
+    PA02.set_pmux_enabled(true).set_pmux(1);
+
     let dac = DAC;
-    // PA02.connect_to(dac); // NOTE - this may burn out chip!!!!
     dac.gate_enable();
     {
         while gclk::GCLK.status().syncbusy() != 0 {}
 
         // Set GCLK_GEN0 as source for ADC
         gclk::GCLK.set_clkctrl(|r| r
-            .set_id(0x1e) // GCLK_ADC
+
+            .set_id(0x21) // GCLK_DAC
             .set_gen(0x0)
             .set_clken(true)
         );    
+        while gclk::GCLK.status().syncbusy() != 0 {}
+        while DAC.status().syncbusy() != 0 {}
         DAC.set_ctrlb(|r| r.set_refsel(0x01).set_eoen(1));
-        DAC.with_ctrla(|r| r.set_enable(1));
+        while DAC.status().syncbusy() != 0 {}
+        DAC.set_ctrla(|r| r.set_enable(1));
+        while DAC.status().syncbusy() != 0 {}
     }
+
+    println!("Running Loop");
 
     let mut v: u8 = 16;
     let s: u8 = 4;
     let mut d: bool = true;
     loop {
-        DAC.set_data(|r| r.set_data((v as u16) << 2));
+        DAC.set_data(|r| r.set_data(v << 1));
         if d {
             v += s;
             if v == 240 {
                 d = !d;
             }
+            
         } else {
             v -= s;
             if v == 16 {
