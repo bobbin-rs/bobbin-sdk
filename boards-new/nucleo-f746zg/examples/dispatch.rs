09460af0467e95fd51912110c215c45b16884b31
Jonathan Soo <jcsoo@agora.com>
2018-04-06 13:11:55 -0400

    Multi-systick demo

--- boards-new/nucleo-f746zg/examples/dispatch.rs
+++ boards-new/nucleo-f746zg/examples/dispatch.rs
@@ -8,13 +8,16 @@ extern crate examples;
 
 use board::mcu::systick;
 
-use board::mcu::bobbin_common::dispatch::*;
+use board::ext::dispatch::*;
+use board::ext::{Dispatcher, IrqHandler};
+
+use core::cell::UnsafeCell;
+
 static mut HANDLER_SLOTS: [Option<IrqHandler>; 2] = [None; 2];
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     board::init();
-    let brd = board::board();
     println!("Dispatch Test");
 
     unsafe {
@@ -23,13 +26,19 @@ pub extern "C" fn main() -> ! {
 
     let reload_value = (216_000_000 / 8000) - 1;
 
-    let mut t = TickHandler::new();    
-    let _ = t.register(brd);
+    let t = TickHandler::new();    
+    let t = Dispatcher::register_handler(15, &t).unwrap();
     t.configure(reload_value);
     t.enable();
-    
+
+    board::delay(100);
+
+    let t2 = TickHandler::new();    
+    let t2 = Dispatcher::register_handler(15, &t2).unwrap();
+
+
     loop {
-        println!("tick: {}", t.count);
+        println!("tick: {} {}", unsafe { *t.count.get()}, unsafe { *t2.count.get()}  );
         board::delay(1000);
     }
 }
@@ -37,34 +46,28 @@ pub extern "C" fn main() -> ! {
 use systick::SYSTICK;
 
 pub struct TickHandler {
-    count: u32,
-    irq_handle: Option<IrqHandle>,
+    count: UnsafeCell<u32>,
 }
 
 impl TickHandler {
     pub fn new() -> Self {
-        Self { count: 0, irq_handle: None }
+        Self { count: UnsafeCell::new(0) }
     }
-
-    pub fn register<R: RegisterExc>(&mut self, reg: R) -> Result<(), RegisterError> {
-        self.irq_handle = Some(reg.register_exc(15, self)?);
-        Ok(())
-    }
-
-    pub fn configure(&mut self, reload_value: u32) {
+    
+    pub fn configure(&self, reload_value: u32) {
         SYSTICK.set_reload_value(reload_value);
         SYSTICK.set_current_value(reload_value);
     }
 
-    pub fn enable(&mut self){
+    pub fn enable(&self) {
         SYSTICK.set_tick_interrupt(true);
         SYSTICK.set_enabled(true);        
     }
 }
 
 impl HandleIrq for TickHandler {
-    unsafe fn handle_irq(&mut self, _irq: u8) -> IrqResult {
-        self.count += 1;
+    unsafe fn handle_irq(&self, _irq: u8) -> IrqResult {
+        *self.count.get() += 1;
         IrqResult::Continue
     }
 }
