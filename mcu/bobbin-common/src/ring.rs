0b07f33ca957a4013c1695f4889ab9a1e1de6152
Jonathan Soo <jcsoo@agora.com>
2018-04-09 11:07:24 -0400

    Add updated ring implementation

--- mcu/bobbin-common/src/ring.rs
+++ mcu/bobbin-common/src/ring.rs
@@ -1,270 +1,305 @@
 use core::cell::UnsafeCell;
-use core::cmp;
-use core::ptr;
 use core::marker::PhantomData;
-// use core::ops::Index;
+use core::{mem, ptr, slice};
 
-
-
-pub struct Ring<'a, T: 'a + Copy> {
-    reader: UnsafeCell<usize>,
-    writer: UnsafeCell<usize>,
-    buffer: *mut [T],
+pub struct Ring<'a, T: 'a> {
+    ptr: *mut T,
+    cap: usize,
+    head: UnsafeCell<usize>,
+    tail: UnsafeCell<usize>,
     _phantom: PhantomData<&'a mut[T]>,
 }
 
-impl<'a, T: 'a + Copy> Ring<'a, T> {
-    pub fn new(buf: &mut[T]) -> Self {
-        Ring {
-            reader: UnsafeCell::new(0),
-            writer: UnsafeCell::new(0),
-            buffer: buf,
-            _phantom: PhantomData,
+impl<'a, T: 'a> Ring<'a, T> {
+    pub fn new(buf: &'a mut [T]) -> Self {
+        let ptr = buf.as_mut_ptr();
+        let cap = buf.len();
+        assert!(cap > 0 && cap.count_ones() == 1, "Only power-of-two sizes larger than zero are supported");
+        mem::forget(buf);
+        Self { ptr, cap, head: UnsafeCell::new(0), tail: UnsafeCell::new(0), _phantom: PhantomData }
+    }
+
+    pub fn into_slice(self) -> &'a [T] {
+        unsafe {
+            slice::from_raw_parts_mut(self.ptr, self.cap)
         }
     }
 
-    pub fn pair(&self) -> (RingReader<T>, RingWriter<T>) {
-        (
-            RingReader { rb: self},
-            RingWriter { rb: self}
-        )
+    pub fn reset(&self) {
+        unsafe {
+            *self.head.get() = 0;
+            *self.tail.get() = 0;
+        }
     }
 
-    fn as_ref(&self) -> &[T] {
-        unsafe { &*self.buffer }
-    }
-
-    // fn as_mut(&self) -> &mut [T]{
-    //     unsafe { &mut *self.buffer }
-    // }    
-
-    fn cap(&self) -> usize {
-        self.as_ref().len()
-    }
-
-    pub fn writer(&self) -> usize {
-        unsafe { ptr::read_volatile(self.writer.get()) }
-    }
-
-    pub fn set_writer(&self, value: usize) {
-        unsafe { ptr::write_volatile(self.writer.get(), value) }
-    }
-
-    pub fn reader(&self) -> usize {
-        unsafe { ptr::read_volatile(self.reader.get()) }
-    }
-
-    pub fn set_reader(&self, value: usize) {
-        unsafe { ptr::write_volatile(self.reader.get(), value) }
-    }
-    pub fn len(&self) -> usize {
-        self.writer().wrapping_sub(self.reader())
-    }
-
-    pub fn rem(&self) -> usize {
-        self.cap().wrapping_sub(self.len())
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.reader() == self.writer()
-    }
-
-    pub fn is_full(&self) -> bool {
-        self.len() == self.cap()
-    }
-
+    #[inline]
     fn phy(&self, index: usize) -> usize {
-        index % self.cap()
+        index & (self.cap - 1)
     }
 
-    fn incr_reader(&self) {
-        assert!(!self.is_empty(), "Attempted to increment empty reader");
-        self.set_reader(self.reader().wrapping_add(1));
+    fn phy_slice(&self) -> &[T] {
+        unsafe { slice::from_raw_parts(self.ptr, self.cap) }
     }
 
-    fn incr_writer(&self) {        
-        assert!(!self.is_full(), "Attempted to increment full writer");
-        self.set_writer(self.writer().wrapping_add(1));     
+    #[inline]
+    unsafe fn elt(&self, index: usize) -> &mut T {
+        &mut *self.ptr.offset(self.phy(index) as isize)
     }
 
-    pub fn enqueue(&self, value: T) -> bool {
-        if self.is_full() {
-            false
-        } else {
-            unsafe {
-                self.write_buf(self.phy(self.writer()) as isize, value);
+    #[inline]
+    pub fn cap(&self) -> usize {
+        self.cap
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.head().wrapping_sub(self.tail())
+    }
+
+    #[inline]
+    pub fn head(&self) -> usize {
+        unsafe { *self.head.get() }
+    }
+
+    #[inline]
+    pub fn tail(&self) -> usize {
+        unsafe { *self.tail.get()}
+    }
+
+
+    pub fn head_elt(&self) -> Option<&mut T> {
+        unsafe {
+            if self.len() < self.cap() {
+                Some(self.elt(self.head()))
+            } else {
+                None
             }
-            self.incr_writer();
-            true
         }
     }
 
-    pub fn dequeue(&self) -> Option<T> {
-        if self.is_empty() {
-            None
-        } else {            
-            unsafe {
-                let value = self.read_buf(self.phy(self.reader()) as isize);
-                self.incr_reader();
-                Some(value)
+    pub fn tail_elt(&self) -> Option<&mut T> {
+        unsafe {
+            if self.len() > 0 {
+                Some(self.elt(self.tail()))
+            } else {
+                None
             }
+        }
+    }
+
+    #[inline]
+    pub fn incr_head(&self) -> usize {
+        let head = self.head().wrapping_add(1);
+        assert!(self.len() <= self.cap());
+        unsafe { ptr::replace(self.head.get(), head) }
+    }
+
+    #[inline]
+    pub fn incr_tail(&self) -> usize {
+        let tail = self.tail().wrapping_add(1);
+        assert!(self.tail() <= self.head());
+        unsafe { ptr::replace(self.tail.get(), tail) }
+    }
+
+    pub fn slices(&self) -> (&[T], &[T]) {
+        let head = self.head();
+        let tail = self.tail();
+
+        if head == tail {
+            (&[], &[])
+        } else {
+            let phy_head = self.phy(self.head());
+            let phy_tail = self.phy(self.tail());
+            if phy_head > phy_tail {
+                let a = &self.phy_slice()[phy_tail..phy_head];
+                let b = &[];
+                (a, b)
+            } else {
+                let a = &self.phy_slice()[phy_tail..];
+                let b = &self.phy_slice()[..phy_head];
+                (a, b)
+            }
+        }        
+    }
+
+    pub fn reader(&self) -> Reader<T> {
+        Reader { inner: self }
+    }
+
+    pub fn writer(&self) -> Writer<T> {
+        Writer { inner: self }
+    }
+
+}
+
+impl<'a, T: 'a + Copy> Ring<'a, T> {
+    pub fn put(&self, value: T) -> Option<()> {
+        if let Some(elt) = self.head_elt() {
+            *elt = value;
+            self.incr_head();
+            Some(())
+        } else {
+            None
         }
     }
 
     pub fn write(&self, buf: &[T]) -> usize {
-        let n = cmp::min(self.rem(), buf.len());
-        for i in 0..n {
-            self.enqueue(buf[i]);
+        let mut i = 0;
+        while i < buf.len() {
+            if let Some(_) = self.put(buf[i]) {
+                i += 1;
+            } else {
+                break
+            }
         }
-        n
+        i
+    }
+
+    pub fn get(&self) -> Option<T> {
+        if let Some(elt) = self.tail_elt() {
+            let value = *elt;
+            self.incr_tail();
+            Some(value)
+        } else {
+            None
+        }
     }
 
     pub fn read(&self, buf: &mut [T]) -> usize {
-        let n = cmp::min(self.len(), buf.len());
-        for i in 0..n {
-            buf[i] = self.dequeue().expect("Ring buffer is empty");
+        let mut i = 0;
+        while i < buf.len() {
+            if let Some(v) = self.get() {
+                buf[i] = v;
+                i += 1;
+            } else {
+                break
+            }
         }
-        n
+        i
+    }
+}
+
+pub struct Reader<'a, T: 'a> {
+    inner: &'a Ring<'a, T>
+}
+
+impl<'a, T: 'a> Reader<'a, T> {
+    pub fn tail_elt(&self) -> Option<&mut T> {
+        self.inner.tail_elt()
+    }
+
+    pub fn incr_tail(&self) -> usize {
+        self.inner.incr_tail()
+    }
+}
+
+impl<'a, T: 'a + Copy> Reader<'a, T> {
+    pub fn get(&self) -> Option<T> {    
+        self.inner.get()
+    }
+
+    pub fn read(&self, buf: &mut [T]) -> usize {    
+        self.inner.read(buf)
+    }
+}
+
+unsafe impl<'a, T: 'a> Send for Reader<'a, T> {}
+unsafe impl<'a, T: 'a> Sync for Reader<'a, T> {}
+
+pub struct Writer<'a, T: 'a> {
+    inner: &'a Ring<'a, T>
+}
+
+impl<'a, T: 'a> Writer<'a, T> {
+    pub fn head_elt(&self) -> Option<&mut T> {
+        self.inner.head_elt()
+    }
+
+    pub fn incr_head(&self) -> usize {
+        self.inner.incr_head()
+    }    
+}
+
+impl<'a, T: 'a + Copy> Writer<'a, T> {
+    pub fn put(&self, value: T) -> Option<()> {
+        self.inner.put(value)
     }    
 
-    unsafe fn read_buf(&self, offset: isize) -> T {
-        ptr::read_volatile((&*self.buffer).as_ptr().offset(offset))
-    }
-
-    unsafe fn write_buf(&self, offset: isize, value: T) {
-        ptr::write_volatile((&mut *self.buffer).as_mut_ptr().offset(offset), value)
-    }
-
-    pub fn peek(&self, index: usize) -> T {
-        if index >= self.len() {
-            panic!("Index is {} but len is {}", index, self.len());
-        }
-        unsafe {
-            self.read_buf(self.phy(index.wrapping_add(self.reader())) as isize)
-        }
+    pub fn write(&self, buf: &[T]) -> usize {
+        self.inner.write(buf)
     }
 }
 
-unsafe impl<'a, T: 'a + Copy> Send for Ring<'a, T> {}
-unsafe impl<'a, T: 'a + Copy> Sync for Ring<'a, T> {}
-
-pub struct RingReader<'a, T: 'a + Copy> {
-    rb: &'a Ring<'a, T>,
-}
-
-impl<'a, T: Copy> RingReader<'a, T> {
-    pub fn dequeue(&self) -> Option<T> {
-        self.rb.dequeue()
-    }
-    pub fn read(&self, buf: &mut [T]) -> usize {
-        self.rb.read(buf)
-    }
-}
-
-unsafe impl<'a, T: 'a + Copy> Send for RingReader <'a, T> {}
-unsafe impl<'a, T: 'a + Copy> Sync for RingReader <'a, T> {}
-
-pub struct RingWriter<'a, T: 'a + Copy> {
-    rb: &'a Ring<'a, T>,
-}
-
-impl<'a, T: Copy> RingWriter<'a, T> {
-    pub fn enqueue(&self, value: T) -> bool {
-        self.rb.enqueue(value)
-    }
-    pub fn write(&self, buf: &[T]) -> usize {    
-        self.rb.write(buf)
-    }
-}
-
-unsafe impl<'a, T: 'a + Copy> Send for RingWriter<'a, T> {}
-unsafe impl<'a, T: 'a + Copy> Sync for RingWriter<'a, T> {}
-
+unsafe impl<'a, T: 'a> Send for Writer<'a, T> {}
+unsafe impl<'a, T: 'a> Sync for Writer<'a, T> {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_static() {
-        let (r, w) = static_ring_buf!(16, u8, 0);
-        w.write(b"Hello, World");
-        let mut dst = [0u8; 64];
-        let n = r.read(&mut dst);
-        assert_eq!(&dst[..n], b"Hello, World");
+    fn test_ring() {
+        let mut buf = [0u8; 16];
+        let ring = Ring::new(&mut buf);
+
+        for i in 0..16 {
+            if let Some(elt) = ring.head_elt() {
+                *elt = i as u8;
+                ring.incr_head();
+            }            
+        }
+
+        for i in 0..16 {
+            if let Some(elt) = ring.tail_elt() {
+                assert_eq!(*elt, i as u8);
+                ring.incr_tail();
+            }
+        }
+
+        let buf = ring.into_slice();
+
+        for i in 0..16 {
+            assert_eq!(buf[i], i as u8);
+        }
     }
-
-
     #[test]
-    fn test_enqueue_dequeue() {
-        let (r, w) = static_ring_buf!(16, u8, 0);
-        
-        for i in 0..16 {
-            assert_eq!(w.enqueue(i as u8), true);
+    fn test_copy_ring() {
+        let mut buf = [0u8; 16];
+        let r = Ring::new(&mut buf);
+        for i in 0..16 { 
+            r.put(i as u8);
         }
-        assert_eq!(w.enqueue(0), false);
         for i in 0..16 {
-            assert_eq!(r.dequeue(), Some(i as u8));
-        }
-        assert_eq!(r.dequeue(), None);
-    }
-
-     #[test]
-    fn test_enqueue_dequeue_u32() {
-        let (r, w) = static_ring_buf!(16, u32, 0);
-        
-        for i in 0..16 {
-            assert_eq!(w.enqueue(i as u32), true);
-        }
-        assert_eq!(w.enqueue(0), false);
-        for i in 0..16 {
-            assert_eq!(r.dequeue(), Some(i as u32));
-        }
-        assert_eq!(r.dequeue(), None);
-    }   
-
-    #[test]
-    fn test_write_read() {
-        let (r, w) = static_ring_buf!(16, u8, 0);
-
-        let src: [u8; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-        let mut dst = [0u8; 16];
-
-        w.write(&src);
-        let n = r.read(&mut dst);
-        assert_eq!(n, 16);
-        for i in 0..16 {
-            assert_eq!(src[i], dst[i]);
-        }
-    }
-
-    pub struct Driver<'a> {
-        w: RingWriter<'a, u8>
-    }
-
-    impl<'a> Driver<'a> {
-        pub fn run(&self) {
-            self.w.enqueue(123);
+            assert_eq!(r.get(), Some(i as u8));
         }
     }
 
     #[test]
-    fn test_driver() {
-        let (r, w) = static_ring_buf!(16, u8, 0);
-        let drv = Driver { w: w };
-        drv.run();
-        assert_eq!(r.dequeue(), Some(123));
-    }
-
-    #[test]
-    fn test_static_driver() {
-        let (r, w) = static_ring_buf!(16, u8, 0);        
-        static mut DRV: Option<Driver> = None;
-        unsafe {
-            DRV = Some(Driver { w: w});
-            DRV.as_ref().unwrap().run();
-        }
-        assert_eq!(r.dequeue(), Some(123));
+    fn test_read_write_ring() {
+        let mut buf = [0u8; 16];
+        let r = Ring::new(&mut buf);
+        assert_eq!(r.write(&[0, 1, 2, 3]), 4);
+        let mut dst = [0u8; 4];
+        assert_eq!(r.read(&mut dst), 4);
+        assert_eq!(dst, [0, 1, 2, 3])
     }    
 
+
+    #[test]
+    fn test_reader_writer_ring() {
+        let mut buf = [0u8; 16];
+        let ring = Ring::new(&mut buf);
+        let r = ring.reader();
+        let w = ring.writer();
+
+        for _ in 0..16 {
+            assert_eq!(w.write(&[0, 1, 2, 3]), 4);
+            let mut dst = [0u8; 4];
+            assert_eq!(r.read(&mut dst), 4);
+
+            assert_eq!(dst, [0, 1, 2, 3])
+        }
+        assert_eq!(ring.head(), 16 * 4);
+        assert_eq!(ring.tail(), 16 * 4);
+    }        
 }
