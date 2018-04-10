1fb5610f08dd8e78f9de93c8c916599b67113636
Jonathan Soo <jcsoo@agora.com>
2018-04-10 12:38:42 -0400

    Add L3GD20-SPI example for DiscoveryF3

---
+++ boards-new/discovery-stm32f3/examples/l3gd20-spi.rs
@@ -0,0 +1,246 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate discovery_stm32f3 as board;
+extern crate embedded_hal as hal;
+
+use hal::prelude::*;
+
+// use board::common::bits::*;
+use board::mcu::pin::*;
+use board::mcu::spi::*;
+
+// A5 = PB6 = I2C1_SCL
+// A4 = PB7 = I2C1_SDA
+
+// Address = 0x35
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    board::init();
+    println!("Running L3GD20 SPI");
+    
+    match run() {
+        Ok(_) => {},
+        Err(_) => println!("Error"),
+    }
+    loop {}
+}
+
+pub fn run() -> Result<!, Error> {
+    let spi = SPI1;
+    let spi_miso = PA6;
+    let spi_mosi = PA7;
+    let spi_sck = PA5;
+    let spi_nss = PE3; 
+
+    spi.gate_enable();
+    spi_miso.port().gate_enable();
+    spi_mosi.port().gate_enable();
+    spi_sck.port().gate_enable();
+    spi_nss.port().gate_enable();
+
+    spi_miso.connect_to(spi);
+    spi_mosi.connect_to(spi);
+    spi_sck.connect_to(spi);
+    
+    spi_sck.speed_high().push_pull();
+    spi_miso.speed_high().pull_up();
+    spi_mosi.speed_high().push_pull();
+    spi_nss.mode_output().set_output(true).set_output(false).set_output(true);
+
+    spi.set_config(|cfg| cfg
+        .set_data_size(DataSize::Bits8)
+        .set_master(true)
+        .set_baud_divider(0b011.into())
+    );
+    spi.with_cr1(|r| r.set_cpha(0).set_cpol(0));
+    spi.with_cr2(|r| r.set_frxth(1));
+    spi.set_output_enabled(true).set_enabled(true);
+
+    println!("SPI Configuration Complete");
+
+    let nss_drv = PinDriver::new(spi_nss.into());
+    let spi_drv = SpiDriver::new(spi.into());
+
+    let mut dev = L3gd20::new(spi_drv, nss_drv);
+
+    println!("ID:   0x{:02x}", dev.id()?);
+    println!("TEMP: 0x{:02x}", dev.temp()?);
+
+    /* Reset then switch to normal mode and enable all three channels */
+    dev.cmd_reset()?;
+    dev.cmd_mode_normal()?;
+
+    let mut n = 0;
+    loop {
+        if n == 0 {
+            println!("{:>6} {:>6} {:>6}","X","Y","Z");
+        }
+        // println!("STATUS: {:02x}", dev.reg_read(0x27));
+        { 
+            // let (xl, xh, yl, yh, zl, zh) = (
+            //     dev.reg_read(0x28),
+            //     dev.reg_read(0x29),
+            //     dev.reg_read(0x2a),
+            //     dev.reg_read(0x2b),
+            //     dev.reg_read(0x2c),
+            //     dev.reg_read(0x2d),
+            // );
+            // let x = (((xh as u16) << 8) | (xl as u16)) as i16;
+            // let y = (((yh as u16) << 8) | (yl as u16)) as i16;
+            // let z = (((zh as u16) << 8) | (zl as u16)) as i16;
+            let (x, y, z) = dev.xyz()?;
+            println!("{:6} {:6} {:6}", x, y, z);
+        }
+        board::delay(500);
+        n = if n == 10 { 0 } else { n + 1};
+    }
+
+}
+
+pub struct L3gd20 {
+    spi: SpiDriver,
+    nss: PinDriver,
+}
+
+impl L3gd20 {
+    pub fn new(spi: SpiDriver, nss: PinDriver) -> Self {
+        Self { spi, nss }
+    }
+
+    pub fn reg_write(&mut self, reg: u8, val: u8) -> Result<(), Error> {
+        let cmd = [reg, val];
+        let mut buf = [0u8, 0u8];
+        self.transfer(&cmd, &mut buf)
+    }
+   
+    pub fn reg_read(&mut self, reg: u8) -> Result<u8, Error> {
+        let cmd = [reg | 1 << 7, 0x00];
+        let mut buf = [0u8, 0u8];
+        self.transfer(&cmd, &mut buf)?;
+        Ok(buf[1])
+    }    
+
+    pub fn transfer(&mut self, src: &[u8], dst: &mut[u8]) -> Result<(), Error> {
+        self.nss.set_low();
+        self.spi.write(src)?;
+        self.spi.transfer(dst)?;
+        self.nss.set_high();
+        Ok(())        
+    }
+
+    pub fn cmd_reset(&mut self) -> Result<(), Error> {
+        self.reg_write(0x20, 0x00)
+    }
+
+    pub fn cmd_mode_normal(&mut self) -> Result<(), Error> {
+        self.reg_write(0x20, 0x0f)
+    }
+
+    pub fn id(&mut self) -> Result<u8, Error>  {
+        self.reg_read(0x0f)
+    }
+
+    pub fn temp(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x26)
+    }
+
+    pub fn xyz(&mut self) -> Result<(i16, i16, i16), Error> {
+        Ok((self.x()?, self.y()?, self.z()?))
+    }
+
+    pub fn x(&mut self) -> Result<i16, Error> {
+        Ok((self.xl()? as u16 | (self.xh()? as u16) << 8) as i16)
+    }
+
+    pub fn y(&mut self) -> Result<i16, Error> {
+        Ok((self.yl()? as u16 | (self.yh()? as u16) << 8) as i16)
+    }
+
+    pub fn z(&mut self) -> Result<i16, Error> {
+        Ok((self.zl()? as u16 | (self.zh()? as u16) << 8) as i16)
+    }
+
+
+    pub fn xl(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x28)
+    }
+    
+    pub fn xh(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x29)
+    }
+    
+    pub fn yl(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x2a)
+    }
+    
+    pub fn yh(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x2b)
+    }
+    
+    pub fn zl(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x2c)
+    }
+    
+    pub fn zh(&mut self) -> Result<u8, Error> {
+        self.reg_read(0x2d)
+    }
+
+}
+
+
+pub struct PinDriver { pin: GpioPin }
+impl PinDriver {
+    pub fn new(pin: GpioPin) -> Self {
+        Self { pin }
+    }
+}
+
+impl hal::digital::OutputPin for PinDriver {
+    fn is_high(&self) -> bool {
+        self.pin.output()
+    }
+
+    fn is_low(&self) -> bool {
+        !self.pin.output()
+    }
+
+    fn set_low(&mut self) { self.pin.set_output(false); }
+    fn set_high(&mut self) { self.pin.set_output(true); }
+}
+
+pub struct Error;
+
+pub struct SpiDriver { spi: SpiPeriph }
+
+impl SpiDriver {
+    pub fn new(spi: SpiPeriph) -> Self {
+        Self { spi }
+    }
+}
+
+// pub trait Transfer<W> {
+//     type Error;
+//     fn transfer<'w>(
+//         &mut self, 
+//         words: &'w mut [W]
+//     ) -> Result<&'w [W], Self::Error>;
+// }
+
+impl hal::blocking::spi::Write<u8> for SpiDriver {
+    type Error = Error;
+    fn write<'w>(&mut self, words: &'w [u8]) -> Result<(), Self::Error> {
+        self.spi.transfer(words, &mut []);
+        Ok(())
+    }
+}
+
+impl hal::blocking::spi::Transfer<u8> for SpiDriver {
+    type Error = Error;
+    fn transfer<'w>(&mut self, words: &'w mut [u8]) -> Result<&'w [u8], Self::Error> {
+        self.spi.transfer(&[], words);
+        Ok(words)
+    }
+}
--- boards-new/discovery-stm32f3/examples/lsm303.rs
+++ boards-new/discovery-stm32f3/examples/lsm303.rs
@@ -4,12 +4,9 @@
 #[macro_use]
 extern crate discovery_stm32f3 as board;
 
+use board::hal::i2c::*;
+use board::hal::gpio::*;
 use board::common::bits::*;
-use board::mcu::pin::*;
-use board::mcu::i2c::*;
-
-// A5 = PB6 = I2C1_SCL
-// A4 = PB7 = I2C1_SDA
 
 // Address = 0x35
 
@@ -18,27 +15,23 @@ pub extern "C" fn main() -> ! {
     board::init();
     println!("Running I2C");
     
-    let addr_gyro: U7 = U7::from(0x6B);
     let addr_accel: U7 = U7::from(0x32 >> 1);
     let addr_mag: U7 = U7::from(0x3d >> 1);
 
     let i2c = I2C1;
     let i2c_port = GPIOB;
-    let i2c_scl = PB8; // D15
-    let i2c_sda = PB9; // D14
+    let i2c_scl = PB6;
+    let i2c_sda = PB7;
 
-    GPIOA.gate_enable();
+    GPIOA.rcc_enable();
     PA6.mode_input().open_drain();
     PA5.mode_input().open_drain();
 
-    i2c.gate_enable();
-    i2c_port.gate_enable();
+    i2c.rcc_enable();
+    i2c_port.rcc_enable();
 
-    i2c_scl.connect_to(i2c);
-    i2c_sda.connect_to(i2c);
-
-    i2c_scl.open_drain();
-    i2c_sda.open_drain();
+    i2c_scl.mode_i2c_scl(&i2c).open_drain();
+    i2c_sda.mode_i2c_sda(&i2c).open_drain();
 
     println!("# Configuring I2C");
 
@@ -55,17 +48,6 @@ pub extern "C" fn main() -> ! {
 
     println!("I2C Configuration Complete");
 
-    println!("Configuring Gyro");
-
-    println!("ID:   {:02x}", i2c.read_reg(addr_gyro, 0x0f));
-    println!("TEMP: {:02x}", i2c.read_reg(addr_gyro, 0x26));
-    /* Reset then switch to normal mode and enable all three channels */
-    i2c.write_reg(addr_gyro, 0x20, 0x00);
-    i2c.write_reg(addr_gyro, 0x20, 0x0f);
-    // for i in 0x20..0x25 {
-    //     println!("{:02x}: {:02x}", i, i2c.read_reg(addr_gyro, i));
-    // }
-
     println!("Configuring Accel");
     // i2c.write_reg(addr_accel, 0x20, 0x00);
     i2c.write_reg(addr_accel, 0x20, 0x57);
@@ -78,29 +60,12 @@ pub extern "C" fn main() -> ! {
     println!("Configuring Magnetometer");
     i2c.write_reg(addr_mag, 0x02, 0x00);
     i2c.write_reg(addr_mag, 0x01, 0x20);
-    i2c.write_reg(addr_mag, 0x00, 0x80);
+    i2c.write_reg(addr_mag, 0x00, 0x90);
     // println!("V: {:02x}", i2c.read_reg(addr_mag, 0x00));
 
-
-    println!("Gyro | Accelerometer | Magnetometer");
+    println!("Accelerometer | Magnetometer");
 
     loop {
-        // println!("STATUS: {:02x}", i2c.read_reg(addr_gyro, 0x27));
-        { 
-            let (xl, xh, yl, yh, zl, zh) = (
-                i2c.read_reg(addr_gyro, 0x28),
-                i2c.read_reg(addr_gyro, 0x29),
-                i2c.read_reg(addr_gyro, 0x2a),
-                i2c.read_reg(addr_gyro, 0x2b),
-                i2c.read_reg(addr_gyro, 0x2c),
-                i2c.read_reg(addr_gyro, 0x2d),
-            );
-            let x = (((xh as u16) << 8) | (xl as u16)) as i16;
-            let y = (((yh as u16) << 8) | (yl as u16)) as i16;
-            let z = (((zh as u16) << 8) | (zl as u16)) as i16;
-            print!("{:6} {:6} {:6}", x, y, z);
-        }
-        print!(" | ");
         {
             let (xl, xh, yl, yh, zl, zh) = (
                 i2c.read_reg(addr_accel, 0x28),
@@ -130,17 +95,11 @@ pub extern "C" fn main() -> ! {
             let z = (((zh as u16) << 8) | (zl as u16)) as i16;
             print!("{:6} {:6} {:6}", x, y, z);
         }        
-        print!(" | ");
-        {
-            let (tl, th) = (
-                i2c.read_reg(addr_mag, 0x31),
-                i2c.read_reg(addr_mag, 0x32),
-            );
-            let t = (((th as u16) << 8) | (tl as u16)) as i16;
-            print!("{}", t);
-        }
-
         println!("");
+
+        // let mut buf = [0u8; 6];
+        // i2c.transfer(addr_gyro, &[0x20], &mut buf);
+        // println!("# {:?}", buf);
         board::delay(500);
     }
 }
