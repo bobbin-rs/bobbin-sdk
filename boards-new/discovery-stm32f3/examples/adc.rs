f4eee026e22e575ea0a957ddb91bd19b2d9b1726
Jonathan Soo <jcsoo@agora.com>
2018-04-10 17:35:25 -0400

    Update ADC example

--- boards-new/discovery-stm32f3/examples/adc.rs
+++ boards-new/discovery-stm32f3/examples/adc.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
 extern crate discovery_stm32f3 as board;
 extern crate examples;
 
@@ -8,6 +9,8 @@ use board::common::bits::*;
 use board::mcu::pin::*;
 use board::mcu::adc::*;
 use board::mcu::c_adc::*;
+
+// PA3 / ADC1_IN4
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
@@ -17,19 +20,55 @@ pub extern "C" fn main() -> ! {
     let a0 = PA3;  // A0
 
     let adc = ADC1;
-    let ch1 = ADC1_CH3;
+    let adc_ch = ADC1_CH4;
 
     adc.gate_enable();
     a0.port().gate_enable();
-    a0.connect_to(ch1);
-    a0.mode_analog();
+    a0.connect_to(adc_ch);
+    a0.mode_analog();    
 
-    adc.set_enabled(true).calibrate();
-    C_ADC.with_ccr(|r| r.set_tsvrefe(1));    
+    // Select HCLK as ADC1 clock source
+    C_ADC12.with_ccr(|r| r.set_ckmode(0b01));
+    adc.init();    
     
-    let ch1: AdcCh = ch1.into();
+    let adc_ch: AdcCh = adc_ch.into();
 
-    let mut app = examples::adc::AdcExample::new(brd.console(), ch1, brd, 500, U12::from(0));
+    let mut app = examples::adc::AdcExample::new(brd.console(), adc_ch, brd, 500, U12::from(0));
     app.run()
 
+}
+
+pub trait Init {
+    fn init_test(&self) -> &Self;
+}
+
+impl Init for AdcPeriph {
+    fn init_test(&self) -> &Self {
+        println!("CCR: {:?}", C_ADC12.ccr());
+        println!("A");
+        self.with_cr(|r| r.set_aden(0));
+        while self.isr().adrdy() != 0 {}
+
+        println!("B");
+        // Enable Analog Voltage Regulator
+        self.with_cr(|r| r.set_advregen(0b00));
+        self.with_cr(|r| r.set_advregen(0b01));
+        
+        println!("C");
+        // Calibrate
+        self.with_cr(|r| r.set_adcaldif(0));
+        self.with_cr(|r| r.set_adcal(1));
+        for _ in 0..100 {
+            let _ = self.cr();
+        }        
+        while self.cr().adcal() != 0 {}
+        println!("D");
+        // Enable ADC
+        self.with_cr(|r| r.set_aden(1));
+        println!("E");
+        // Wait until ADC is ready
+        while self.isr().adrdy() == 0 {}
+        
+        self
+    }    
 }
