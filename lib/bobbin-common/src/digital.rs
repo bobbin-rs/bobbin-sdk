f7da1e95cdf25070b32d3831805eb1090196ff26
Jonathan Soo <jcsoo@agora.com>
2018-04-14 12:01:34 -0400

    Remove unused files

--- lib/bobbin-common/src/digital.rs
+++
@@ -1,9 +0,0 @@
-pub trait DigitalOutput {
-    fn output(&self) -> bool;
-    fn set_output(&self, value: bool) -> &Self;
-    fn toggle_output(&self) -> &Self;
-}
-
-pub trait DigitalInput {
-    fn input(&self) -> bool;
-}
