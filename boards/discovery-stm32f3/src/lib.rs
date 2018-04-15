e66a8f2e02a2c8eed626f3e57cfb26fda51fe4c2
Jonathan Soo <jcsoo@agora.com>
2018-04-15 16:26:15 -0400

    Update Discovery-STM32F3

--- boards/discovery-stm32f3/src/lib.rs
+++ boards/discovery-stm32f3/src/lib.rs
@@ -4,9 +4,12 @@
 #[cfg(target_os="none")]
 pub extern crate cortex_m_rt;
 pub extern crate stm32f3x as mcu;
+pub extern crate bobbin_sys;
 
+pub use bobbin_sys::{system, memory, heap, print, println};
+#[cfg(feature="logger")]
+pub use bobbin_sys::logger;
 
-pub use mcu::bobbin_common::{print, println};
 pub use mcu::bobbin_common as common;
 
 #[cfg(target_os="none")]
@@ -20,25 +23,34 @@ pub mod console;
 pub mod led;
 pub mod btn;
 pub mod delay;
-pub mod sys;
 
 pub use delay::delay;
 
 pub fn init() -> System {    
-    System::init()
+    system::System::init(|| {
+        ::cache::init();
+        ::clock::init();
+        ::console::init();
+        ::led::init();
+        ::btn::init();
+        ::delay::init();
+        #[cfg(feature="logger")]
+        Logger::init();          
+    })
 }
 
-pub type System = sys::System<
+pub type System = system::System<
         Mcu,
         Clock,
+        Dispatcher,
 >;
 
 pub type Mcu = mcu::Stm32f3x;
 pub type Clock = clock::SystemClock;
-pub type Memory = mcu::bobbin_common::sys::memory::Memory;
-pub type Heap = mcu::bobbin_common::sys::heap::Heap;
+pub type Memory = memory::Memory;
+pub type Heap = heap::Heap;
 #[cfg(feature="logger")]
-pub type Logger = mcu::bobbin_common::logger::Logger;
+pub type Logger = logger::Logger;
 pub type Dispatcher = mcu::dispatch::Dispatcher<mcu::dispatch::ExcHandlers8>;
 
 #[cfg(target_os="none")]
