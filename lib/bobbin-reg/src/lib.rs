77ac73498a62bde1af84c26e5ac3081bc7bacb94
Jonathan Soo <jcsoo@agora.com>
2018-04-16 07:35:19 -0400

    Convert to singleton style

--- lib/bobbin-reg/src/lib.rs
+++ lib/bobbin-reg/src/lib.rs
@@ -8,19 +8,44 @@ mod rw {
 pub trait Register<T: Copy + Default> {
     const ADDR: *mut T;
 
-    fn read() -> T { unsafe { rw::read(Self::ADDR) } }
+    fn ptr(&self) -> *mut T { Self::ADDR }
 
-    fn write(value: T) { unsafe { rw::write(Self::ADDR, value) } }
+    fn read(&self) -> T { unsafe { rw::read(Self::ADDR) } }
 
-    fn with<F: FnOnce(T) -> T>(f: F) {
-        Self::write(f(Self::read()))
+    fn write(&self, value: T) { unsafe { rw::write(Self::ADDR, value) } }
+
+    fn with<F: FnOnce(T) -> T>(&self, f: F) {
+        self.write(f(self.read()))
     }
 
-    fn set<F: FnOnce(T) -> T>(f: F) {
-        Self::write(f(T::default()))
+    fn set<F: FnOnce(T) -> T>(&self, f: F) {
+        self.write(f(T::default()))
     }
 
-    fn clear() {
-        Self::write(T::default())
+    fn clear(&self) {
+        self.write(T::default())
+    }
+}
+
+#[macro_export]
+macro_rules! register {
+    ($rid:ident, $rty:ident, $rval:ty, $raddr:expr) => {
+        pub const $rid: $rty = $rty {};
+        pub struct $rty {}
+        impl Register<$rval> for $rty { 
+            const ADDR: *mut $rval = $raddr as *mut $rval;
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    register!(GPIOA_MODER, GpioaModer, u32, 0x1000_0000);
+
+    #[test]
+    fn test_ptr() {
+        assert_eq!(GPIOA_MODER.ptr(), 0x1000_0000 as *mut u32);
     }
 }
