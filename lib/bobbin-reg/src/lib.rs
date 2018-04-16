0e70dfbec217972f2e2170f57135598600da084c
Jonathan Soo <jcsoo@agora.com>
2018-04-16 07:50:40 -0400

    Add RegisterArray

--- lib/bobbin-reg/src/lib.rs
+++ lib/bobbin-reg/src/lib.rs
@@ -11,9 +11,9 @@ pub trait Register {
 
     fn ptr(&self) -> *mut Self::Value { Self::ADDR }
 
-    fn read(&self) -> Self::Value { unsafe { rw::read(Self::ADDR) } }
+    fn read(&self) -> Self::Value { unsafe { rw::read(self.ptr()) } }
 
-    fn write(&self, value: Self::Value) { unsafe { rw::write(Self::ADDR, value) } }
+    fn write(&self, value: Self::Value) { unsafe { rw::write(self.ptr(), value) } }
 
     fn with<F: FnOnce(Self::Value) -> Self::Value>(&self, f: F) {
         self.write(f(self.read()))
@@ -28,29 +28,29 @@ pub trait Register {
     }
 }
 
-pub trait RegisterArray {
+pub trait RegisterArray 
+{
     type Value: Copy + Default;
-    type Index: Copy;
+    type Index: Copy + ::core::ops::Mul<isize, Output=isize>;
     const ADDR: *mut Self::Value;
-    const DIM: Self::Index;
     const INCR: isize;
 
-    fn ptr(&self) -> *mut Self::Value { Self::ADDR }
+    fn ptr(&self, index: Self::Index) -> *mut Self::Value { unsafe { (Self::ADDR as *mut u8).offset(index * Self::INCR) as *mut Self::Value } }
 
-    fn read(&self) -> Self::Value { unsafe { rw::read(Self::ADDR) } }
+    fn read(&self, index: Self::Index) -> Self::Value { unsafe { rw::read(self.ptr(index)) } }
 
-    fn write(&self, value: Self::Value) { unsafe { rw::write(Self::ADDR, value) } }
+    fn write(&self, index: Self::Index, value: Self::Value) { unsafe { rw::write(self.ptr(index), value) } }
 
-    fn with<F: FnOnce(Self::Value) -> Self::Value>(&self, f: F) {
-        self.write(f(self.read()))
+    fn with<F: FnOnce(Self::Value) -> Self::Value>(&self, index: Self::Index, f: F) {
+        self.write(index, f(self.read(index)))
     }
 
-    fn set<F: FnOnce(Self::Value) -> Self::Value>(&self, f: F) {
-        self.write(f(Self::Value::default()))
+    fn set<F: FnOnce(Self::Value) -> Self::Value>(&self, index: Self::Index, f: F) {
+        self.write(index, f(Self::Value::default()))
     }
 
-    fn clear(&self) {
-        self.write(Self::Value::default())
+    fn clear(&self, index: Self::Index) {
+        self.write(index, Self::Value::default())
     }
 }
 
@@ -66,14 +66,35 @@ macro_rules! register {
     };
 }
 
+#[macro_export]
+macro_rules! register_array {
+    ($rid:ident, $rty:ident, $rval:ty, $raddr:expr, $rdim:ty, $incr:expr) => {
+        pub const $rid: $rty = $rty {};
+        pub struct $rty {}
+        impl RegisterArray for $rty { 
+            type Value = $rval;
+            type Index = $rdim;            
+            const ADDR: *mut $rval = $raddr as *mut $rval;
+            const INCR: isize = $incr;
+        }
+    };
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     register!(GPIOA_MODER, GpioaModer, u32, 0x1000_0000);
+    register_array!(REG_ARR, RegArr, u32, 0x1000_0000, isize, 0x10);
 
     #[test]
     fn test_ptr() {
         assert_eq!(GPIOA_MODER.ptr(), 0x1000_0000 as *mut u32);
+    }
+    #[test]
+    fn test_arr() {
+        assert_eq!(REG_ARR.ptr(1), 0x1000_0010 as *mut u32);
     }
 }
