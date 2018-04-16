dff7e4802fcdca1e17c7dc2e668b55079c5f0e78
Jonathan Soo <jcsoo@agora.com>
2018-04-16 07:36:52 -0400

    Use associated type

--- lib/bobbin-reg/src/lib.rs
+++ lib/bobbin-reg/src/lib.rs
@@ -5,25 +5,26 @@ mod rw {
     pub use ::core::ptr::write_volatile as write;
 }
 
-pub trait Register<T: Copy + Default> {
-    const ADDR: *mut T;
+pub trait Register {
+    type Value: Copy + Default;
+    const ADDR: *mut Self::Value;
 
-    fn ptr(&self) -> *mut T { Self::ADDR }
+    fn ptr(&self) -> *mut Self::Value { Self::ADDR }
 
-    fn read(&self) -> T { unsafe { rw::read(Self::ADDR) } }
+    fn read(&self) -> Self::Value { unsafe { rw::read(Self::ADDR) } }
 
-    fn write(&self, value: T) { unsafe { rw::write(Self::ADDR, value) } }
+    fn write(&self, value: Self::Value) { unsafe { rw::write(Self::ADDR, value) } }
 
-    fn with<F: FnOnce(T) -> T>(&self, f: F) {
+    fn with<F: FnOnce(Self::Value) -> Self::Value>(&self, f: F) {
         self.write(f(self.read()))
     }
 
-    fn set<F: FnOnce(T) -> T>(&self, f: F) {
-        self.write(f(T::default()))
+    fn set<F: FnOnce(Self::Value) -> Self::Value>(&self, f: F) {
+        self.write(f(Self::Value::default()))
     }
 
     fn clear(&self) {
-        self.write(T::default())
+        self.write(Self::Value::default())
     }
 }
 
@@ -32,7 +33,8 @@ macro_rules! register {
     ($rid:ident, $rty:ident, $rval:ty, $raddr:expr) => {
         pub const $rid: $rty = $rty {};
         pub struct $rty {}
-        impl Register<$rval> for $rty { 
+        impl Register for $rty { 
+            type Value = $rval;
             const ADDR: *mut $rval = $raddr as *mut $rval;
         }
     };
