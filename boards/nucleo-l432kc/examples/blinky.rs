9d4a5deea23f48a696cf43c681275db1170e1733
Jonathan Soo <jcsoo@agora.com>
2018-04-18 17:42:07 -0400

    blinky update WIP

--- boards/nucleo-l432kc/examples/blinky.rs
+++ boards/nucleo-l432kc/examples/blinky.rs
@@ -7,8 +7,9 @@ extern crate examples;
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    let _ = board::init();    
-    let brd = board::board();
-    let app = examples::led::BlinkLed::new(brd.led0(), brd, 500);
-    app.run()
+    let sys = board::init();    
+    sys.run(|sys| {
+        let app = examples::led::BlinkLed::new(sys.led0(), sys.tick(), 500);
+        app.run()
+    })
 }
