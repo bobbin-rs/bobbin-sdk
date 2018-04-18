25bae4b211fe884245115a23069a19c4c2319aec
Jonathan Soo <jcsoo@agora.com>
2018-04-18 15:28:02 -0400

    Update memory example to explore binary size

--- boards/nucleo-f746zg/examples/memory.rs
+++ boards/nucleo-f746zg/examples/memory.rs
@@ -4,12 +4,21 @@
 #[macro_use]
 extern crate nucleo_f746zg as board;
 
+use ::board::prelude::*;
+
 static mut DATA: [u8; 1024] = [0u8; 1024];
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
     let mut sys = board::init();
+    match run(&mut sys) {
+        Ok(_) => println!("Ok"),
+        Err(_) => println!("Error"),
+    }
+    loop {}
+}
 
+pub fn run(sys: &mut board::System) -> Result<(), Error> {
     for i in 0..1024 {
         unsafe { DATA[i] = i as u8; }
     }
@@ -18,11 +27,11 @@ pub extern "C" fn main() -> ! {
     // println!("{:?}", sys.memory());
 
     let heap = sys.heap_mut();
-    println!("Initial Heap: {:?}", heap);
+    // println!("Initial Heap: {:?}", heap);
 
     unsafe { heap.extend(4096) }
 
-    println!("{:?}", heap);
+    // println!("{:?}", heap);
 
     #[derive(Debug)]
     pub struct Abc { 
@@ -31,18 +40,18 @@ pub extern "C" fn main() -> ! {
         c: u32,
     };
 
-    let v = heap.new(Abc { a: 10, b: 20, c: 30 });
-    println!("v @ {:p}: {:?}", v, v);
-    println!("{:?}", heap);
+    let _v = heap.try_new(Abc { a: 10, b: 20, c: 30 })?;
+    // println!("v @ {:p}: {:?}", v, v);
+    // println!("{:?}", heap);
 
-    let data = heap.slice(0u16, 1024);
-    println!("data @ {:p}", data);
-    println!("{:?}", heap);
+    let _data = heap.try_slice(0u16, 1024)?;
+    // println!("data @ {:p}", data);
+    // println!("{:?}", heap);
 
-    heap.align(512);
-    println!("{:?}", heap);    
+    heap.try_align(512)?;
+    // println!("{:?}", heap);    
     heap.freeze();
-    println!("{:?}", heap);    
-    loop {}
+    // println!("{:?}", heap);    
+    Ok(())
 }
 
