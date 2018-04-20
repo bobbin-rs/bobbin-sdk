0d3d09cc09cb317d2336334f71f2f6a66dfa4c51
Jonathan Soo <jcsoo@agora.com>
2018-04-20 10:17:58 -0400

    Add write_x_hex example

---
+++ boards/nucleo-f746zg/examples/write_hex.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+extern crate nucleo_f746zg as board;
+
+use board::bobbin_sys::board::Board;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    let mut brd = board::init();
+    brd.run(|brd| {
+        if let Some(console) = brd.console() {
+            console.write_u8_hex(0xab);
+            console.write_u16_hex(0xabcd);
+            console.write_u32_hex(0xabcd1234);
+        }
+        loop {}
+    })
+}
