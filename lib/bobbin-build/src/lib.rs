63bbcdd526c569751aaa7168de4f7588c40783d9
Jonathan Soo <jcsoo@agora.com>
2018-04-21 13:03:35 -0400

    Add bobbin-build top-level docs

--- lib/bobbin-build/src/lib.rs
+++ lib/bobbin-build/src/lib.rs
@@ -1,26 +1,25 @@
-#![allow(unused_imports)]
+//! This crate provides build-time functionality.
 
 use std::env;
 use std::io;
 use std::fs::{self, File};
 use std::path::{PathBuf, Path};
 
+/// Searches for a linker script in `link/` based on enabled Cargo features and copies it to the top
+/// crate's build directory. Matching files with a `.ld` extension are copied to `link.ld`, and files
+/// with a `.x` extension are copied to `memory.x`.
 pub fn setup_linker() {
     let cfg_target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     if cfg_target_os != "none" {
-        // println!("cargo:warning=CFG_TARGET_OS is {}, skipping Linker Script", cfg_target_os);
         return
     }
 
     if let Some((ld_script, dst)) = find_ld_script("link").unwrap() {
         copy_link_script(ld_script, dst);
-    } else {
-        // panic!("No linker script found for variant");
     }
 }
 
-
-pub fn find_ld_script<P: AsRef<Path>>(ld_dir: P) -> io::Result<Option<(PathBuf, &'static str)>> {
+fn find_ld_script<P: AsRef<Path>>(ld_dir: P) -> io::Result<Option<(PathBuf, &'static str)>> {
     for dir_entry in ld_dir.as_ref().read_dir()? {
         if let Ok(dir_entry) = dir_entry {
             let path = dir_entry.path();
@@ -42,7 +41,7 @@ pub fn find_ld_script<P: AsRef<Path>>(ld_dir: P) -> io::Result<Option<(PathBuf, 
     Ok(None)
 }
 
-pub fn copy_link_script<P: AsRef<Path>>(src: P, dst: &str) {
+fn copy_link_script<P: AsRef<Path>>(src: P, dst: &str) {
     // Pass our linker script to the top crate
     let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     fs::copy(src, out.join(dst)).unwrap();
