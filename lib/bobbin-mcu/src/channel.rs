f0b45eae64163b8b7a3b03b493099b28054ddf57
Jonathan Soo <jcsoo@agora.com>
2018-04-21 16:36:58 -0400

    Document mcu channel

--- lib/bobbin-mcu/src/channel.rs
+++ lib/bobbin-mcu/src/channel.rs
@@ -18,20 +18,27 @@
 use periph::Periph;
 use signal::SignalType;
 
+/// Represents a channel associated with a peripheral.
 pub trait Channel<P: Periph> {
+    /// Returns the peripheral instance associated with the peripheral.
     fn periph(&self) -> P { P::default() }
+    /// Returns the index of the peripheral.
     fn index(&self) -> u8;
 }
 
+/// Indicates that a signal of `SignalType` is associated with a channel.
 pub trait ChannelSource<ST: SignalType, SRC> {
+    /// Returns the selector used to connect a channel source.
     fn selector(&self) -> u8;
 }
 
+/// Connect a signal to a peripheral channel.
 pub trait ConnectChannel<SRC, STY, PERIPH, CH>
 where 
     STY: SignalType,
     PERIPH: Periph,
     CH: Channel<PERIPH> + ChannelSource<STY, SRC>,
 {
+    /// Connect a channel associated with `signal_type`.
     fn connect_channel(&self, signal_type: STY, channel: CH);
 }
