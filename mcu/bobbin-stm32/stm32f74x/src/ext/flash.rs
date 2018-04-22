ebd8515825a84bdffb4267945f1d813d8170fcce
Jonathan Soo <jcsoo@agora.com>
2018-04-22 14:36:25 -0400

    Remove unused code

--- mcu/bobbin-stm32/stm32f74x/src/ext/flash.rs
+++ mcu/bobbin-stm32/stm32f74x/src/ext/flash.rs
@@ -41,43 +41,6 @@ impl FlashBusy for FlashPeriph {
     }
 }
 
-// impl FlashErase for FlashPeriph {    
-//     fn flash_erase(&self, addr: *const u32,) {
-//         while self.flash_busy() {}
-//         let snb = match addr as u32 {
-//             0x0800_0000 => 0,
-//             0x0800_8000 => 1,
-//             0x0801_0000 => 2,
-//             0x0801_8000 => 3,
-//             0x0802_0000 => 4,
-//             0x0804_0000 => 5,
-//             0x0808_0000 => 6,
-//             0x080c_0000 => 7,
-//             _ => panic!("Invalid flash sector address"),
-//         };        
-//         self.with_cr(|r| r.set_snb(snb).set_ser(1).set_strt(1));
-//         while self.flash_busy() {}
-//         self.with_cr(|r| r.set_ser(0));
-//     }
-// }
-
-// impl FlashWrite<u8> for FlashPeriph {
-//     fn flash_write(&self, addr: *mut u8, data: &[u8]) -> usize {
-//         while self.flash_busy() {}
-//         self.with_cr(|r| r.set_pg(1).set_psize(0b00));
-//         for i in 0..data.len() {
-//             unsafe {
-//                 *addr.offset(i as isize) = data[i];
-//                 asm!("dsb");
-//             }
-//             while self.flash_busy() {}
-//         }
-//         self.with_cr(|r| r.set_pg(0));        
-//         data.len()
-//     }
-// }
-
-
 // impl FlashWrite<u16> for FlashPeriph {
 //     fn flash_write(&self, addr: *mut u16, data: &[u16]) -> usize {
 //         while self.flash_busy() {}
