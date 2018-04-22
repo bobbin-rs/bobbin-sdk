3f82904724e381c4ec50c665efb555efe43feb17
Jonathan Soo <jcsoo@agora.com>
2018-04-22 17:43:43 -0400

    Simplify flash trait

--- lib/bobbin-hal/src/flash.rs
+++ lib/bobbin-hal/src/flash.rs
@@ -21,12 +21,16 @@ pub enum FlashError {
     InvalidEraseSize,
     /// The erase operation failed.
     EraseFailed,
+    /// The erase operation timed out.
+    EraseTimeout,
     /// The write address was not valid.
     InvalidWriteAddr,
     /// The write size was not valid.
     InvalidWriteSize,
     /// The write operation failed.
     WriteFailed,
+    /// Write Timeout
+    WriteTimeout,
 }
 
 /// Query the Flash device for erase and write parameters.
@@ -42,15 +46,21 @@ pub trait FlashInfo {
 pub trait FlashErase {
     /// Put the device into erase mode. `erase_end()` must be called when erasing is done.
     fn erase_begin(&self);
-    /// Starts the erase process for the page containing `addr`. The device must have
+    /// Starts the erase process for the page beginning at `addr`. The device must have
     /// previously be put in erase mode using `erase_begin()`.
-    fn erase_start(&self, addr: *mut u8, len: usize) -> Result<(), FlashError>;
+    fn erase_start(&self, addr: *mut u8) -> Result<(), FlashError>;
     /// Returns true if an erase is in progress.
     fn erase_complete(&self) -> bool;
     /// Busy wait for the erase to complete.
-    fn erase_wait(&self);
+    fn erase_wait(&self) -> Result<(), FlashError> {
+        while !self.erase_complete() {}
+        Ok(())
+    }
     /// Erase flash and wait for the erase to complete, then end the erase.
-    fn erase(&self, addr: *mut u8, len: usize);
+    fn erase(&self, addr: *mut u8) -> Result<(), FlashError> {
+        self.erase_start(addr)?;
+        self.erase_wait()
+    }
     /// Exit erase mode. Behavior is undefined if the device is not currently in erase mode or if
     /// an erase is pending.
     fn erase_end(&self);
@@ -65,9 +75,15 @@ pub trait FlashWrite {
     /// Returns true if the current write is complete;
     fn write_complete(&self) -> bool;
     /// Busy wait for the write to complete;
-    fn write_wait(&self);
+    fn write_wait(&self) -> Result<(), FlashError> {
+        while !self.write_complete() {}
+        Ok(())
+    }
     /// Write `len` bytes from `addr` to flash memory and wait for the write to complete.
-    fn write(&self, dst: *mut u8, src: &[u8]) -> Result<(), FlashError>;
+    fn write(&self, dst: *mut u8, src: &[u8]) -> Result<(), FlashError> {
+        self.write_start(dst, src)?;
+        self.write_wait()
+    }
     /// Exit write mode. Behavior is undefined if the device is not currently in write mode or
     /// if a write is pending.
     fn write_end(&self);
