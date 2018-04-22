3c732e18308b8424863241137b07f2f0d4cfea99
Jonathan Soo <jcsoo@agora.com>
2018-04-22 11:03:23 -0400

    Document `tick`

--- lib/bobbin-sys/src/tick.rs
+++ lib/bobbin-sys/src/tick.rs
@@ -1,3 +1,5 @@
+//! Global timer
+
 use ::core::cell::UnsafeCell;
 use ::core::ptr;
 
@@ -10,40 +12,49 @@ static mut TICKS: UnsafeCell<u32> = UnsafeCell::new(u32::max_value() - 2500);
 struct TickToken;
 static mut TICK_TOKEN: Option<TickToken> = Some(TickToken);
 
+/// Global singleton providing a millisecond tick counter.
 pub struct Tick {
     _private: ()
 }
 
 impl Tick {
+    /// Acquire the global Tick singleton.
     pub fn take() -> Tick {
         unsafe { while let None = TICK_TOKEN {} }
         Tick { _private: () }
     }
 
+    /// Release the global Tick singleton.
     pub fn release(_tick: Tick) {
         unsafe { TICK_TOKEN = Some(TickToken) }
     }
 
+    /// Increment the global tick counter. Usually called from a timer interrupt.
     #[inline]
     pub fn incr_ticks() {
         unsafe { ptr::write_volatile(TICKS.get(), ptr::read_volatile(TICKS.get()).wrapping_add(1)) }
     }
 
+    /// Returns the value of the global tick counter.
     #[inline]
     pub fn ticks(&self) -> u32 {
         unsafe { ptr::read_volatile(TICKS.get()) }
     }
 
+    /// Returns the number of ticks elapsed since `t`.
     #[inline]
     pub fn ticks_since(&self, t: u32) -> u32 {
         self.ticks().wrapping_sub(t)
     }
 
+    /// Busy wait until `ms` ticks have elapsed.
     pub fn delay(&self, ms: u32) {
         let t = self.ticks();
         while self.ticks_since(t) < ms {}
     }
 
+    /// Loop until either `f` returns false or `timeout` ticks have elapsed. Returns
+    /// Error::Timeout if the timeout has elapsed.
     pub fn wait_while<F: FnMut()->bool>(&self, timeout: u32, mut f: F) -> Result<(), Error> {
         let t = self.ticks();
         loop {
@@ -57,6 +68,8 @@ impl Tick {
         }        
     }
 
+    /// Loop until either `f` returns true or `timeout` ticks have elapsed. Returns
+    /// Error::Timeout if the timeout has elapsed.
     pub fn wait_until<F: FnMut()->bool>(&self, timeout: u32, mut f: F) -> Result<(), Error> {
         let t = self.ticks();
         loop {
