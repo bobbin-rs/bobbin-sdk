ebabe824824bf86e06edcc07513870877d61449b
Jonathan Soo <jcsoo@agora.com>
2018-04-30 10:00:27 -0400

    Add comments

--- mcu-examples/bobbin-stm32f746zg-example/src/blinky.rs
+++ mcu-examples/bobbin-stm32f746zg-example/src/blinky.rs
@@ -9,17 +9,19 @@ use mcu::bobbin_mcu::prelude::*;
 use mcu::bobbin_hal::prelude::*;
 use mcu::pin::*;
 
+// The LED for this board is pin PB0. Notice that PB0 is a constant of type Pb0.
 pub const LED: Pb0 = PB0;
 
 fn main() {
+    // Initialize the MCU (enable instruction cache, disable watchdogs, etc.)
     mcu::ext::init();
     
     LED
-        .port_gate_enable()
-        .mode_output();
+        .port_gate_enable() // Enable the port associated with this pin.
+        .mode_output(); // Set this port to digital output.
 
     loop {
-        LED.toggle_output();
-        for _ in 0..5_000_000 { unsafe { asm!("nop") }}
+        LED.toggle_output(); // Toggle the digital output.        
+        for _ in 0..5_000_000 { unsafe { asm!("nop") }} // Delay
     }    
 }
--- mcu-examples/bobbin-stm32f746zg-example/src/console.rs
+++ mcu-examples/bobbin-stm32f746zg-example/src/console.rs
@@ -1,15 +1,15 @@
 #![no_std]
 #![feature(asm)]
 
-extern crate cortex_m_rt;
-extern crate panic_abort;
-extern crate stm32f74x as mcu;
+extern crate cortex_m_rt; // use the cortex-m-rt crate
+extern crate panic_abort; // abort on panic
+extern crate stm32f74x as mcu; // use the MCU crate
 
 use mcu::bobbin_mcu::prelude::*;
 use mcu::bobbin_hal::prelude::*;
+use mcu::pin::*;
 use mcu::ext::rcc::DedicatedClock;
 use mcu::usart::*;
-use mcu::pin::*;
 
 pub const USART: Usart3 = USART3;
 const USART_TX: Pd8 = PD8;
@@ -18,23 +18,25 @@ const USART_CLOCK: u32 = 16_000_000; // Use HSI Clock
 const USART_BAUD: u32 = 115_200;
 
 fn main() {
+    // Initialize the MCU (enable instruction cache, disable watchdogs, etc.)
     mcu::ext::init();
+    
     USART_TX
-        .port_gate_enable()
-        .connect_to(USART);
+        .port_gate_enable() // Enable the clock for the port associated with this pin.
+        .connect_to(USART); // Connect the pin to the USART that we are using.
 
     USART_RX
-        .port_gate_enable()
-        .connect_to(USART);
+        .port_gate_enable() // Enable the clock for the port associated with this pin.
+        .connect_to(USART); // Connect the pin to the USART that we are using.
 
     USART
-        .set_clock_source(DedicatedClock::Hsi)
-        .gate_enable()
-        .set_config(|c| c.set_baud_clock(USART_BAUD, USART_CLOCK))
-        .enable();
+        .set_clock_source(DedicatedClock::Hsi) // use the 16MHz HSI clock source
+        .gate_enable() // Enable the USART clock
+        .set_config(|c| c.set_baud_clock(USART_BAUD, USART_CLOCK)) // Configure the USART
+        .enable(); // Enable the USART
 
     loop {
-        USART.write(b"Hello, World\r\n");
+        USART.write(b"Hello, World\r\n"); // Write a byte slice. Use `\r\n` for a newline.
         for _ in 0..10_000_000 { unsafe { asm!("nop") }}
     }    
 }
