e3d1dace546001d249c943fc33fe1f8394ff763c
Jonathan Soo <jcsoo@agora.com>
2019-06-29 10:03:16 -0400

    Test with SAMD21 mcu crate

--- mcu-examples/bobbin-samd51-example/src/blinky_basic.rs
+++ mcu-examples/bobbin-samd51-example/src/blinky_basic.rs
@@ -6,25 +6,30 @@ use cortex_m_rt::entry;
 use cortex_m::asm;
 use core::ptr;
 
-use samd21::bobbin_hal::prelude::*;
-use samd21::pin::PA23;
-use samd21::port::PORTA;
-const PORTA_OUTTGL: u32 = 0x4100_801C;
-
 // LED Pin D13 = a23
+
+// const PORTA: u32 = 0x4100_8000;
+const PORTA_DIRSET: u32 = 0x4100_8008;
+const PORTA_OUTTGL: u32 = 0x4100_801C;
 
 #[entry]
 fn main() -> ! {
-    PA23.set_mode_output();
 
+    // Enable PORT bus clock (CLK_PORT_APB)
+    // Set a23 as output
+    // DIRSET |= 1 << 23
+
+    unsafe {
+        ptr::write_volatile(PORTA_DIRSET as *mut u32, 1 << 23);
+    }
+
+    // 
     // Set a23 high / set a23 low
 
     loop {
         for _ in 0..1_000_000 {
             asm::nop();
         }
-        // PA23.toggle_output();
-        // PORTA.set_outtgl(|r| r.set_outtgl(23, 1));
         unsafe {
             ptr::write_volatile(PORTA_OUTTGL as *mut u32, 1 << 23);
         }        
