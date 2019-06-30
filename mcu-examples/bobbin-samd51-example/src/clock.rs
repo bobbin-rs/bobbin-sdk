f6340e7f0711e67ddfb22ebb9beea0eefa11f674
Jonathan Soo <jcsoo@agora.com>
2019-06-30 17:14:12 -0400

    Working 120MHz clock startup

--- mcu-examples/bobbin-samd51-example/src/clock.rs
+++ mcu-examples/bobbin-samd51-example/src/clock.rs
@@ -13,7 +13,10 @@ use samd51::bobbin_sys::{print, println};
 use samd51::port::PORTB;
 use samd51::sercom::SERCOM5;
 use samd51::pin::PA23;
-use samd51::gclk;
+use samd51::gclk::{self, GCLK};
+use samd51::nvmctrl::NVMCTRL;
+use samd51::mclk::MCLK;
+use samd51::oscctrl::OSCCTRL;
 
 // LED Pin D13 = a23
 
@@ -70,7 +73,58 @@ fn main() -> ! {
     PORTB.with_pincfg(16, |r| r.set_pmuxen(1));
     PORTB.with_pincfg(17, |r| r.set_pmuxen(1));
 
-    gclk::GCLK.write_pchctrl(35, gclk::Pchctrl(0)
+    // Set Flash to Half Auto Wait State
+    //    - NVMCTRLA.CTRLA.RWS = 0x1 (Half Auto Wait State)
+
+    NVMCTRL.with_ctrla(|r| r.set_rws(0x1));
+
+    // Enable GCLK APB
+    //    - MCLK.APBAMASK |= GCLK
+
+    MCLK.with_apbamask(|r| r.set_gclk(1));
+
+    // Set GEN5 to 2MHz
+    //    - GENCTRL[5].SRC = 0x06 (DFLL @ 48MHz)
+    //    - GENCTRL[5].DIV = 24 (divide by 24) => 2MHz
+    //    - GENCTRL[5].GENEN = 1
+
+    GCLK.with_genctrl(5, |r| r.set_src(0x6).set_div(24).set_genen(1));
+
+    // Set DPLL0 (GCLK1) to use GEN5 as source
+    //   - PCHCTRL[1].CHEN = 1
+    //   - PCHCTRL[1].GEN = 0x5 (GEN5)
+
+    GCLK.with_pchctrl(1, |r| r.set_chen(1).set_gen(0x5));
+
+    // Configure DPLL0 to output 120MHz (60 * 2MHz)
+    //   - DPLLRATIO[0].LDR = 59 (multiply by 60)
+    //   - DPLLRATIO[0].LDRFRAC = 0
+    //   - DPLLCTRLB.REFCLK = 0 (GCLK5)
+    //   - DPLLCTRLA.ENABLE = 1
+    //   - DPLLCTRLA.ONDEMAND = 0
+
+    OSCCTRL
+        .with_dpllratio(0, |r| r.set_ldr(59).set_ldrfrac(0))
+        .with_dpllctrlb(0, |r| r.set_refclk(0))
+        .with_dpllctrla(0, |r| r.set_enable(1).set_ondemand(0));
+
+    // Set GEN0 to use DPLL0
+    //   - SRC = 0x07 (DPLL0)
+    //   - DIV = 1
+    //   - OE = 1
+    //   - GENEN = 1
+
+    GCLK.with_genctrl(0, |r| r.set_src(0x7).set_div(1).set_oe(1).set_genen(1));
+
+    //  Set MCLK CPU Divider to 1 (120MHz)
+    //   - MCLK.CPUDIV.DIV = 1 (Divide by 1)
+
+    MCLK.with_cpudiv(|r| r.set_div(1));
+
+
+    // Use clockgen 0 for SERCOM5
+
+    GCLK.write_pchctrl(35, gclk::Pchctrl(0)
         .set_gen(0x0)
         .set_chen(1)
     );
@@ -79,7 +133,7 @@ fn main() -> ! {
 
     let cfg = samd51::ext::sercom::Config::default()
         .set_mode(samd51::ext::sercom::Mode::UsartInternal)
-        .set_baud_clock(115_200, 48_000_000)
+        .set_baud_clock(115_200, 120_000_000)
         .set_rxpo(0x1)
         .set_txpo(0x0);
     SERCOM5.configure(cfg);
