4c25318d813356e4b6c288116aebf1b71b4a3b85
Evgenii P <eupn@protonmail.com>
2019-07-09 03:01:52 +0700

    Use cortex-m-rt attributes instead of macros for IRQs in board crates

--- board/feather-m0/src/lib.rs
+++ board/feather-m0/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
-#![feature(use_extern_macros)]
 
 extern crate panic_abort;
 extern crate cortex_m_rt;
-pub extern crate samd21 as mcu;
+use cortex_m_rt::exception;
 
+pub extern crate samd21 as mcu;
 
 pub mod prelude;
 pub mod led;
@@ -28,6 +28,18 @@ impl bobbin_sys::board::Board for FeatherM0 {
     fn id(&self) -> &'static str { "feather-m0" }    
 }
 
-cortex_m_rt::default_handler!(bobbin_sys::irq_dispatch::IrqDispatcher::<Mcu>::handle_exception);
-cortex_m_rt::exception!(SYS_TICK, bobbin_sys::tick::Tick::tick);
-cortex_m_rt::exception!(PENDSV, bobbin_sys::pend::Pend::pend);
+
+#[exception]
+fn DefaultHandler(_irqn: i16) {
+    bobbin_sys::irq_dispatch::IrqDispatcher::<Mcu>::handle_exception();
+}
+
+#[exception]
+fn SysTick() {
+    bobbin_sys::tick::Tick::tick();
+}
+
+#[exception]
+fn PendSV() {
+    bobbin_sys::pend::Pend::pend();
+}
